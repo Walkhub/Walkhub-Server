515162daf0c4fec6f05d08805736bd97ebaea017
smoothbear <kjbin0420@naver.com>
2022-01-03 11:22:42 +0900

    :gear: :: (#3) 프로젝트 구조 설정

---
+++ src/main/java/com/walkhub/walkhub/WalkhubApplication.java
@@ -0,0 +1,13 @@
+package com.walkhub.walkhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walkhub/walkhub/WalkhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walkhub.walkhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
