86c6ba2697cd88cdb4341de23c5b3fb7c12723c7
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-04 17:55:27 +0900

    📑 :: school Entity 생성

---
+++ src/main/java/com/walkhub/walkhub/domain/user/entity/School.java
@@ -0,0 +1,25 @@
+package com.walkhub.walkhub.domain.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class School {
+
+    @Id
+    @Column(length = 7, nullable = false)
+    private String agencyCode;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column()
+    private String logoImage;
+}
