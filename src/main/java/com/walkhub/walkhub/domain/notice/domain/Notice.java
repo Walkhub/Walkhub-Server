b2e7118a41ad3cd0bb5ae4133f8a17f396199408
이준서 <ljchan71@gmail.com>
2022-01-05 23:39:32 +0900

    ⚡ :: builder추가

--- src/main/java/com/walkhub/walkhub/domain/notice/domain/Notice.java
+++ src/main/java/com/walkhub/walkhub/domain/notice/domain/Notice.java
@@ -2,8 +2,11 @@ package com.walkhub.walkhub.domain.notice.domain;
 
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.global.entity.BaseTimeEntity;
+import com.walkhub.walkhub.global.enums.Scope;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,14 +30,23 @@ public class Notice extends BaseTimeEntity {
 	@Column(length = 200, nullable = false)
 	private String title;
 
-	@Column(columnDefinition = "char(3)")
-	private String scope;
+	@Column(columnDefinition = "char(3)", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Scope scope;
 
-	@Column(columnDefinition = "TEXT")
+	@Column(columnDefinition = "TEXT", nullable = false)
 	private String content;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
+	@Builder
+	public Notice(String title, Scope scope, String content,
+		User user) {
+		this.title = title;
+		this.scope = scope;
+		this.content = content;
+		this.user = user;
+	}
 }
