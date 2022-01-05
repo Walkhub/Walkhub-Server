7971ec59b79491fc1839f6fe88a6558596762e2d
이준서 <ljchan71@gmail.com>
2022-01-05 23:53:30 +0900

    📑 :: BadgeCollection entity 추가

---
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/BadgeCollectionId.java
@@ -0,0 +1,17 @@
+package com.walkhub.walkhub.domain.user.domain;
+
+import java.io.Serializable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class BadgeCollectionId implements Serializable {
+
+	private Long badge;
+
+	private Long user;
+}
