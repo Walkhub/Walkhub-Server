c1a360ab32d2c215337f628375f8645ec5a81124
김범진 <sdpthf@naver.com>
2022-01-05 14:25:33 +0900

    ♻ :: @Repository 제거

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/GroupRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/GroupRepository.java
@@ -3,8 +3,6 @@ package com.walkhub.walkhub.domain.user.domain.repository;
 import com.walkhub.walkhub.domain.user.domain.Group;
 import com.walkhub.walkhub.domain.user.domain.GroupId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface GroupRepository extends JpaRepository<Group, GroupId> {
 }
--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/SchoolRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/SchoolRepository.java
@@ -2,8 +2,6 @@ package com.walkhub.walkhub.domain.user.domain.repository;
 
 import com.walkhub.walkhub.domain.user.domain.School;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SchoolRepository extends JpaRepository<School, String> {
 }
