97ac01998f28c31e5c8a5c69a73bf5a5532c0e2a
김범진 <sdpthf@naver.com>
2022-01-05 19:18:33 +0900

    📑 :: LocationId

---
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/LocationId.java
@@ -0,0 +1,26 @@
+package com.walkhub.walkhub.domain.exercise.domain;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class LocationId implements Serializable {
+
+    @Column(nullable = false)
+    private Integer order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+
+}
