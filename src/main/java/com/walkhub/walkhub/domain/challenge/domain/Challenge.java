863c7afd5549fd409440c877eadf304a46aa700e
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-05 19:25:39 +0900

    📑 :: Challenge 추가

---
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/Challenge.java
@@ -0,0 +1,74 @@
+package com.walkhub.walkhub.domain.challenge.domain;
+
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.global.enums.Scope;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Challenge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    private String image;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+    @Column(nullable = false)
+    private LocalDateTime endAt;
+
+    @Column(length = 200, nullable = false)
+    private String award;
+
+    @Column(nullable = false)
+    private Scope scope;
+
+    @Column(nullable = false)
+    private Long goal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder()
+    public Challenge(String name, String content, Long goal, String award,
+                     LocalDateTime createAt, LocalDateTime endAt, Scope scope, User user) {
+        this.name = name;
+        this.content = content;
+        this.goal = goal;
+        this.award = award;
+        this.createAt = createAt;
+        this.endAt = endAt;
+        this.scope = scope;
+        this.user = user;
+
+    }
+
+    public void updateChallenge(String name, String content, Long goal, String award, String image,
+                                LocalDateTime createAt, LocalDateTime endAt, Scope scope) {
+        this.name = name;
+        this.content = content;
+        this.goal = goal;
+        this.award = award;
+        this.image = image;
+        this.createAt = createAt;
+        this.endAt = endAt;
+        this.scope = scope;
+    }
+}
---
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
@@ -0,0 +1,7 @@
+package com.walkhub.walkhub.domain.challenge.domain.repository;
+
+import com.walkhub.walkhub.domain.challenge.domain.Challenge;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ChallengeRepository extends CrudRepository<Challenge, Long> {
+}
