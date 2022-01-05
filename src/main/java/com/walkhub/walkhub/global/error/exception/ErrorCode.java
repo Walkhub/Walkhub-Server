33679941ef5ad30f0b2c0ae667c561d5e851aa00
이준서 <ljchan71@gmail.com>
2022-01-06 08:57:57 +0900

    📑 :: ErrorCode

---
+++ src/main/java/com/walkhub/walkhub/global/error/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.walkhub.walkhub.global.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    ;
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+}
