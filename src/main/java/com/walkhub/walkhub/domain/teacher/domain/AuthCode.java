39bd6c7427a594761bb97b86844e4c5bacbb666a
김범진 <sdpthf@naver.com>
2022-01-05 13:58:08 +0900

    📑 :: AuthCode

---
+++ src/main/java/com/walkhub/walkhub/domain/teacher/domain/AuthCode.java
@@ -0,0 +1,35 @@
+package com.walkhub.walkhub.domain.teacher.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import javax.persistence.Column;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash
+public class AuthCode {
+
+    @Id
+    @Column(name = "agency_code", columnDefinition = "char(7)")
+    private String school;
+
+    @Column(columnDefinition = "char(7)")
+    private String authCode;
+
+    @TimeToLive
+    private Long expiredAt;
+
+    @Builder
+    public AuthCode(String school, String authCode) {
+        this.school = school;
+        this.authCode = authCode;
+        this.expiredAt = 300L;
+    }
+
+}
