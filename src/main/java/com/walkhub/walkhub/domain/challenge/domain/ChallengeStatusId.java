6f45dad18b77edd2de8bf7d67690206c187ce974
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-05 20:11:05 +0900

    📑 :: ChallengeStatusId

---
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/ChallengeStatusId.java
@@ -0,0 +1,19 @@
+package com.walkhub.walkhub.domain.challenge.domain;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class ChallengeStatusId implements Serializable {
+
+    private Long challenge;
+
+    private Long user;
+}
