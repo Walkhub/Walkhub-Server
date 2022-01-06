2d401cf1055e62e7fe47d7780ecb99a6dddcaede
lyutvs <lyutvs@gmail.com>
2022-01-07 01:26:27 +0900

    ⚙️ :: EnabelConfigurationPropertiesScan 주입

--- src/main/java/com/walkhub/walkhub/WalkhubApplication.java
+++ src/main/java/com/walkhub/walkhub/WalkhubApplication.java
@@ -2,9 +2,11 @@ package com.walkhub.walkhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class WalkhubApplication {
 
