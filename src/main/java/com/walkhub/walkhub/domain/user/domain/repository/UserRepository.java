62ab0df78a5eaeb1964a2a7f9227e15398a416ab
김범진 <sdpthf@naver.com>
2022-01-07 00:06:51 +0900

    📑 :: AuthDetailsService

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.walkhub.walkhub.domain.user.domain.repository;
 import com.walkhub.walkhub.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByAccountId(String accountId);
 }
