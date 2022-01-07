411eb175c4992cb0c349ab56d7fc9d91f2019fff
김범진 <sdpthf@naver.com>
2022-01-07 11:10:39 +0900

    ♻ :: import 제거

--- src/main/java/com/walkhub/walkhub/global/security/SecurityConfig.java
+++ src/main/java/com/walkhub/walkhub/global/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.walkhub.walkhub.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
