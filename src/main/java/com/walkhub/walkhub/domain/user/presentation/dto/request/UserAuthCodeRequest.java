8c9d59c98e8d2f5dd7968b4b2abed81eab873c90
김범진 <sdpthf@naver.com>
2022-01-10 13:34:32 +0900

    ⚡ :: dto validation

--- src/main/java/com/walkhub/walkhub/domain/user/presentation/dto/request/UserAuthCodeRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/user/presentation/dto/request/UserAuthCodeRequest.java
@@ -3,13 +3,15 @@ package com.walkhub.walkhub.domain.user.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class UserAuthCodeRequest {
 
-    @Pattern(regexp = "^\\\\d{3}\\\\d{3,4}\\\\d{4}$", message = "잘못된 전화번호입니다.")
+    @NotBlank(message = "phone_number는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @Size(min = 11, max = 11, message = "phone_number는 11자여야 합니다.")
     private String phoneNumber;
 
 }
