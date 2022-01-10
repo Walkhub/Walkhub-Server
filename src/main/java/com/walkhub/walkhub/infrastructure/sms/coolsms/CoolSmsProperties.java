0ddbd1ab5533bf7b9454985541ad5d30db494129
김범진 <sdpthf@naver.com>
2022-01-10 11:15:27 +0900

    ♻ :: @RequiredArgsConstructor로 변경

--- src/main/java/com/walkhub/walkhub/infrastructure/sms/coolsms/CoolSmsProperties.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/sms/coolsms/CoolSmsProperties.java
@@ -1,10 +1,12 @@
 package com.walkhub.walkhub.infrastructure.sms.coolsms;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
+@RequiredArgsConstructor
 @ConstructorBinding
 @ConfigurationProperties(prefix = "coolsms")
 public class CoolSmsProperties {
@@ -12,11 +14,5 @@ public class CoolSmsProperties {
     private final String key;
     private final String secret;
     private final String phoneNumber;
-
-    public CoolSmsProperties(String key, String secret, String phoneNumber) {
-        this.key = key;
-        this.secret = secret;
-        this.phoneNumber = phoneNumber;
-    }
 
 }
