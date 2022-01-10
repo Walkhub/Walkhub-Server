c6f10c7bf38deeb0efe07fa95f7b2aba465f4784
jhhong0509 <jhhong0509@naver.com>
2022-01-10 16:13:12 +0900

    :recycle: :: RefreshToken 필드 명 수정

--- src/main/java/com/walkhub/walkhub/domain/auth/domain/RefreshToken.java
+++ src/main/java/com/walkhub/walkhub/domain/auth/domain/RefreshToken.java
@@ -12,7 +12,7 @@ import org.springframework.data.redis.core.RedisHash;
 public class RefreshToken {
 
     @Id
-    private String refreshToken;
+    private String token;
 
     private String accountId;
 
