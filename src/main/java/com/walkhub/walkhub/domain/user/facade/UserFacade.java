1f469133d622bd573d743c625e264be411f0a932
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-10 23:31:07 +0900

    ⚡️ :: userExists 추가

--- src/main/java/com/walkhub/walkhub/domain/user/facade/UserFacade.java
+++ src/main/java/com/walkhub/walkhub/domain/user/facade/UserFacade.java
@@ -2,6 +2,7 @@ package com.walkhub.walkhub.domain.user.facade;
 
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.domain.repository.UserRepository;
+import com.walkhub.walkhub.domain.user.exception.UserExistsException;
 import com.walkhub.walkhub.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,5 +16,11 @@ public class UserFacade {
 	public User getUserById(Long userId) {
 		return userRepository.findById(userId)
 			.orElseThrow(() -> UserNotFoundException.EXCEPTION);
+	}
+
+	public void	userExists(String accountId) {
+		if (userRepository.findByAccountId(accountId).isPresent()) {
+			throw UserExistsException.EXCEPTION;
+		}
 	}
 }
