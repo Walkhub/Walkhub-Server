a163f3d416b29777cb3acc6668b36fb5e6f53321
이준서 <ljchan71@gmail.com>
2022-01-10 21:13:04 +0900

    ⚡️ :: queryMyPage

--- src/main/java/com/walkhub/walkhub/domain/user/presentation/UserController.java
+++ src/main/java/com/walkhub/walkhub/domain/user/presentation/UserController.java
@@ -1,6 +1,8 @@
 package com.walkhub.walkhub.domain.user.presentation;
 
 import com.walkhub.walkhub.domain.user.presentation.dto.request.UserAuthCodeRequest;
+import com.walkhub.walkhub.domain.user.presentation.dto.response.QueryMyPageResponse;
+import com.walkhub.walkhub.domain.user.service.QueryMyPageService;
 import com.walkhub.walkhub.domain.user.service.UserAuthCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,11 +16,17 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserAuthCodeService userAuthCodeService;
+    private final QueryMyPageService queryMyPageService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/verification-codes")
     public void sendAuthCode(@RequestBody @Valid UserAuthCodeRequest request) {
         userAuthCodeService.execute(request);
+    }
+
+    @GetMapping
+    public QueryMyPageResponse queryMyPage() {
+        return queryMyPageService.execute();
     }
 
 }
