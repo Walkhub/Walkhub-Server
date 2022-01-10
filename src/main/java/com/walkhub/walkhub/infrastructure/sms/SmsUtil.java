f880e3914955a415c4a6c4685f2e0603cdf6cae8
김범진 <sdpthf@naver.com>
2022-01-10 10:19:38 +0900

    ⚡ :: Return 타입 변경

--- src/main/java/com/walkhub/walkhub/infrastructure/sms/SmsUtil.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/sms/SmsUtil.java
@@ -1,5 +1,5 @@
 package com.walkhub.walkhub.infrastructure.sms;
 
 public interface SmsUtil {
-    String sendCode(String phoneNumber, String code);
+    void sendCode(String phoneNumber, String code);
 }
