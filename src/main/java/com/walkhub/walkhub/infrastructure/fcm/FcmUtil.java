b53a766ab15b5adefcb597cf2094a075ae92e0e8
lyutvs <lyutvs@gmail.com>
2022-01-11 23:07:16 +0900

    ♻️ :: 어노테이션 정리 및 임포트 정리

--- src/main/java/com/walkhub/walkhub/infrastructure/fcm/FcmUtil.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/fcm/FcmUtil.java
@@ -1,7 +1,5 @@
 package com.walkhub.walkhub.infrastructure.fcm;
 
-import com.walkhub.walkhub.domain.notification.domain.type.Type;
-import com.walkhub.walkhub.global.enums.Scope;
 import com.walkhub.walkhub.infrastructure.fcm.dto.SendDto;
 
 public interface FcmUtil {
