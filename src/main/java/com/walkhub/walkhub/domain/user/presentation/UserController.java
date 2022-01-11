9c869dff05862fb9531fac64a25d47e88f463eea
parksangwoo <b01056721617@gmail.com>
2022-01-11 23:04:47 +0900

    ⚡ :: PatchMapping으로 변경

--- src/main/java/com/walkhub/walkhub/domain/user/presentation/UserController.java
+++ src/main/java/com/walkhub/walkhub/domain/user/presentation/UserController.java
@@ -9,13 +9,7 @@ import com.walkhub.walkhub.domain.user.presentation.dto.request.UserSignUpReques
 import com.walkhub.walkhub.domain.user.presentation.dto.response.QueryUserProfileResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -52,7 +46,7 @@ public class UserController {
         return userSignUpService.execute(request);
     }
 
-    @PostMapping("/health")
+    @PatchMapping("/health")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void inputHealthInformationRequest(@RequestBody @Valid InputHealthInformationRequest request) {
         inputHealthInformationService.execute(request);
