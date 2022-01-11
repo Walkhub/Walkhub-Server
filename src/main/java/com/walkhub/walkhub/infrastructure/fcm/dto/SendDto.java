7ee26bebcef4909eba6bed7646476834329aef6d
lyutvs <lyutvs@gmail.com>
2022-01-11 22:38:30 +0900

    📑 :: SendDto 작성

---
+++ src/main/java/com/walkhub/walkhub/infrastructure/fcm/dto/SendDto.java
@@ -0,0 +1,20 @@
+package com.walkhub.walkhub.infrastructure.fcm.dto;
+
+import com.walkhub.walkhub.domain.notification.domain.type.Type;
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.global.enums.Scope;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SendDto {
+    private final User user;
+    private final String title;
+    private final String content;
+    private final Long value;
+    private final Type type;
+    private final Scope scope;
+}
