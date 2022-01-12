a0e6f26f1252c84e6e6ee1bd782040e7f170dbf8
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-12 16:01:33 +0900

    ⚡️ :: Exception 처리 수정

--- src/main/java/com/walkhub/walkhub/infrastructure/image/s3/S3Facade.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/image/s3/S3Facade.java
@@ -3,13 +3,12 @@ package com.walkhub.walkhub.infrastructure.image.s3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.walkhub.walkhub.global.exception.ImageNotFoundException;
+import com.walkhub.walkhub.global.exception.SaveImageFalseException;
 import com.walkhub.walkhub.infrastructure.image.ImageUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -26,8 +25,8 @@ public class S3Facade implements ImageUtil {
         try {
             amazonS3Client.putObject(new PutObjectRequest(s3Properties.getBucket(), fileName, image.getInputStream(), null)
                     .withCannedAcl(CannedAccessControlList.PublicRead));
-        } catch (IOException e) {
-            throw ImageNotFoundException.EXCEPTION;
+        } catch (Exception e) {
+            throw SaveImageFalseException.EXCEPTION;
         }
 
         return getFileUrl(fileName);
