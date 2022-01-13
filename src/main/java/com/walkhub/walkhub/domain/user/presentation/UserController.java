8e3cb1a5d9a757abec84ad1fbfd969a674a5a12e
lyutvs <lyutvs@gmail.com>
2022-01-13 15:16:28 +0900

    ♻️ :: API 부분 변경

--- src/main/java/com/walkhub/walkhub/domain/user/presentation/UserController.java
+++ src/main/java/com/walkhub/walkhub/domain/user/presentation/UserController.java
@@ -10,14 +10,7 @@ import com.walkhub.walkhub.domain.user.presentation.dto.response.QueryUserProfil
 import com.walkhub.walkhub.domain.user.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -51,8 +44,9 @@ public class UserController {
         return queryUserProfileService.execute(userId);
     }
 
-    @GetMapping("/{user-id}/badges")
-    public QueryMyPageResponse titleBadgeSetting(@PathVariable("user-id") Long userId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/badges/{badge-id}")
+    public QueryMyPageResponse titleBadgeSetting(@PathVariable("badge-id")Long badgeId) {
         return titleBadgeSettingService.execute();
     }
 
