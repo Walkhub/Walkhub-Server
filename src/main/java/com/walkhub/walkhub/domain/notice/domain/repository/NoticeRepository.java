5288ee85166c8e04199a100cab7200c70aa3c21d
이준서 <ljchan71@gmail.com>
2022-01-14 23:52:24 +0900

    ⚡️:: crud -> jpa

--- src/main/java/com/walkhub/walkhub/domain/notice/domain/repository/NoticeRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/notice/domain/repository/NoticeRepository.java
@@ -1,7 +1,8 @@
 package com.walkhub.walkhub.domain.notice.domain.repository;
 
 import com.walkhub.walkhub.domain.notice.domain.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface NoticeRepository extends CrudRepository<Notice, Long> {
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
 }
