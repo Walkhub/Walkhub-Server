35a2984e2b3a6e52939e45aef1be1c656265b817
parksangwoo <b01056721617@gmail.com>
2022-01-16 16:29:14 +0900

    📑 :: NotificationNotFoundException

---
+++ src/main/java/com/walkhub/walkhub/domain/notification/exception/NotificationNotFoundException.java
@@ -0,0 +1,15 @@
+package com.walkhub.walkhub.domain.notification.exception;
+
+import com.walkhub.walkhub.domain.user.exception.UserNotFoundException;
+import com.walkhub.walkhub.global.error.exception.ErrorCode;
+import com.walkhub.walkhub.global.error.exception.WalkhubException;
+
+public class NotificationNotFoundException extends WalkhubException {
+    public static final WalkhubException EXCEPTION =
+            new NotificationNotFoundException();
+
+    private NotificationNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+
+}
