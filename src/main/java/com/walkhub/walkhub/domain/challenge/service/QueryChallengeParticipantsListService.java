4bcf030ab6c6cdf50b2067d6347fa5febe07d9ea
김범진 <sdpthf@naver.com>
2022-01-18 00:21:47 +0900

    ⚡ :: 전체 사용자 접근 가능

--- src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeParticipantsListService.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeParticipantsListService.java
@@ -4,8 +4,6 @@ import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeStatusRep
 import com.walkhub.walkhub.domain.challenge.presentation.dto.response.ChallengeParticipantsElement;
 import com.walkhub.walkhub.domain.challenge.presentation.dto.response.ChallengeParticipantsListResponse;
 import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.domain.user.facade.UserFacade;
-import com.walkhub.walkhub.global.enums.Authority;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,17 +15,12 @@ import java.util.stream.Collectors;
 @Service
 public class QueryChallengeParticipantsListService {
 
-    private final UserFacade userFacade;
-
     private final ChallengeStatusRepository challengeStatusRepository;
 
     @Transactional(readOnly = true)
     public ChallengeParticipantsListResponse execute(Long challengeId) {
-        User user = userFacade.getCurrentUser();
-
         List<ChallengeParticipantsElement> result = challengeStatusRepository.findByChallengeId(challengeId)
                 .stream()
-                .filter(authority -> !user.getAuthority().equals(Authority.USER))
                 .map(challenge -> {
                     User participant = challenge.getUser();
                     return ChallengeParticipantsElement.builder()
