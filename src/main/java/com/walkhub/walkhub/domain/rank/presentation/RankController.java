8cbd952cc1d78013f1ceb2e925f5f1ada8083f78
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-17 10:35:53 +0900

    :bookmark_tabs: :: RankController

---
+++ src/main/java/com/walkhub/walkhub/domain/rank/presentation/RankController.java
@@ -0,0 +1,27 @@
+package com.walkhub.walkhub.domain.rank.presentation;
+
+import com.walkhub.walkhub.domain.rank.presentation.dto.response.UserListResponse;
+import com.walkhub.walkhub.domain.rank.service.UserSearchService;
+import com.walkhub.walkhub.global.enums.Scope;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/ranks")
+@RestController
+public class RankController {
+
+    private final UserSearchService userSearchService;
+
+    @GetMapping("/search")
+    public UserListResponse userSearch(@RequestParam String name,
+                                       @RequestParam Scope scope,
+                                       @RequestParam String agencyCode,
+                                       @RequestParam Integer grade,
+                                       @RequestParam Integer classNum) {
+        return userSearchService.execute(name, scope, agencyCode, grade, classNum);
+    }
+}
