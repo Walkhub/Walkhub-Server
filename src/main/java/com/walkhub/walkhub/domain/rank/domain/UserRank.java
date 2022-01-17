801531b8cc19bc50428b1e50d01dba5cecab5a2f
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-17 10:34:47 +0900

    :bookmark_tabs: :: UserRank

---
+++ src/main/java/com/walkhub/walkhub/domain/rank/domain/UserRank.java
@@ -0,0 +1,32 @@
+package com.walkhub.walkhub.domain.rank.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Immutable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Immutable
+@Table(name = "user_rank")
+public class UserRank {
+
+    @Id
+    private String accountId;
+
+    private String name;
+
+    private Integer grade;
+    private Integer classNum;
+    private String profileImageUrl;
+
+    private Long walkCount;
+    private Long userRank;
+    private String agencyCode;
+
+}
