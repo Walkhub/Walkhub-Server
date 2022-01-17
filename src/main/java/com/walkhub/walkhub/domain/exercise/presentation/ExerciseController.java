13e5915abcc1124f85aabf7f2e8d07b0bc735015
이준서 <ljchan71@gmail.com>
2022-01-17 16:00:05 +0900

    ⚡ :: responseStatus

--- src/main/java/com/walkhub/walkhub/domain/exercise/presentation/ExerciseController.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/ExerciseController.java
@@ -4,9 +4,11 @@ import com.walkhub.walkhub.domain.exercise.presentation.dto.request.CreateExerci
 import com.walkhub.walkhub.domain.exercise.presentation.dto.response.CreateExerciseResponse;
 import com.walkhub.walkhub.domain.exercise.service.CreateExerciseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class ExerciseController {
 	private final CreateExerciseService createExerciseService;
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public CreateExerciseResponse createExercise(@RequestBody CreateExerciseRequest request) {
 		return createExerciseService.execute(request);
 	}
