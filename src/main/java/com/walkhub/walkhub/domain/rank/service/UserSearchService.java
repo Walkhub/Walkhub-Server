adbbc7e06b731f1a9c7fe8374442285d9f6f71e1
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-17 10:37:44 +0900

    :bookmark_tabs: :: UserSearchService

---
+++ src/main/java/com/walkhub/walkhub/domain/rank/service/UserSearchService.java
@@ -0,0 +1,44 @@
+package com.walkhub.walkhub.domain.rank.service;
+
+import com.walkhub.walkhub.domain.rank.domain.UserRank;
+import com.walkhub.walkhub.domain.rank.domain.repository.UserRankRepository;
+import com.walkhub.walkhub.domain.rank.presentation.dto.response.UserListResponse;
+import com.walkhub.walkhub.domain.rank.presentation.dto.response.UserSearchResponse;
+import com.walkhub.walkhub.global.enums.Scope;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class UserSearchService {
+
+    private final UserRankRepository userRankRepository;
+
+    public UserListResponse execute(String name, Scope scope, String agencyCode, Integer grade, Integer classNum) {
+        List<UserRank> userRankList;
+
+        if (scope == Scope.CLS) {
+            userRankList = userRankRepository.findTop100ByNameContainsAndAgencyCodeAndClassNumAndGrade(name, agencyCode, grade, classNum);
+        } else {
+            userRankList = userRankRepository.findTop100ByNameContainsAndAgencyCode(name, agencyCode);
+        }
+
+        List<UserSearchResponse> userList = userRankList.stream().map(userRank ->
+                UserSearchResponse.builder()
+                        .accountId(userRank.getAccountId())
+                        .classNum(userRank.getClassNum())
+                        .grade(userRank.getGrade())
+                        .name(userRank.getName())
+                        .profileImageUrl(userRank.getProfileImageUrl())
+                        .walkCount(userRank.getClassNum())
+                        .build()
+        ).collect(Collectors.toList());
+
+        return UserListResponse.builder()
+                .userList(userList).build();
+
+    }
+}
