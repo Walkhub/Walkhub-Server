af5d10d456406f7b06b46ba7e2df7d51fec6b97c
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-19 20:26:56 +0900

    :recycle: :: SaveImageFailException ->  SaveImageFailedException로 변경

--- src/main/java/com/walkhub/walkhub/global/exception/SaveImageFailedException.java
+++ src/main/java/com/walkhub/walkhub/global/exception/SaveImageFailedException.java
@@ -3,12 +3,12 @@ package com.walkhub.walkhub.global.exception;
 import com.walkhub.walkhub.global.error.exception.ErrorCode;
 import com.walkhub.walkhub.global.error.exception.WalkhubException;
 
-public class SaveImageFaildException extends WalkhubException {
+public class SaveImageFailedException extends WalkhubException {
 
     public static final WalkhubException EXCEPTION =
-            new SaveImageFaildException();
+            new SaveImageFailedException();
 
-    private SaveImageFaildException() {
+    private SaveImageFailedException() {
         super(ErrorCode.SAVE_IMAGE_FAIL);
     }
 }
