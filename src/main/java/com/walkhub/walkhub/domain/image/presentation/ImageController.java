6796b3a513052fc77f3e3bbf7db5b3553d5eaeba
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-19 10:04:03 +0900

    :bookmark_tabs: :: ImageController

---
+++ src/main/java/com/walkhub/walkhub/domain/image/presentation/ImageController.java
@@ -0,0 +1,24 @@
+package com.walkhub.walkhub.domain.image.presentation;
+
+import com.walkhub.walkhub.domain.image.presentation.dto.response.ImageUrlResponse;
+import com.walkhub.walkhub.domain.image.service.ImageUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/images")
+@RestController
+public class ImageController {
+
+    private final ImageUploadService imageUploadService;
+
+    @PostMapping
+    public ImageUrlResponse saveImage(List<MultipartFile> files) {
+        return imageUploadService.execute(files);
+    }
+}
