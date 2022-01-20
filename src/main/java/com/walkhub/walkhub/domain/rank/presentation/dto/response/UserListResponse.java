b14ad8dcea005b0c036cbe2e521d468879182275
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-20 15:12:53 +0900

    ⚡️ :: static class UserSearchResponse 추가

--- src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/UserListResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/UserListResponse.java
@@ -10,4 +10,16 @@ import java.util.List;
 public class UserListResponse {
 
     private final List<UserSearchResponse> userList;
+
+    @Getter
+    @Builder
+    public static class UserSearchResponse {
+        private final String accountId;
+        private final String name;
+        private final Integer rank;
+        private final Integer grade;
+        private final Integer classNum;
+        private final String profileImageUrl;
+        private final Integer walkCount;
+    }
 }
