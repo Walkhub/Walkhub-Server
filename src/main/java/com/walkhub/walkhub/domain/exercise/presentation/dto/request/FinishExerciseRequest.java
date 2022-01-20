bd84d90244759d133446567fcf57a4d3b50c1940
김범진 <sdpthf@naver.com>
2022-01-20 16:18:22 +0900

    📑 :: FinishExerciseRequest

---
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/request/FinishExerciseRequest.java
@@ -0,0 +1,23 @@
+package com.walkhub.walkhub.domain.exercise.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class FinishExerciseRequest {
+
+    @NotNull(message = "walk_count는 null, 공백을 허용하지 않습니다.")
+    private Integer walkCount;
+
+    @NotNull(message = "distance는 null, 공백을 허용하지 않습니다.")
+    private Integer distance;
+
+    @NotEmpty(message = "image_url은 공백을 허용하지 않습니다.")
+    private List<String> imageUrl;
+
+}
