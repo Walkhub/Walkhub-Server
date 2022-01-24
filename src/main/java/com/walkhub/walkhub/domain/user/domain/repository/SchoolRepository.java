e760715a26696e4f186dc45d2de74d4223349974
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-24 09:38:59 +0900

    :recycle: :: import 삭제

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/SchoolRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/SchoolRepository.java
@@ -3,7 +3,5 @@ package com.walkhub.walkhub.domain.user.domain.repository;
 import com.walkhub.walkhub.domain.user.domain.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface SchoolRepository extends JpaRepository<School, String> {
 }
