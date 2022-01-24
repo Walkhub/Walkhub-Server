1c2a1bff2b2baa02496f0339d9d420af6f49953f
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-24 09:37:08 +0900

    :coffin: :: findByName 삭제

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/SchoolRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/SchoolRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SchoolRepository extends JpaRepository<School, String> {
-    Optional<School> findByName(String name);
 }
