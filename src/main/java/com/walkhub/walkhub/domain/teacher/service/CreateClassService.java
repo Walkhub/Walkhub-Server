29b3ff67fd91cecc3c81edfa40aa014fcc57f53a
김범진 <sdpthf@naver.com>
2022-01-24 16:26:26 +0900

    ♻ :: DataIntegrityViolationException 예외 잡기

--- src/main/java/com/walkhub/walkhub/domain/teacher/service/CreateClassService.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/service/CreateClassService.java
@@ -1,16 +1,16 @@
 package com.walkhub.walkhub.domain.teacher.service;
 
+import com.walkhub.walkhub.domain.teacher.exception.AlreadyCreatedException;
+import com.walkhub.walkhub.domain.teacher.presentation.dto.request.CreateClassRequest;
+import com.walkhub.walkhub.domain.teacher.presentation.dto.response.CreateClassResponse;
 import com.walkhub.walkhub.domain.user.domain.Group;
-import com.walkhub.walkhub.domain.user.domain.GroupId;
 import com.walkhub.walkhub.domain.user.domain.School;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.domain.repository.GroupRepository;
-import com.walkhub.walkhub.domain.teacher.exception.AlreadyCreatedException;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
-import com.walkhub.walkhub.domain.teacher.presentation.dto.request.CreateClassRequest;
-import com.walkhub.walkhub.domain.teacher.presentation.dto.response.CreateClassResponse;
 import com.walkhub.walkhub.global.utils.code.RandomCodeUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,24 +25,23 @@ public class CreateClassService {
     public CreateClassResponse execute(CreateClassRequest request) {
         User user = userFacade.getCurrentUser();
         School userSchool = user.getSchool();
-        String agencyCode = userSchool.getAgencyCode();
         Integer grade = request.getGrade();
         Integer classNum = request.getClassNum();
+        String classCode = RandomCodeUtil.make();
 
-        if (groupRepository.findById(new GroupId(grade, classNum, agencyCode)).isPresent()) {
+        try {
+            Group group = Group.builder()
+                    .grade(grade)
+                    .classNum(classNum)
+                    .school(userSchool)
+                    .classCode(classCode)
+                    .build();
+
+            groupRepository.save(group);
+            user.setGroup(group);
+        } catch (DataIntegrityViolationException e) {
             throw AlreadyCreatedException.EXCEPTION;
         }
-
-        String classCode = RandomCodeUtil.make();
-        Group group = Group.builder()
-                .grade(grade)
-                .classNum(classNum)
-                .school(userSchool)
-                .classCode(classCode)
-                .build();
-
-        groupRepository.save(group);
-        user.setGroup(group);
 
         return new CreateClassResponse(classCode);
     }
