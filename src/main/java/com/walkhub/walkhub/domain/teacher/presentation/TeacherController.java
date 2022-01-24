6beafc22ed3433e003a92cc3256bb508d3f76546
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-24 22:12:56 +0900

    ⚡️ :: verificationCode api 추가

--- src/main/java/com/walkhub/walkhub/domain/teacher/presentation/TeacherController.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/presentation/TeacherController.java
@@ -2,9 +2,12 @@ package com.walkhub.walkhub.domain.teacher.presentation;
 
 import com.walkhub.walkhub.domain.teacher.presentation.dto.request.CreateClassRequest;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.response.CreateClassResponse;
+import com.walkhub.walkhub.domain.teacher.presentation.dto.response.VerificationCodeResponse;
 import com.walkhub.walkhub.domain.teacher.service.CreateClassService;
+import com.walkhub.walkhub.domain.teacher.service.VerificationCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +22,17 @@ import javax.validation.Valid;
 public class TeacherController {
 
     private final CreateClassService createClassService;
+    private final VerificationCodeService verificationCodeService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/classes")
     public CreateClassResponse createClass(@RequestBody @Valid CreateClassRequest request) {
         return createClassService.execute(request);
+    }
+
+    @GetMapping("/verification")
+    public VerificationCodeResponse verificationCode() {
+        return verificationCodeService.execute();
     }
 
 }
