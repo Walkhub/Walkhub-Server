7e05d41577015a27570cb7f2824df9901d274c10
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-25 15:19:22 +0900

    :bookmark_tabs: :: SchoolLogoSettingService

---
+++ src/main/java/com/walkhub/walkhub/domain/school/service/SchoolLogoSettingService.java
@@ -0,0 +1,34 @@
+package com.walkhub.walkhub.domain.school.service;
+
+import com.walkhub.walkhub.domain.school.exception.AgencyCodeNotMatchException;
+import com.walkhub.walkhub.domain.school.presentation.dto.request.SchoolLogoRequest;
+import com.walkhub.walkhub.domain.user.domain.School;
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.domain.user.domain.repository.SchoolRepository;
+import com.walkhub.walkhub.domain.user.exception.SchoolNotFoundException;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class SchoolLogoSettingService {
+
+    private final UserFacade userFacade;
+    private final SchoolRepository schoolRepository;
+
+    @Transactional
+    public void execute(String agencyCode, SchoolLogoRequest request) {
+        User user = userFacade.getCurrentUser();
+
+        School school = schoolRepository.findById(agencyCode)
+                .orElseThrow(() -> SchoolNotFoundException.EXCEPTION);
+
+        if (!user.getSchool().getAgencyCode().equals(school.getAgencyCode())) {
+            throw AgencyCodeNotMatchException.EXCEPTION;
+        }
+
+        school.setLogoImage(request.getImageUrl());
+    }
+}
