3e535220085222e9b5bc3762237210c7980286ad
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-25 15:32:22 +0900

    :recycle: :: @NotBlank 추가

--- src/main/java/com/walkhub/walkhub/domain/school/presentation/dto/request/SchoolLogoRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/school/presentation/dto/request/SchoolLogoRequest.java
@@ -3,9 +3,12 @@ package com.walkhub.walkhub.domain.school.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class SchoolLogoRequest {
 
+    @NotBlank(message = "image_url은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
     private String imageUrl;
 }
