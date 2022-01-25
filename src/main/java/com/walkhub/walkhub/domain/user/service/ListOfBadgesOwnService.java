4f9b2a031769929a7413035c0b8d9a805c6232fa
lyutvs <lyutvs@gmail.com>
2022-01-25 11:06:50 +0900

    ⚡️:: ListOfBadgesOwnService

--- src/main/java/com/walkhub/walkhub/domain/user/service/ListOfBadgesOwnService.java
+++ src/main/java/com/walkhub/walkhub/domain/user/service/ListOfBadgesOwnService.java
@@ -2,39 +2,29 @@ package com.walkhub.walkhub.domain.user.service;
 
 import com.walkhub.walkhub.domain.user.domain.Badge;
 import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.domain.user.domain.repository.BadgeRepository;
-import com.walkhub.walkhub.domain.user.domain.repository.UserRepository;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.domain.user.presentation.dto.response.ListOfBadgesOwnResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
 @Service
 public class ListOfBadgesOwnService {
 
-    private final BadgeRepository badgeRepository;
     private final UserFacade userFacade;
 
+    @Transactional(readOnly = true)
+    public ListOfBadgesOwnResponse execute(Long userId) {
 
-    public ListOfBadgesOwnResponse execute(Badge badge) {
-        User user = userFacade.getCurrentUser();
-        List<Badge> badges = badgeRepository.findBadgesById(badge).stream()
-                .map(this::getBadge)
-                .collect(toList());
-
-        return new ListOfBadgesOwnResponse(user, badges);
-
-    }
-
-    private ListOfBadgesOwnResponse getBadge(Badge badge) {
+        User user = userFacade.getUserById(userId);
+        Badge badge = user.getBadge();
         return ListOfBadgesOwnResponse.builder()
-                .id(badge.getId())
-                .image(badge.getImage())
-                .name(badge.getName())
+                .name(user.getName())
+                .image(user.getProfileImageUrl())
                 .build();
     }
+
 }
+
