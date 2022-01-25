08c8c4b7d9ac17ab48d8318c39ec5f1445f5abad
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-25 15:20:56 +0900

    ⚡️ :: AGENCY_CODE_NOT_MATCH ErrorCode 추가

--- src/main/java/com/walkhub/walkhub/global/error/exception/ErrorCode.java
+++ src/main/java/com/walkhub/walkhub/global/error/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
 
     PASSWORD_NOT_MATCH(400, "AUTH-400-1", "Password Not Match"),
     SAVE_IMAGE_FAILED(400, "COMMON-400-1", "Save Image Failed"),
+    AGENCY_CODE_NOT_MATCH(400, "SCHOOL-400-1", "AgencyCode Not Match"),
 
     EXPIRED_JWT(401, "COMMON-401-1", "Expired Jwt"),
     INVALID_JWT(401, "COMMON-401-2", "Invalid Jwt"),
