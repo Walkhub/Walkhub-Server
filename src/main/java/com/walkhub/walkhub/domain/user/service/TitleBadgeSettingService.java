748f626e0e2bfd9cfcc290445b78bb0a99b312ea
lyutvs <lyutvs@gmail.com>
2022-01-25 12:27:52 +0900

    ⚡️ :: TitleBadgeSettingService

--- src/main/java/com/walkhub/walkhub/domain/user/service/TitleBadgeSettingService.java
+++ src/main/java/com/walkhub/walkhub/domain/user/service/TitleBadgeSettingService.java
@@ -3,7 +3,6 @@ package com.walkhub.walkhub.domain.user.service;
 import com.walkhub.walkhub.domain.user.domain.Badge;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.domain.repository.BadgeRepository;
-import com.walkhub.walkhub.domain.user.domain.repository.UserRepository;
 import com.walkhub.walkhub.domain.user.exception.BadgeNotFoundException;
 import com.walkhub.walkhub.domain.user.exception.BadgeUnauthorizedException;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
@@ -22,13 +21,13 @@ public class TitleBadgeSettingService {
     private final UserFacade userFacade;
 
     @Transactional
-    public QueryMyPageResponse execute(QueryTitleBadgeRequest request) {
+    public QueryMyPageResponse execute(Long badgeId) {
         User user = userFacade.getCurrentUser();
-        Badge titleBadge = badgeRepository.findById(request.getBadgeId())
+        Badge titleBadge = badgeRepository.findById(badgeId)
                 .orElseThrow(() -> BadgeNotFoundException.EXCEPTION);
 
 
-        if (!user.getBadge().equals(request.getBadgeId())) {
+        if (!user.getBadge().equals(badgeId)) {
             throw BadgeUnauthorizedException.EXCEPTION;
         }
         return QueryMyPageResponse.builder()
