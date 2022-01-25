f7ff32dbca09236eab8c5089e1c9bbdb54dd9d43
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-25 15:19:47 +0900

    :bookmark_tabs: :: SchoolLogoRequest

---
+++ src/main/java/com/walkhub/walkhub/domain/school/presentation/dto/request/SchoolLogoRequest.java
@@ -0,0 +1,11 @@
+package com.walkhub.walkhub.domain.school.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SchoolLogoRequest {
+
+    private String imageUrl;
+}
