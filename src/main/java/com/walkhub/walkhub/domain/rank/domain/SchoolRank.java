b71fcd4fe903ff1e6a820f868892f2de45bd4c0e
김범진 <sdpthf@naver.com>
2022-01-25 23:26:42 +0900

    📑 :: SchoolRank

---
+++ src/main/java/com/walkhub/walkhub/domain/rank/domain/SchoolRank.java
@@ -0,0 +1,39 @@
+package com.walkhub.walkhub.domain.rank.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class SchoolRank {
+
+    @Id
+    @Column(length = 7)
+    private String agencyCode;
+
+    @Column(length = 20, unique = true, nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TINYINT", unique = true, nullable = false)
+    private Integer rank;
+
+    @Column(nullable = false)
+    private Integer walkCount;
+
+    private String logoImageUrl;
+
+    @Builder
+    public SchoolRank(String agencyCode, String name, Integer rank, Integer walkCount) {
+        this.agencyCode = agencyCode;
+        this.name = name;
+        this.rank = rank;
+        this.walkCount = walkCount;
+    }
+}
