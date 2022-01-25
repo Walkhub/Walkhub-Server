47d1e00af3023081d5e2f0eece65b2542ba9839d
김범진 <sdpthf@naver.com>
2022-01-26 08:45:07 +0900

    ♻ :: element -> static

--- src/main/java/com/walkhub/walkhub/domain/challenge/presentation/dto/response/ChallengeParticipantsListResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presentation/dto/response/ChallengeParticipantsListResponse.java
@@ -1,6 +1,7 @@
 package com.walkhub.walkhub.domain.challenge.presentation.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
@@ -10,5 +11,16 @@ import java.util.List;
 public class ChallengeParticipantsListResponse {
 
     private final List<ChallengeParticipantsElement> challengeParticipantsList;
+
+    @Builder
+    public static class ChallengeParticipantsElement {
+        private final Long id;
+
+        private final String name;
+
+        private final String profileImageUrl;
+
+        private final String schoolName;
+    }
 
 }
