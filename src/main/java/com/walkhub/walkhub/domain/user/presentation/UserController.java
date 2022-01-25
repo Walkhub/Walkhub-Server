66defe4d61c035f4fdd477fd3573de09f971c31f
lyutvs <lyutvs@gmail.com>
2022-01-25 11:07:09 +0900

    ⚡️ :: UserController

--- src/main/java/com/walkhub/walkhub/domain/user/presentation/UserController.java
+++ src/main/java/com/walkhub/walkhub/domain/user/presentation/UserController.java
@@ -5,6 +5,7 @@ import com.walkhub.walkhub.domain.user.presentation.dto.request.InputHealthInfor
 import com.walkhub.walkhub.domain.user.presentation.dto.request.UpdateUserInfoRequest;
 import com.walkhub.walkhub.domain.user.presentation.dto.request.UserAuthCodeRequest;
 import com.walkhub.walkhub.domain.user.presentation.dto.request.UserSignUpRequest;
+import com.walkhub.walkhub.domain.user.presentation.dto.response.ListOfBadgesOwnResponse;
 import com.walkhub.walkhub.domain.user.presentation.dto.response.QueryMyPageResponse;
 import com.walkhub.walkhub.domain.user.presentation.dto.response.QueryUserProfileResponse;
 import com.walkhub.walkhub.domain.user.service.*;
@@ -70,7 +71,7 @@ public class UserController {
     }
 
     @GetMapping("/{user-id}/badges")
-    public void getBadgeList(@PathVariable("user-id") Long userId) {
-        listOfBadgesOwnService.execute(userId);
+    public ListOfBadgesOwnResponse getBadgeList(@PathVariable("user-id") Long userId) {
+        return listOfBadgesOwnService.execute(userId);
     }
 }
