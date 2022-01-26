78ac1cacb35d814fe67a731f98cba9169e8268a6
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-26 21:05:37 +0900

    ⚡️ :: RefreshTokenController 추가

--- src/main/java/com/walkhub/walkhub/domain/auth/presentation/AuthController.java
+++ src/main/java/com/walkhub/walkhub/domain/auth/presentation/AuthController.java
@@ -1,11 +1,15 @@
 package com.walkhub.walkhub.domain.auth.presentation;
 
 import com.walkhub.walkhub.domain.auth.presentation.dto.request.SignInRequest;
+import com.walkhub.walkhub.domain.auth.presentation.dto.response.UserAccessTokenResponse;
 import com.walkhub.walkhub.domain.auth.presentation.dto.response.UserTokenResponse;
+import com.walkhub.walkhub.domain.auth.service.TokenRefreshService;
 import com.walkhub.walkhub.domain.auth.service.UserSignInService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -15,9 +19,15 @@ import javax.validation.Valid;
 public class AuthController {
 
     private final UserSignInService userSignInService;
+    private final TokenRefreshService tokenRefreshService;
 
     @PostMapping
     public UserTokenResponse userSignIn(@RequestBody @Valid SignInRequest request) {
         return userSignInService.execute(request);
+    }
+
+    @PutMapping
+    public UserAccessTokenResponse userTokenRefresh(@RequestHeader("Refresh-Token") String refreshToken) {
+        return tokenRefreshService.execute(refreshToken);
     }
 }
