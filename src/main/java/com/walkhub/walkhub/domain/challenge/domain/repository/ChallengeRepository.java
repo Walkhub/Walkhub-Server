10171aa37ea96892a8d74dac0572041c5cea6ecc
이준서 <ljchan71@gmail.com>
2022-01-26 09:32:18 +0900

    ⚡️ :: findAllByScope

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
@@ -1,7 +1,18 @@
 package com.walkhub.walkhub.domain.challenge.domain.repository;
 
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
+import com.walkhub.walkhub.domain.user.domain.Group;
+import com.walkhub.walkhub.domain.user.domain.School;
+import com.walkhub.walkhub.domain.user.domain.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ChallengeRepository extends CrudRepository<Challenge, Long> {
+public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+
+	@Query("select c from Challenge c where c.scope = com.walkhub.walkhub.global.enums.Scope.ALL or "
+		+ "c.user.school = :school and (c.scope = com.walkhub.walkhub.global.enums.Scope.SCH or "
+		+ "(c.scope = com.walkhub.walkhub.global.enums.Scope.CLS and c.user.group = :group))")
+	List<Challenge> findAllByScope((School school, Group group);
 }
