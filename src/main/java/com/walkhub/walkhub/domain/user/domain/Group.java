a76903e55a618be43dd3b6eb9a6ad8e93415d333
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-26 12:32:44 +0900

    :recycle: :: User Entity & User Repository 파일 위치 변경

--- src/main/java/com/walkhub/walkhub/domain/user/domain/Group.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/Group.java
@@ -1,11 +1,18 @@
 package com.walkhub.walkhub.domain.user.domain;
 
+import com.walkhub.walkhub.domain.school.domain.School;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
--- src/main/java/com/walkhub/walkhub/domain/user/service/UpdateSchoolInfoService.java
+++ src/main/java/com/walkhub/walkhub/domain/user/service/UpdateSchoolInfoService.java
@@ -1,8 +1,8 @@
 package com.walkhub.walkhub.domain.user.service;
 
-import com.walkhub.walkhub.domain.user.domain.School;
+import com.walkhub.walkhub.domain.school.domain.School;
+import com.walkhub.walkhub.domain.school.domain.repository.SchoolRepository;
 import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.domain.user.domain.repository.SchoolRepository;
 import com.walkhub.walkhub.domain.user.exception.SchoolNotFoundException;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.domain.user.presentation.dto.request.UpdateSchoolInfoRequest;
