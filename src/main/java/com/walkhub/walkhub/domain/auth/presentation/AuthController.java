0adbe7177af6b61eb77321e14a4a9db532a98e10
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-26 21:11:55 +0900

    ⚡️ :: endpoint 수정

--- src/main/java/com/walkhub/walkhub/domain/auth/presentation/AuthController.java
+++ src/main/java/com/walkhub/walkhub/domain/auth/presentation/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
-@RestController("/users/auth")
+@RestController("/users/token")
 public class AuthController {
 
     private final UserSignInService userSignInService;
