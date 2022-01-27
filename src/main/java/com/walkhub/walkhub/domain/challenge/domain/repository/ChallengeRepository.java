7c1194b2b1875aa8c5f2bf1c9ded46af621f6c11
이준서 <ljchan71@gmail.com>
2022-01-27 22:52:02 +0900

    🐛 :: 패키지 주소 오류

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
@@ -3,7 +3,6 @@ package com.walkhub.walkhub.domain.challenge.domain.repository;
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.school.domain.School;
 import com.walkhub.walkhub.domain.user.domain.Group;
-import com.walkhub.walkhub.domain.user.domain.School;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
