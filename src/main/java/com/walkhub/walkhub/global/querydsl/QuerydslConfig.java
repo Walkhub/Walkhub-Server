b0539c3c35c8f0cf1db4519239a653f9a4a94908
parksangwoo <b01056721617@gmail.com>
2022-01-28 20:59:16 +0900

    ⚡️ :: 주입 방식으로 변경

--- src/main/java/com/walkhub/walkhub/global/querydsl/QuerydslConfig.java
+++ src/main/java/com/walkhub/walkhub/global/querydsl/QuerydslConfig.java
@@ -1,16 +1,16 @@
 package com.walkhub.walkhub.global.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Configuration
+@RequiredArgsConstructor
 public class QuerydslConfig {
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
