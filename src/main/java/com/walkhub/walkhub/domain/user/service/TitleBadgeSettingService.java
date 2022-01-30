540d9e625bc4a6b23317b6681c4b7f8bf85be4d3
lyutvs <lyutvs@gmail.com>
2022-01-31 00:28:02 +0900

    ⚡️ :: TitleBadgeSettingService

--- src/main/java/com/walkhub/walkhub/domain/user/service/TitleBadgeSettingService.java
+++ src/main/java/com/walkhub/walkhub/domain/user/service/TitleBadgeSettingService.java
@@ -2,12 +2,9 @@ package com.walkhub.walkhub.domain.user.service;
 
 import com.walkhub.walkhub.domain.user.domain.Badge;
 import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.domain.user.domain.repository.BadgeRepository;
-import com.walkhub.walkhub.domain.user.exception.BadgeNotFoundException;
-import com.walkhub.walkhub.domain.user.exception.BadgeUnauthorizedException;
+import com.walkhub.walkhub.domain.user.facade.BadgeFacade;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
-import com.walkhub.walkhub.domain.user.presentation.dto.request.QueryTitleBadgeRequest;
-import com.walkhub.walkhub.domain.user.presentation.dto.response.QueryMyPageResponse;
+import com.walkhub.walkhub.domain.user.presentation.dto.request.UpdateTitleBadgeRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,25 +14,15 @@ import javax.transaction.Transactional;
 @Service
 public class TitleBadgeSettingService {
 
-    private final BadgeRepository badgeRepository;
     private final UserFacade userFacade;
+    private final BadgeFacade badgeFacade;
 
     @Transactional
-    public QueryMyPageResponse execute(Long badgeId) {
+    public void execute(Long badgeId, UpdateTitleBadgeRequest request) {
         User user = userFacade.getCurrentUser();
-        Badge titleBadge = badgeRepository.findById(badgeId)
-                .orElseThrow(() -> BadgeNotFoundException.EXCEPTION);
+        badgeFacade.getBadgeById(badgeId);
 
+        user.setBadge(new Badge(request.getName(), request.getImageUrl()));
 
-        if (!user.getBadge().equals(badgeId)) {
-            throw BadgeUnauthorizedException.EXCEPTION;
-        }
-        return QueryMyPageResponse.builder()
-                .titleBadge(QueryMyPageResponse.TitleBadge.builder()
-                        .id(titleBadge.getId())
-                        .name(titleBadge.getName())
-                        .image(titleBadge.getImage())
-                        .build())
-                .build();
     }
 }
