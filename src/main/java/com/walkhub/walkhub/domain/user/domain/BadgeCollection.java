6ed2d6b06f9cc611bfbc5fc59d3db1bf7bd95f91
lyutvs <lyutvs@gmail.com>
2022-01-31 00:27:27 +0900

    ⚡️ : : BadgeController

--- src/main/java/com/walkhub/walkhub/domain/user/domain/BadgeCollection.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/BadgeCollection.java
@@ -1,17 +1,11 @@
 package com.walkhub.walkhub.domain.user.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
