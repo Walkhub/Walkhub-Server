ef3c8c3bbd41d895f56e31d1bea5a76bca3d581e
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-03 22:44:08 +0900

    :bookmark_tabs: :: SocketExceptionListener

--- src/main/java/com/walkhub/walkhub/global/websocket/exception/SocketExceptionListener.java
+++ src/main/java/com/walkhub/walkhub/global/websocket/exception/SocketExceptionListener.java
@@ -1,2 +1,55 @@
-package com.walkhub.walkhub.global.websocket.exception;public class SocketExceptionListener {
+package com.walkhub.walkhub.global.websocket.exception;
+
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.listener.ExceptionListener;
+import com.walkhub.walkhub.global.error.ErrorResponse;
+import com.walkhub.walkhub.global.error.exception.ErrorCode;
+import com.walkhub.walkhub.global.error.exception.WalkhubException;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.List;
+
+public class SocketExceptionListener implements ExceptionListener {
+    @Override
+    public void onEventException(Exception e, List<Object> args, SocketIOClient client) {
+        runExceptionHandling(e, client);
+    }
+
+    @Override
+    public void onDisconnectException(Exception e, SocketIOClient client) {
+        runExceptionHandling(e, client);
+    }
+
+    @Override
+    public void onConnectException(Exception e, SocketIOClient client) {
+        runExceptionHandling(e, client);
+    }
+
+    @Override
+    public void onPingException(Exception e, SocketIOClient client) {
+        runExceptionHandling(e, client);
+    }
+
+    @Override
+    public boolean exceptionCaught(ChannelHandlerContext ctx, Throwable e) {
+        return false;
+    }
+
+    private void runExceptionHandling(Exception e, SocketIOClient client) {
+        final ErrorCode errorCode;
+
+        if (e.getCause() instanceof WalkhubException) {
+            errorCode = ((WalkhubException) e.getCause()).getErrorCode();
+        } else {
+            errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        }
+        ErrorResponse message = new ErrorResponse(
+                errorCode.getStatus(),
+                errorCode.getCode(),
+                errorCode.getMessage()
+        );
+
+        client.sendEvent("error", message);
+    }
+
 }
