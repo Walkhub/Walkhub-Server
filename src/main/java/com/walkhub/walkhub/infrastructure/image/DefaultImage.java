424f8c9c8cea092d1c9bd0172a194bdfb8e7c281
이준서 <ljchan71@gmail.com>
2022-02-04 09:51:47 +0900

    📑 :: DefaultImage

---
+++ src/main/java/com/walkhub/walkhub/infrastructure/image/DefaultImage.java
@@ -0,0 +1,8 @@
+package com.walkhub.walkhub.infrastructure.image;
+
+public class DefaultImage {
+	public static final String USER_PROFILE_IMAGE = "";
+	public static final String CHALLENGE_IMAGE = "";
+	public static final String SCHOOL_LOGO_IMAGE = "";
+	public static final String BADGE_IMAGE = "";
+}
