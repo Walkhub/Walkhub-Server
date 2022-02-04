c1b85231125e399d095de4acaf8aa62102caaf91
김범진 <sdpthf@naver.com>
2022-02-04 09:59:27 +0900

    ⚡ :: SocketRunner 빈 등록 순서 지정

--- src/main/java/com/walkhub/walkhub/global/websocket/SocketRunner.java
+++ src/main/java/com/walkhub/walkhub/global/websocket/SocketRunner.java
@@ -3,9 +3,11 @@ package com.walkhub.walkhub.global.websocket;
 import com.corundumstudio.socketio.SocketIOServer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
+@Order(1)
 @Component
 public class SocketRunner implements CommandLineRunner {
 
