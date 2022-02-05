8354b06eb4000c0c897317fc8a043009914c6cd0
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-06 02:03:30 +0900

    ⚡️ :: HashMap 사용하도록 변경 & 자기자신 응원 불가로 if 추가

--- src/main/java/com/walkhub/walkhub/domain/cheering/service/CheeringService.java
+++ src/main/java/com/walkhub/walkhub/domain/cheering/service/CheeringService.java
@@ -1,30 +1,36 @@
 package com.walkhub.walkhub.domain.cheering.service;
 
 import com.corundumstudio.socketio.SocketIOClient;
-import com.corundumstudio.socketio.SocketIOServer;
+import com.walkhub.walkhub.domain.cheering.exception.CanNotCheerMyselfException;
 import com.walkhub.walkhub.domain.cheering.exception.SocketClientNotFoundException;
 import com.walkhub.walkhub.domain.cheering.presentation.dto.request.CheerRequest;
 import com.walkhub.walkhub.domain.cheering.presentation.dto.response.CheeringMessage;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
+import com.walkhub.walkhub.global.websocket.connect.WebSocketJwtHandler;
 import com.walkhub.walkhub.global.websocket.property.ClientProperty;
 import com.walkhub.walkhub.global.websocket.property.SocketProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class CheeringService {
 
-    private final SocketIOServer socketIOServer;
     private final UserFacade userFacade;
 
     public void execute(SocketIOClient socketIOClient, CheerRequest request) {
-        User user = userFacade.getUserById(socketIOClient.get(ClientProperty.USER_KEY));
+        User user = userFacade.getUserByAccountId(socketIOClient.get(ClientProperty.USER_KEY));
+        if (user.getId().equals(request.getUserId())) {
+            throw CanNotCheerMyselfException.EXCEPTION;
+        }
 
-        SocketIOClient clientToSend = socketIOServer.getAllClients().stream()
-                .filter(client -> client.get(ClientProperty.USER_KEY).equals(request.getUserId()))
-                .findFirst()
+        User targetUser = userFacade.getUserById(request.getUserId());
+
+        SocketIOClient clientToSend = Optional.of(WebSocketJwtHandler.socketIOClientMap
+                        .get(targetUser.getAccountId()))
                 .orElseThrow(() -> SocketClientNotFoundException.EXCEPTION);
 
         CheeringMessage message = CheeringMessage.builder()
