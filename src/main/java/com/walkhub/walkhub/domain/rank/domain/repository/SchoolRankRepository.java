21347c2ac266c7086b56dfed2d02f9d72017091d
parksangwoo <b01056721617@gmail.com>
2022-02-05 21:04:36 +0900

    ♻️ :: SchoolRankRepository PK 타입 변경

--- src/main/java/com/walkhub/walkhub/domain/rank/domain/repository/SchoolRankRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/domain/repository/SchoolRankRepository.java
@@ -1,7 +1,8 @@
 package com.walkhub.walkhub.domain.rank.domain.repository;
 
 import com.walkhub.walkhub.domain.rank.domain.SchoolRank;
+import com.walkhub.walkhub.domain.rank.domain.SchoolRankId;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SchoolRankRepository extends CrudRepository<SchoolRank, String> {
+public interface SchoolRankRepository extends CrudRepository<SchoolRank, SchoolRankId> {
 }
