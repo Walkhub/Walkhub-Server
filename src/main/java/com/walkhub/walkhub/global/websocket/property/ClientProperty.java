90ff791c76a842c0543e08da3fdd351a1afe43fc
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-05 20:36:31 +0900

    :recycle: :: user key -> user_key로 변경

--- src/main/java/com/walkhub/walkhub/global/websocket/property/ClientProperty.java
+++ src/main/java/com/walkhub/walkhub/global/websocket/property/ClientProperty.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientProperty {
 
-    public static final String USER_KEY = "user key";
+    public static final String USER_KEY = "user_key";
 
 }
