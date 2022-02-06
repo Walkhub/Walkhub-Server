f8ab9033d6e4fab19913ed37cef157e923c80692
jhhong0509 <jhhong0509@naver.com>
2022-02-06 23:38:22 +0900

    ⚡️ :: (#72) 사용자의 ExerciseAnalysis 조회 추가

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseAnalysisRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseAnalysisRepository.java
@@ -1,7 +1,12 @@
 package com.walkhub.walkhub.domain.exercise.domain.repository;
 
 import com.walkhub.walkhub.domain.exercise.domain.ExerciseAnalysis;
+import com.walkhub.walkhub.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface ExerciseAnalysisRepository extends CrudRepository<ExerciseAnalysis, Long> {
+    Optional<ExerciseAnalysis> findByUserAndDate(User user, LocalDate date);
 }
