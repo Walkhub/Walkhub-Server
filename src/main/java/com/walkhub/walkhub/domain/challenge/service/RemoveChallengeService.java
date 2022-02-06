e5c32431ffdb16173cac79f61bb9e0ef0ceb561f
lyutvs <lyutvs@gmail.com>
2022-02-06 22:25:42 +0900

    ♻️:: InvalidRoleException

--- src/main/java/com/walkhub/walkhub/domain/challenge/service/RemoveChallengeService.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/RemoveChallengeService.java
@@ -2,12 +2,9 @@ package com.walkhub.walkhub.domain.challenge.service;
 
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeRepository;
-import com.walkhub.walkhub.domain.challenge.exception.NotMySchoolException;
 import com.walkhub.walkhub.domain.challenge.facade.ChallengeFacade;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
-import com.walkhub.walkhub.global.enums.Authority;
-import com.walkhub.walkhub.global.exception.InvalidRoleException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,14 +21,6 @@ public class RemoveChallengeService {
     public void execute(Long id) {
         User user = userFacade.getCurrentUser();
         Challenge challenge = challengeFacade.getById(id);
-
-        if (!Authority.TCHR.equals(user.getAuthority())) {
-            throw InvalidRoleException.EXCEPTION;
-        }
-
-        if (!challenge.isMySchool(user.getRealSchoolAgencyCode())){
-            throw NotMySchoolException.EXCEPTION;
-        }
 
         challengeRepository.delete(challenge);
     }
