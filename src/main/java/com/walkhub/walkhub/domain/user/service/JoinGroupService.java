4049d02f6cf2000eaafd0aaa8846a7d560a3d748
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-07 16:24:36 +0900

    :recycle: :: 로직 수정

--- src/main/java/com/walkhub/walkhub/domain/user/service/JoinGroupService.java
+++ src/main/java/com/walkhub/walkhub/domain/user/service/JoinGroupService.java
@@ -3,7 +3,6 @@ package com.walkhub.walkhub.domain.user.service;
 import com.walkhub.walkhub.domain.school.exception.AgencyCodeNotMatchException;
 import com.walkhub.walkhub.domain.user.domain.Group;
 import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.domain.user.exception.AlreadyJoinedException;
 import com.walkhub.walkhub.domain.user.exception.InvalidClassCodeException;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.domain.user.presentation.dto.request.JoinGroupRequest;
@@ -21,13 +20,9 @@ public class JoinGroupService {
     public void execute(String agencyCode, Integer grade, Integer classNum, JoinGroupRequest request) {
         User user = userFacade.getCurrentUser();
 
-        if (user.getGroup() != null) {
-            throw AlreadyJoinedException.EXCEPTION;
-        }
-
         Group group = userFacade.getGroupByGroupId(agencyCode, grade, classNum);
 
-        if (!request.getClassCode().equals(group.getClassCode())) {
+        if (!group.getClassCode().equals(request.getClassCode())) {
             throw InvalidClassCodeException.EXCEPTION;
         }
 
