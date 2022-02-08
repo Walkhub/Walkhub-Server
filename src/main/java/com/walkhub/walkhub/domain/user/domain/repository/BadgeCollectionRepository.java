c0371da90a2b3cfeb39dbd31fa73bc390ee31f3b
lyutvs <lyutvs@gmail.com>
2022-02-08 09:57:01 +0900

    📑:: BadgeCollectionRepository 생성

---
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/BadgeCollectionRepository.java
@@ -0,0 +1,13 @@
+package com.walkhub.walkhub.domain.user.domain.repository;
+
+import com.walkhub.walkhub.domain.user.domain.Badge;
+import com.walkhub.walkhub.domain.user.domain.BadgeCollection;
+import com.walkhub.walkhub.domain.user.domain.BadgeCollectionId;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface BadgeCollectionRepository extends CrudRepository<BadgeCollection, BadgeCollectionId> {
+
+    Optional<Badge> findBadgeCollectionByBadge(Long badgeId);
+}
