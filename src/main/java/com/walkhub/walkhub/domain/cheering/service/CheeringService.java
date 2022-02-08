df28a2c7be072d614d81e4d4d6ae3bea834005a8
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-08 11:28:56 +0900

    ⚡️ :: cherringCount 로직 추가

--- src/main/java/com/walkhub/walkhub/domain/cheering/service/CheeringService.java
+++ src/main/java/com/walkhub/walkhub/domain/cheering/service/CheeringService.java
@@ -3,8 +3,11 @@ package com.walkhub.walkhub.domain.cheering.service;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.walkhub.walkhub.domain.cheering.exception.CanNotCheerMyselfException;
 import com.walkhub.walkhub.domain.cheering.exception.SocketClientNotFoundException;
+import com.walkhub.walkhub.domain.cheering.exception.UserNotExercisingException;
 import com.walkhub.walkhub.domain.cheering.presentation.dto.request.CheerRequest;
 import com.walkhub.walkhub.domain.cheering.presentation.dto.response.CheeringMessage;
+import com.walkhub.walkhub.domain.exercise.domain.Exercise;
+import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseRepository;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.global.websocket.connect.WebSocketJwtHandler;
@@ -20,6 +23,7 @@ import java.util.Optional;
 public class CheeringService {
 
     private final UserFacade userFacade;
+    private final ExerciseRepository exerciseRepository;
 
     public void execute(SocketIOClient socketIOClient, CheerRequest request) {
         User user = userFacade.getUserByAccountId(socketIOClient.get(ClientProperty.USER_KEY));
@@ -37,6 +41,11 @@ public class CheeringService {
                 .userName(user.getName())
                 .userId(user.getId())
                 .build();
+
+        Exercise exercise = exerciseRepository.findByIsExercisingTrueAndUser(targetUser)
+                .orElseThrow(() -> UserNotExercisingException.EXCEPTION);
+
+        exercise.addCheeringCount();
 
         clientToSend.sendEvent(SocketProperty.NEW_CHEERING, message);
 
