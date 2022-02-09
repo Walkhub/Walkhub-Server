8fe6a257ab95ce9bd287d1e2a95d37389c7338fb
이준서 <ljchan71@gmail.com>
2022-02-10 01:20:03 +0900

    ⚡️ :: 마지막 위치 조회

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/LocationRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/LocationRepository.java
@@ -1,8 +1,12 @@
 package com.walkhub.walkhub.domain.exercise.domain.repository;
 
+import com.walkhub.walkhub.domain.exercise.domain.Exercise;
 import com.walkhub.walkhub.domain.exercise.domain.Location;
 import com.walkhub.walkhub.domain.exercise.domain.LocationId;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LocationRepository extends CrudRepository<Location, LocationId> {
+	Location findTop1ByExerciseOrderByOrderDesc(Exercise exercise);
 }
