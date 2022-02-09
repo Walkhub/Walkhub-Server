0f1dd794fb2fdd3261dfe970803e204c7aebd456
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-10 00:53:05 +0900

    ⚡️ :: Page<School> findAllBy(Pageable pageable) 추가

--- src/main/java/com/walkhub/walkhub/domain/school/domain/repository/SchoolRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/school/domain/repository/SchoolRepository.java
@@ -1,10 +1,13 @@
 package com.walkhub.walkhub.domain.school.domain.repository;
 
 import com.walkhub.walkhub.domain.school.domain.School;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface SchoolRepository extends JpaRepository<School, Long> {
     List<School> findAllByNameContaining(String name);
+    Page<School> findAllBy(Pageable pageable);
 }
