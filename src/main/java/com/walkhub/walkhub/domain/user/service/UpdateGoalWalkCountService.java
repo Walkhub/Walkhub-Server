b8717e014549b407dd12fc0834c056ee07f673c8
lyutvs <lyutvs@gmail.com>
2022-02-10 00:31:14 +0900

    📑:: UpdateGoalWalkCountService

---
+++ src/main/java/com/walkhub/walkhub/domain/user/service/UpdateGoalWalkCountService.java
@@ -0,0 +1,20 @@
+package com.walkhub.walkhub.domain.user.service;
+
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
+import com.walkhub.walkhub.domain.user.presentation.dto.request.UpdateGoalWalkCountRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateGoalWalkCountService {
+
+    private final UserFacade userFacade;
+
+    public void execute(UpdateGoalWalkCountRequest request) {
+        User user = userFacade.getCurrentUser();
+
+        user.updateWalkCount(request.getWalkCount());
+    }
+}
