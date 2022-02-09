031b617f7a4541355c19b9ea30f491370adc651c
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-10 00:54:12 +0900

    :bookmark_tabs: :: SchoolListResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/su/presentation/dto/response/SchoolListResponse.java
@@ -0,0 +1,25 @@
+package com.walkhub.walkhub.domain.su.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class SchoolListResponse {
+
+    private final List<SchoolResponse> schoolList;
+
+    @Getter
+    @Builder
+    public static class SchoolResponse {
+        private final Long schoolId;
+        private final String name;
+        private final String logoImageUrl;
+        private final Long userCount;
+        private final LocalDateTime createdAt;
+    }
+}
