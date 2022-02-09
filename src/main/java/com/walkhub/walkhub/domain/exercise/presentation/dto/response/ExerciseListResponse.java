bcc9fb1eba41a6a299cc885de66294ba3144dc87
이준서 <ljchan71@gmail.com>
2022-02-10 01:21:32 +0900

    📑 :: ExerciseListResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/response/ExerciseListResponse.java
@@ -0,0 +1,30 @@
+package com.walkhub.walkhub.domain.exercise.presentation.dto.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExerciseListResponse {
+
+	private final List<ExerciseResponse> exerciseList;
+
+	@Getter
+	@Builder
+	public static class ExerciseResponse {
+
+		private final Long exerciseId;
+
+		private final String imageUrl;
+
+		private final LocalDateTime startAt;
+
+		private final BigDecimal latitude;
+
+		private final BigDecimal longitude;
+	}
+}
