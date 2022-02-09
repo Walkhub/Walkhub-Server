4a009114c7e1d662b8bceb9c98587ef54d84afbb
이준서 <ljchan71@gmail.com>
2022-02-09 23:26:00 +0900

    📑 :: CalorieLevel entity

---
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/CalorieLevel.java
@@ -0,0 +1,48 @@
+package com.walkhub.walkhub.domain.user.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class CalorieLevel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	private Integer size;
+
+	@NotNull
+	private String message;
+
+	@NotNull
+	private String foodImageUrl;
+
+	@NotNull
+	@Column(columnDefinition = "TINYINT")
+	private Integer level;
+
+	@NotNull
+	private Integer calorie;
+
+	@Builder
+	public CalorieLevel(Integer size, String message, String foodImageUrl, Integer level,
+		Integer calorie) {
+		this.size = size;
+		this.message = message;
+		this.foodImageUrl = foodImageUrl;
+		this.level = level;
+		this.calorie = calorie;
+	}
+}
