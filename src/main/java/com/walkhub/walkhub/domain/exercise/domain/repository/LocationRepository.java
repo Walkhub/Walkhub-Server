9f570e53033de4ab854fe2b8afc71fe7e19c961d
이준서 <ljchan71@gmail.com>
2022-02-10 01:33:48 +0900

    ⚡️ :: code smell 해결

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/LocationRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/LocationRepository.java
@@ -3,8 +3,6 @@ package com.walkhub.walkhub.domain.exercise.domain.repository;
 import com.walkhub.walkhub.domain.exercise.domain.Exercise;
 import com.walkhub.walkhub.domain.exercise.domain.Location;
 import com.walkhub.walkhub.domain.exercise.domain.LocationId;
-import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LocationRepository extends CrudRepository<Location, LocationId> {
