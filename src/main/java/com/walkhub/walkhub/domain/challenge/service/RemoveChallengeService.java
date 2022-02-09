bf16b70325013ff93972d43d55f40fcaa6f41e26
lyutvs <lyutvs@gmail.com>
2022-02-09 23:25:48 +0900

    ⚡️:: 내꺼인지 체크

--- src/main/java/com/walkhub/walkhub/domain/challenge/service/RemoveChallengeService.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/RemoveChallengeService.java
@@ -3,6 +3,7 @@ package com.walkhub.walkhub.domain.challenge.service;
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeRepository;
 import com.walkhub.walkhub.domain.challenge.facade.ChallengeFacade;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +12,11 @@ import org.springframework.stereotype.Service;
 public class RemoveChallengeService {
 
     private final ChallengeFacade challengeFacade;
+    private final UserFacade userFacade;
     private final ChallengeRepository challengeRepository;
 
     public void execute(Long id) {
+        userFacade.getCurrentUser();
         Challenge challenge = challengeFacade.getByChallengeId(id);
         challengeRepository.delete(challenge);
     }
