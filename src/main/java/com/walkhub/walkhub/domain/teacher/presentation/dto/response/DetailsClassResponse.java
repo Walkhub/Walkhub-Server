711c06cfedb20f9c714604c754733ae01bdb2afd
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-09 23:56:27 +0900

    :bookmark_tabs: :: DetailsClassResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/teacher/presentation/dto/response/DetailsClassResponse.java
@@ -0,0 +1,25 @@
+package com.walkhub.walkhub.domain.teacher.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class DetailsClassResponse {
+
+    private final String classCode;
+    private final List<UserListResponse> userList;
+
+
+    @Getter
+    @Builder
+    public static class UserListResponse {
+        private Long userId;
+        private String name;
+        private String profileImageUrl;
+        private Integer walkCount;
+    }
+}
