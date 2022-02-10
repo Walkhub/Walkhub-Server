8a8e68f4827a862412dde7a7f0bf058c7cc89f4c
이준서 <ljchan71@gmail.com>
2022-02-10 09:33:33 +0900

    ⚡️ :: ChallengeRepository

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
@@ -12,9 +12,9 @@ import org.springframework.data.repository.query.Param;
 public interface ChallengeRepository extends CrudRepository<Challenge, Long> {
 
 	@Query("select c from Challenge c where c.userScope = com.walkhub.walkhub.global.enums.UserScope.ALL or "
-		+ "(c.user.school = :school and c.userScope = com.walkhub.walkhub.global.enums.UserScope.SCHOOL or "
-		+ "(c.user.group.grade = :grade and c.userScope = com.walkhub.walkhub.global.enums.UserScope.GRADE or "
-		+ "(c.user.group = :group and c.userScope = com.walkhub.walkhub.global.enums.UserScope.CLASS)))")
+		+ "c.user.school = :school and (c.userScope = com.walkhub.walkhub.global.enums.UserScope.SCHOOL or "
+		+ "c.user.group.grade = :grade and (c.userScope = com.walkhub.walkhub.global.enums.UserScope.GRADE or "
+		+ "c.user.group = :group and (c.userScope = com.walkhub.walkhub.global.enums.UserScope.CLASS)))")
 	List<Challenge> findAllByScope(@Param("school") School school, @Param("grade") Integer grade, @Param("group") Group group);
 
 //	@Query("select c from Challenge c join fetch c.user join fetch c.challengeStatuses where c.id = :challengeId")
