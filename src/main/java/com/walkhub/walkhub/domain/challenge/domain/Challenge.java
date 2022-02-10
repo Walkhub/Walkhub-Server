a243e7915ce5c06db4a0cdea0048aff63a0beee2
이준서 <ljchan71@gmail.com>
2022-02-10 15:24:56 +0900

    ⚡️ :: import제거

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/Challenge.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/Challenge.java
@@ -27,8 +27,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
