6098581083fcc0120d52c538c5a8fe5e6b1a9506
parksangwoo <b01056721617@gmail.com>
2022-02-10 15:51:10 +0900

    📑 :: QueryUserListService

---
+++ src/main/java/com/walkhub/walkhub/domain/teacher/service/QueryUserListService.java
@@ -0,0 +1,20 @@
+package com.walkhub.walkhub.domain.teacher.service;
+
+import com.walkhub.walkhub.domain.teacher.presentation.dto.response.QueryUserListResponse;
+import com.walkhub.walkhub.domain.user.domain.repository.UserRepository;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class QueryUserListService {
+    private final UserRepository userRepository;
+    private final UserFacade userFacade;
+
+    public QueryUserListResponse execute(Integer page, String scope, String sort, Integer grade, Integer classNum) {
+        return QueryUserListResponse.builder()
+                .userList(userRepository.queryUserList(page, scope, sort, grade, classNum, userFacade.getCurrentUser()))
+                .build();
+    }
+}
