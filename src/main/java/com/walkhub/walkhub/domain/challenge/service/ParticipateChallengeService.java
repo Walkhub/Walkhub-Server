6953a16339f6dc7c3705af0fe0da3ebfd053ad02
김범진 <sdpthf@naver.com>
2022-02-10 22:41:13 +0900

    📑 :: ParticipateChallengeService

---
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/ParticipateChallengeService.java
@@ -0,0 +1,66 @@
+package com.walkhub.walkhub.domain.challenge.service;
+
+import com.walkhub.walkhub.domain.challenge.domain.Challenge;
+import com.walkhub.walkhub.domain.challenge.domain.ChallengeStatus;
+import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeStatusRepository;
+import com.walkhub.walkhub.domain.challenge.exception.AlreadyParticipatedException;
+import com.walkhub.walkhub.domain.challenge.exception.InvalidScopeException;
+import com.walkhub.walkhub.domain.challenge.facade.ChallengeFacade;
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ParticipateChallengeService {
+
+    private final UserFacade userFacade;
+    private final ChallengeFacade challengeFacade;
+    private final ChallengeStatusRepository challengeStatusRepository;
+
+    public void execute(Long challengeId) {
+        Challenge challenge = challengeFacade.getChallengeById(challengeId);
+        User user = userFacade.getCurrentUser();
+        User writer = challenge.getUser();
+
+        if (!verifyScope(user, writer, challenge)) {
+            throw InvalidScopeException.EXCEPTION;
+        }
+
+        if (challengeStatusRepository.findByChallengeAndUser(challenge, user).isPresent()) {
+            throw AlreadyParticipatedException.EXCEPTION;
+        }
+
+        ChallengeStatus challengeStatus = ChallengeStatus.builder()
+                .challenge(challenge)
+                .user(user)
+                .successCount(0L)
+                .build();
+
+        challengeStatusRepository.save(challengeStatus);
+    }
+
+    private boolean verifyScope(User user, User writer, Challenge challenge) {
+        switch (challenge.getUserScope()) {
+            case SCHOOL: {
+                if (!user.getSchool().equals(writer.getSchool())) {
+                    return false;
+                }
+            }
+            case GRADE: {
+                if (!user.getSection().getGrade().equals(writer.getSection().getGrade())) {
+                    return false;
+                }
+            }
+            case CLASS: {
+                if (!user.getSection().getClassNum().equals(writer.getSection().getClassNum())) {
+                    return false;
+                }
+            }
+            default:
+                return true;
+        }
+    }
+
+}
