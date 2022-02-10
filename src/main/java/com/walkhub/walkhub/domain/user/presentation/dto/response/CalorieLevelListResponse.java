d4e73eee18b1ba6a76f234e64f13665faaaaefa6
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-10 16:31:39 +0900

    :bookmark_tabs: :: CalorieLevelListResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/user/presentation/dto/response/CalorieLevelListResponse.java
@@ -0,0 +1,25 @@
+package com.walkhub.walkhub.domain.user.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class CalorieLevelListResponse {
+
+    private final List<CalorieLevelResponse> caloriesLevelList;
+
+    @Getter
+    @Builder
+    public static class CalorieLevelResponse {
+        private final String foodImageUrl;
+        private final String foodName;
+        private final Integer calorie;
+        private final Integer size;
+        private final Integer level;
+        private final String message;
+    }
+}
