658ee7f21fdc48c9b50b2c1d3dd89c146c5a2a6f
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-10 09:25:19 +0900

    ⚡️ :: Create Challenge api 추가

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
@@ -1,9 +1,19 @@
 package com.walkhub.walkhub.domain.challenge.presenstation;
 
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.request.CreateChallengeRequest;
+import com.walkhub.walkhub.domain.challenge.service.CreateChallengeService;
 import com.walkhub.walkhub.domain.challenge.service.RemoveChallengeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/challenges")
@@ -11,11 +21,18 @@ import org.springframework.web.bind.annotation.*;
 public class ChallengeController {
 
     private final RemoveChallengeService removeChallengeService;
+    private final CreateChallengeService createChallengeService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{challenge-id}")
     public void removeChallenge(@PathVariable(name = "challenge-id") Long id) {
         removeChallengeService.execute(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createChallenge(@RequestBody @Valid CreateChallengeRequest request) {
+        createChallengeService.execute(request);
     }
 
 }
