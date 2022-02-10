fe874bf2010b7b98e046d6c334f26464de58083f
lyutvs <lyutvs@gmail.com>
2022-02-11 03:02:54 +0900

    ⚡️:: NotificationController

--- src/main/java/com/walkhub/walkhub/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/walkhub/walkhub/domain/notification/presentation/NotificationController.java
@@ -1,7 +1,11 @@
 package com.walkhub.walkhub.domain.notification.presentation;
 
+import com.walkhub.walkhub.domain.notification.domain.NotificationEntity;
+import com.walkhub.walkhub.domain.notification.presentation.dto.response.QueryNotificationListResponse;
 import com.walkhub.walkhub.domain.notification.service.NotificationReadService;
+import com.walkhub.walkhub.domain.notification.service.QueryNotificationListService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class NotificationController {
 
     private final NotificationReadService notificationReadService;
+    private final QueryNotificationListService queryNotificationListService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/{notification-id}")
@@ -18,4 +23,8 @@ public class NotificationController {
         notificationReadService.execute(notificationId);
     }
 
+    @GetMapping("/")
+    public QueryNotificationListResponse notificationList(Pageable pageable, NotificationEntity notificationEntity) {
+        return queryNotificationListService.execute(pageable,notificationEntity);
+    }
 }
