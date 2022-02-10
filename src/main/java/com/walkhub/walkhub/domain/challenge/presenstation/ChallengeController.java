4930c5f503c1ee86b0c2229467379c5da2fe022a
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-10 11:08:14 +0900

    ⚡️ :: Update Challenge api 추가

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
@@ -1,14 +1,16 @@
 package com.walkhub.walkhub.domain.challenge.presenstation;
 
-import com.walkhub.walkhub.domain.challenge.presenstation.dto.request.CreateChallengeRequest;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.request.ChallengeRequest;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeListResponse;
 import com.walkhub.walkhub.domain.challenge.service.CreateChallengeService;
 import com.walkhub.walkhub.domain.challenge.service.QueryChallengeListService;
 import com.walkhub.walkhub.domain.challenge.service.RemoveChallengeService;
+import com.walkhub.walkhub.domain.challenge.service.UpdateChallengeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +28,7 @@ public class ChallengeController {
     private final RemoveChallengeService removeChallengeService;
     private final CreateChallengeService createChallengeService;
     private final QueryChallengeListService queryChallengeListService;
+    private final UpdateChallengeService updateChallengeService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{challenge-id}")
@@ -35,7 +38,7 @@ public class ChallengeController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void createChallenge(@RequestBody @Valid CreateChallengeRequest request) {
+    public void createChallenge(@RequestBody @Valid ChallengeRequest request) {
         createChallengeService.execute(request);
     }
 
@@ -43,4 +46,12 @@ public class ChallengeController {
     public QueryChallengeListResponse queryChallengeList() {
         return queryChallengeListService.execute();
     }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{challenge-id}")
+    public void updateChallenge(@PathVariable("challenge-id") Long id,
+                                @RequestBody @Valid ChallengeRequest request) {
+        updateChallengeService.execute(id, request);
+    }
+
 }
