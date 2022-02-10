2674161b119541042be86b65f3f99d2af9415659
lyutvs <lyutvs@gmail.com>
2022-02-11 03:02:25 +0900

    📑:: QueryNotificatoinListService

---
+++ src/main/java/com/walkhub/walkhub/domain/notification/service/QueryNotificationListService.java
@@ -0,0 +1,45 @@
+package com.walkhub.walkhub.domain.notification.service;
+
+import com.walkhub.walkhub.domain.notification.domain.NotificationEntity;
+import com.walkhub.walkhub.domain.notification.domain.repository.NotificationListRepository;
+import com.walkhub.walkhub.domain.notification.presentation.dto.response.QueryNotificationListResponse;
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.walkhub.walkhub.domain.notification.presentation.dto.response.QueryNotificationListResponse.*;
+
+@RequiredArgsConstructor
+@Service
+public class QueryNotificationListService {
+
+    private final NotificationListRepository notificationListRepository;
+    private final UserFacade userFacade;
+
+    @Transactional(readOnly = true)
+    public QueryNotificationListResponse execute(Pageable pageable, NotificationEntity notificationEntity) {
+
+        User user = userFacade.getCurrentUser();
+
+        List<NotificationResponse> notificationLists = notificationListRepository.findByUser(user, pageable)
+                .stream()
+                .map(notificationList -> NotificationResponse.builder()
+                        .id(notificationEntity.getId())
+                        .title(notificationEntity.getTitle())
+                        .content(notificationEntity.getContent())
+                        .type(notificationEntity.getType())
+                        .value(notificationEntity.getValue())
+                        .isRead(notificationList.getIsRead())
+                        .build())
+                .collect(Collectors.toList());
+
+        return new QueryNotificationListResponse(notificationLists);
+
+    }
+}
