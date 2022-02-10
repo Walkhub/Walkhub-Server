3c977e04e10fc9dea242992016ba4b66135712d2
lyutvs <lyutvs@gmail.com>
2022-02-11 03:02:12 +0900

    📑:: QueryNotificationListReponse

---
+++ src/main/java/com/walkhub/walkhub/domain/notification/presentation/dto/response/QueryNotificationListResponse.java
@@ -0,0 +1,27 @@
+package com.walkhub.walkhub.domain.notification.presentation.dto.response;
+
+import com.walkhub.walkhub.domain.notification.domain.type.Type;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class QueryNotificationListResponse {
+
+    private final List<NotificationResponse> notificationList;
+
+    @Getter
+    @Builder
+    public static class NotificationResponse{
+
+        private final Long id;
+        private final String title;
+        private final String content;
+        private final Type type;
+        private final Long value;
+        private final boolean isRead;
+    }
+}
