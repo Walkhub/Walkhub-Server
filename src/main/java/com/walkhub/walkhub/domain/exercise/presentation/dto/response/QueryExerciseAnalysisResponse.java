ad2bb6576631c053cebd094170d402d41618af99
lyutvs <lyutvs@gmail.com>
2022-02-10 22:43:54 +0900

    📑:: QueryExericiseAnalysisResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/response/QueryExerciseAnalysisResponse.java
@@ -0,0 +1,35 @@
+package com.walkhub.walkhub.domain.exercise.presentation.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class QueryExerciseAnalysisResponse {
+
+    private final List<ExerciseAnalysisResponse> exerciseAnalysisResponses;
+
+    @Getter
+    @Builder
+    public static class ExerciseAnalysisResponse {
+        private final Long levelId;
+        private final Integer foodCalorie;
+        private final List<WalkCountListResponse> walkCountList;
+        private final Integer dailyWalkCountGoal;
+        private final Integer walkCount;
+        private final Integer calorie;
+        private final Integer distance;
+        private final LocalDateTime walkTime;
+    }
+
+    @Getter
+    @Builder
+    public static class WalkCountListResponse {
+        private final Integer walkCount;
+    }
+}
