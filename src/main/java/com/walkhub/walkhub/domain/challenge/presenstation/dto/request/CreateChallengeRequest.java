d90c8dd39d9b2bf7b3206363b959eac8bc9ebdf1
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-10 15:21:34 +0900

    ⚡️ :: Request 나눔

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/request/CreateChallengeRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/request/CreateChallengeRequest.java
@@ -12,8 +12,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-public class ChallengeRequest {
-
+public class CreateChallengeRequest {
     @NotBlank(message = "name은 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
     private String name;
 
