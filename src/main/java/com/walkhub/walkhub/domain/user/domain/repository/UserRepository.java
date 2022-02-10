4fd37dd15bb6baa1725d40f973205d2211c105ee
parksangwoo <b01056721617@gmail.com>
2022-02-10 15:49:12 +0900

    ⚡️ :: UserRepositoryCustom 추가

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long>, UserRepositoryCustom {
     Optional<User> findByAccountId(String accountId);
     Optional<User> findByPhoneNumber(String phoneNumber);
 }
