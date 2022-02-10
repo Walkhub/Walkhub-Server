958acc031ba1a8352439b791676a82e9773651ba
이준서 <ljchan71@gmail.com>
2022-02-10 16:16:18 +0900

    ⚡️ :: ChallengeStatusRepository

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeStatusRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeStatusRepository.java
@@ -2,9 +2,10 @@ package com.walkhub.walkhub.domain.challenge.domain.repository;
 
 import com.walkhub.walkhub.domain.challenge.domain.ChallengeStatus;
 import com.walkhub.walkhub.domain.challenge.domain.ChallengeStatusId;
+import com.walkhub.walkhub.domain.user.domain.User;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChallengeStatusRepository extends CrudRepository<ChallengeStatus, ChallengeStatusId> {
-//    Optional<ChallengeStatus> findByUserAndChallenge(User user, Challenge challenge);
-//    List<ChallengeStatus> findByChallengeId(Long challengeId);
+	List<ChallengeStatus> findAllByUser(User user);
 }
