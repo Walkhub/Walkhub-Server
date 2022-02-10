0a50c15a646570c05f9bd13741b978767d1e6830
parksangwoo <b01056721617@gmail.com>
2022-02-10 15:50:54 +0900

    📑 :: QueryUserListResponse DTO

---
+++ src/main/java/com/walkhub/walkhub/domain/teacher/presentation/dto/response/QueryUserListResponse.java
@@ -0,0 +1,39 @@
+package com.walkhub.walkhub.domain.teacher.presentation.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class QueryUserListResponse {
+
+    private List<UserListInfo> userList;
+
+    @Getter
+    @NoArgsConstructor
+    @Builder
+    public static class UserListInfo {
+        private Long userId;
+        private String name;
+        private String profileImageUrl;
+        private Integer grade;
+        private Integer classNum;
+        private Integer number;
+        private Boolean isTeacher;
+
+        @QueryProjection
+        public UserListInfo(Long userId, String name, String profileImageUrl, Integer grade, Integer classNum, Integer number, Boolean isTeacher) {
+            this.userId = userId;
+            this.name = name;
+            this.profileImageUrl = profileImageUrl;
+            this.grade = grade;
+            this.classNum = classNum;
+            this.number = number;
+            this.isTeacher = isTeacher;
+        }
+    }
+}
