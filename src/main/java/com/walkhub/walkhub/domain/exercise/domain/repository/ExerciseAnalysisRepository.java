a1ef39239f5e0ca7e958edc74eedc0789afea596
김범진 <sdpthf@naver.com>
2022-02-11 11:30:19 +0900

    ⚡ :: findAllByUser()

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseAnalysisRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseAnalysisRepository.java
@@ -5,8 +5,10 @@ import com.walkhub.walkhub.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface ExerciseAnalysisRepository extends CrudRepository<ExerciseAnalysis, Long> {
     Optional<ExerciseAnalysis> findByUserAndDate(User user, LocalDate date);
+    List<ExerciseAnalysis> findAllByUser(User user);
 }
