8f8dff00605fc0369714d73d0085320c305e24cf
김범진 <sdpthf@naver.com>
2022-02-11 19:24:12 +0900

    ⚰ :: SchoolRankInfo View

--- src/main/java/com/walkhub/walkhub/domain/rank/domain/repository/SchoolRankInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.walkhub.walkhub.domain.rank.domain.repository;
-
-import com.walkhub.walkhub.domain.rank.domain.SchoolRankInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRankInfoRepository extends JpaRepository<SchoolRankInfo, String> {
-}
