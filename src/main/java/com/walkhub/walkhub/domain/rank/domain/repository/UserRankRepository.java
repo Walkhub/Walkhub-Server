adffd3e113708c4756a7df877153041c40afc401
parksangwoo <b01056721617@gmail.com>
2022-02-12 02:16:02 +0900

    ⚡️ :: extends UserRankRepositoryCustom

--- src/main/java/com/walkhub/walkhub/domain/rank/domain/repository/UserRankRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/domain/repository/UserRankRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UserRankRepository extends JpaRepository<UserRankInfo, String> {
+public interface UserRankRepository extends JpaRepository<UserRankInfo, String>, UserRankRepositoryCustom {
     List<UserRankInfo> findTop100ByNameContainsAndAgencyCode(String name, String agencyCode);
     List<UserRankInfo> findTop100ByNameContainsAndAgencyCodeAndClassNumAndGrade(String name, String agencyCode, Integer grade, Integer classNum);
 }
