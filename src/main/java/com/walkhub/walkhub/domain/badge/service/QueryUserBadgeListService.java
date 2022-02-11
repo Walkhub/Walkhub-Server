250424f70462c6a6f06644d36564ca91cda0079f
lyutvs <lyutvs@gmail.com>
2022-02-11 13:02:20 +0900

    📑:: QueryUserBadgeListService

---
+++ src/main/java/com/walkhub/walkhub/domain/badge/service/QueryUserBadgeListService.java
@@ -0,0 +1,36 @@
+package com.walkhub.walkhub.domain.badge.service;
+
+import com.walkhub.walkhub.domain.badge.domain.repository.BadgeRepository;
+import com.walkhub.walkhub.domain.badge.presentation.dto.response.QueryUserBadgeListResponse;
+import com.walkhub.walkhub.domain.badge.presentation.dto.response.QueryUserBadgeListResponse.DefaultBadgeResponse;
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class QueryUserBadgeListService {
+
+    private final BadgeRepository badgeRepository;
+    private final UserFacade userFacade;
+
+    public QueryUserBadgeListResponse execute(Long badgeId) {
+
+        User user = userFacade.getCurrentUser();
+        List<DefaultBadgeResponse> badgeList = badgeRepository.findAllById(badgeId)
+                .stream()
+                .map(badge -> DefaultBadgeResponse.builder()
+                        .name(badge.getName())
+                        .imageUrl(badge.getImageUrl())
+                        .condition(badge.getCondition())
+                        .createAt(badge.getCreateAt())
+                        .build())
+                .collect(Collectors.toList());
+
+        return new QueryUserBadgeListResponse(badgeList);
+    }
+}
