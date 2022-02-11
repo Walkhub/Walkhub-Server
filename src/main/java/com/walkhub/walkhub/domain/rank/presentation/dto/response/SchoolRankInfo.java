232e4686b871b894ad198d29bd083e8b762b21c8
김범진 <sdpthf@naver.com>
2022-02-11 23:58:51 +0900

    ♻ :: 사용하지 않는 import 제거

--- src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/SchoolRankInfo.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/SchoolRankInfo.java
@@ -3,8 +3,6 @@ package com.walkhub.walkhub.domain.rank.presentation.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.persistence.Id;
-
 @Getter
 @Builder
 public class SchoolRankInfo {
