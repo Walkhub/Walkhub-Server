685d5d9f8b232f8f39d8ba8aec476cca68b464be
parksangwoo <b01056721617@gmail.com>
2022-02-12 02:17:00 +0900

    ⚡️ :: queryUserRankListByMySchool 라우터 추가

--- src/main/java/com/walkhub/walkhub/domain/rank/presentation/RankController.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/presentation/RankController.java
@@ -1,6 +1,8 @@
 package com.walkhub.walkhub.domain.rank.presentation;
 
 import com.walkhub.walkhub.domain.rank.presentation.dto.response.UserListResponse;
+import com.walkhub.walkhub.domain.rank.presentation.dto.response.UserRankListResponse;
+import com.walkhub.walkhub.domain.rank.service.QueryUserRankListService;
 import com.walkhub.walkhub.domain.rank.service.UserSearchService;
 import com.walkhub.walkhub.global.enums.UserScope;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RankController {
 
     private final UserSearchService userSearchService;
+    private final QueryUserRankListService queryUserRankListService;
 
     @GetMapping("/users/search")
     public UserListResponse userSearch(@RequestParam String name,
@@ -23,5 +26,10 @@ public class RankController {
                                        @RequestParam(required = false) Integer grade,
                                        @RequestParam(required = false) Integer classNum) {
         return userSearchService.execute(name, userScope, agencyCode, grade, classNum);
+    }
+
+    @GetMapping("/users/my-school")
+    public UserRankListResponse queryUserRankListByMySchool(@RequestParam String scope, @RequestParam String dateType) {
+        return queryUserRankListService.execute(scope, dateType);
     }
 }
