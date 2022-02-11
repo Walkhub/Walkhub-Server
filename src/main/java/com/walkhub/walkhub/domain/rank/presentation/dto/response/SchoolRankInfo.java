3259062768b752d6c1756fbeccf077fd1c45f971
김범진 <sdpthf@naver.com>
2022-02-11 19:24:32 +0900

    📑 ::SchoolRankJob

---
+++ src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/SchoolRankInfo.java
@@ -0,0 +1,23 @@
+package com.walkhub.walkhub.domain.rank.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Id;
+
+@Getter
+@Builder
+public class SchoolRankInfo {
+    private final String schoolId;
+
+    private final String name;
+
+    private final String logoImageUrl;
+
+    private final Long userCount;
+
+    private final Integer walkCount;
+
+    private final Integer ranking;
+
+}
