e97e847bf7295675a96e46868c055ffa1f6ae25b
이준서 <ljchan71@gmail.com>
2022-02-11 12:53:35 +0900

    ⚡ :: TeacherController

--- src/main/java/com/walkhub/walkhub/domain/teacher/presentation/TeacherController.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/presentation/TeacherController.java
@@ -3,12 +3,17 @@ package com.walkhub.walkhub.domain.teacher.presentation;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.request.CreateClassRequest;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.response.CodeResponse;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.response.DetailsClassResponse;
+import com.walkhub.walkhub.domain.teacher.presentation.dto.response.QueryUserDetailsResponse;
 import com.walkhub.walkhub.domain.teacher.service.CreateClassService;
 import com.walkhub.walkhub.domain.teacher.service.DeleteClassService;
 import com.walkhub.walkhub.domain.teacher.service.QueryStudentCodeService;
+import com.walkhub.walkhub.domain.teacher.service.QueryUserDetailsService;
 import com.walkhub.walkhub.domain.teacher.service.RefreshClassCodeService;
 import com.walkhub.walkhub.domain.teacher.service.VerificationCodeService;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +38,7 @@ public class TeacherController {
     private final DeleteClassService deleteClassService;
     private final RefreshClassCodeService refreshClassCodeService;
     private final QueryStudentCodeService queryStudentCodeService;
+    private final QueryUserDetailsService queryUserDetailsService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/classes")
@@ -61,4 +68,10 @@ public class TeacherController {
         return queryStudentCodeService.execute();
     }
 
+    @GetMapping("/users/{user-id}")
+    public QueryUserDetailsResponse queryUserDetails(@PathVariable("user-id") Long userId,
+        @RequestParam("start_at") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startAt,
+        @RequestParam("end_at") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endAt) {
+        return queryUserDetailsService.execute(userId, startAt, endAt);
+    }
 }
