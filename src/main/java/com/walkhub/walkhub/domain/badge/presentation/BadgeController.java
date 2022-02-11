221747b24e9bc7c760ffdae04f2c39003d45e869
lyutvs <lyutvs@gmail.com>
2022-02-11 13:02:32 +0900

    ⚡️:: BadgeController

--- src/main/java/com/walkhub/walkhub/domain/badge/presentation/BadgeController.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/presentation/BadgeController.java
@@ -1,5 +1,7 @@
 package com.walkhub.walkhub.domain.badge.presentation;
 
+import com.walkhub.walkhub.domain.badge.presentation.dto.response.QueryUserBadgeListResponse;
+import com.walkhub.walkhub.domain.badge.service.QueryUserBadgeListService;
 import com.walkhub.walkhub.domain.badge.service.SetTitleBadgeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,11 +13,17 @@ import org.springframework.web.bind.annotation.*;
 public class BadgeController {
 
     private final SetTitleBadgeService setTitleBadgeService;
+    private final QueryUserBadgeListService queryUserBadgeListService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{badge-id}")
     public void setTitleBadge(@PathVariable("badge-id") Long badgeId) {
         setTitleBadgeService.execute(badgeId);
+    }
+
+    @GetMapping
+    public QueryUserBadgeListResponse queryUserBadgeList(@PathVariable(name = "badge-id")Long id) {
+        return queryUserBadgeListService.execute(id);
     }
 
 }
