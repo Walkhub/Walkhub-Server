2fee0eb579d0b1abaefd90f8abdb3fbda5df84c4
김범진 <sdpthf@naver.com>
2022-02-11 15:43:47 +0900

    ♻ :: 안쓰는 메소드 제거

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseAnalysisRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseAnalysisRepository.java
@@ -5,10 +5,8 @@ import com.walkhub.walkhub.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public interface ExerciseAnalysisRepository extends CrudRepository<ExerciseAnalysis, Long>, CustomExerciseRepository {
     Optional<ExerciseAnalysis> findByUserAndDate(User user, LocalDate date);
-    List<ExerciseAnalysis> findAllByUser(User user);
 }
