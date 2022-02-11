f22abaa241462d14f9f1d5cd98f4bb83e468e56f
lyutvs <lyutvs@gmail.com>
2022-02-11 13:01:56 +0900

    📑:: QueryUserBadgeListResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/badge/presentation/dto/response/QueryUserBadgeListResponse.java
@@ -0,0 +1,24 @@
+package com.walkhub.walkhub.domain.badge.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class QueryUserBadgeListResponse {
+
+    private final List<DefaultBadgeResponse> userBadgeList;
+
+    @Getter
+    @Builder
+    public static class DefaultBadgeResponse{
+        private final String name;
+        private final String imageUrl;
+        private final String condition;
+        private final LocalDateTime createAt;
+    }
+}
