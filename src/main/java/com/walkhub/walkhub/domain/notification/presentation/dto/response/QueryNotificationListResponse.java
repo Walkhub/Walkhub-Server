10bec66762c4253fb7da1bb4d8faa91195d668d1
lyutvs <lyutvs@gmail.com>
2022-02-11 13:27:46 +0900

    ♻️:: 행삭제

--- src/main/java/com/walkhub/walkhub/domain/notification/presentation/dto/response/QueryNotificationListResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/notification/presentation/dto/response/QueryNotificationListResponse.java
@@ -16,7 +16,6 @@ public class QueryNotificationListResponse {
     @Getter
     @Builder
     public static class NotificationResponse{
-
         private final Long id;
         private final String title;
         private final String content;
