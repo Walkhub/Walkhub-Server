8cb5c939e302205d13d4c0b59562eb49730a1e7f
lyutvs <lyutvs@gmail.com>
2022-02-12 23:39:15 +0900

    ♻️:: this 키워드 사용

--- src/main/java/com/walkhub/walkhub/domain/notification/service/QueryNotificationListService.java
+++ src/main/java/com/walkhub/walkhub/domain/notification/service/QueryNotificationListService.java
@@ -1,5 +1,6 @@
 package com.walkhub.walkhub.domain.notification.service;
 
+import com.walkhub.walkhub.domain.notification.domain.NotificationEntity;
 import com.walkhub.walkhub.domain.notification.domain.NotificationList;
 import com.walkhub.walkhub.domain.notification.domain.repository.NotificationListRepository;
 import com.walkhub.walkhub.domain.notification.presentation.dto.response.QueryNotificationListResponse;
@@ -22,20 +23,19 @@ public class QueryNotificationListService {
     private final NotificationListRepository notificationListRepository;
     private final UserFacade userFacade;
 
-
     @Transactional(readOnly = true)
     public QueryNotificationListResponse execute(Pageable pageable) {
 
         User user = userFacade.getCurrentUser();
         List<NotificationResponse> notificationLists = notificationListRepository.findByUser(user, pageable)
                 .stream()
-                .map(QueryNotificationListService::getBadge)
+                .map(this::getNotification)
                 .collect(Collectors.toList());
 
         return new QueryNotificationListResponse(notificationLists);
     }
 
-    private static NotificationResponse getBadge(NotificationList notificationList) {
+    private NotificationResponse getNotification(NotificationList notificationList) {
         return NotificationResponse.builder()
                 .id(notificationList.getNotificationEntity().getId())
                 .title(notificationList.getNotificationEntity().getTitle())
