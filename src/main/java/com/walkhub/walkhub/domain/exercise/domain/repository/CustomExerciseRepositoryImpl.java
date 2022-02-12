059e2c343227e8cfa74befa271928c14ab956cee
김범진 <sdpthf@naver.com>
2022-02-12 21:32:44 +0900

    ⚡ :: 임시 커밋

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CustomExerciseRepositoryImpl.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CustomExerciseRepositoryImpl.java
@@ -2,46 +2,55 @@ package com.walkhub.walkhub.domain.exercise.domain.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.walkhub.walkhub.domain.challenge.domain.ChallengeStatus;
+import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.challenge.domain.type.GoalScope;
+import com.walkhub.walkhub.domain.exercise.domain.type.GoalType;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static com.walkhub.walkhub.domain.exercise.domain.QExerciseAnalysis.exerciseAnalysis;
+import static com.walkhub.walkhub.domain.user.domain.QUser.user;
 
 @RequiredArgsConstructor
-public class CustomExerciseRepositoryImpl /*implements CustomExerciseRepository*/ {
+public class CustomExerciseRepositoryImpl implements CustomExerciseRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    // 해당 챌린지에 대해 성공한 날짜
-//    @Override
-//    public List<LocalDate> querySuccessDateList(ChallengeStatus challengeStatus) {
-//        return jpaQueryFactory
-//                .select(exerciseAnalysis.date)
-//                .from(exerciseAnalysis)
-//                .where(
-//                        exerciseAnalysis.user.eq(challengeStatus.getUser()),
-//                        challengeGoalTypeFilter(challengeStatus)
-//                )
-//                .fetch();
-//    }
-//
-//    // 챌린지 목표 타입에 따른 필터링
-//    private BooleanExpression challengeGoalTypeFilter(ChallengeStatus challengeStatus) {
-//        GoalScope goalScope = challengeStatus.getChallenge().getGoalScope();
-//        switch (goalScope) {
-//            case ALL: {
-//                return null;
-//            }
-//            case DAY: {
-//                return exerciseAnalysis.walkCount.gt(challengeStatus.getChallenge().getGoal());
-//            }
-//            default:
-//                return null;
-//        }
-//    }
+    @Override
+    public List<LocalDate> querySuccessDateList(Long userId, Challenge challenge) {
+        LocalDate startAt = challenge.getStartAt().toLocalDate();
+        LocalDate endAt = challenge.getEndAt().toLocalDate();
+
+        return jpaQueryFactory
+                .select(exerciseAnalysis.date)
+                .from(exerciseAnalysis)
+                .join(user)
+                .on(exerciseAnalysis.user.id.eq(userId))
+                .where(
+                        exerciseAnalysis.date.between(startAt, endAt),
+                        challengeGoalTypeFilter(challenge)
+                )
+                .fetch();
+    }
+
+    private BooleanExpression challengeGoalTypeFilter(Challenge challenge) { // 수정 필요
+        if (challenge.getGoalType() == GoalType.WALK) {
+            return challengeGoalScopeFilter(challenge);
+        }
+
+        return exerciseAnalysis.distance.gt(challenge.getGoal());
+    }
+
+    private BooleanExpression challengeGoalScopeFilter(Challenge challenge) { // 수정 필요
+        GoalScope goalScope = challenge.getGoalScope();
+
+        if (goalScope == GoalScope.ALL) {
+            // 모든 걸음 수 합으로 수정 필요
+            return null;
+        }
+        return exerciseAnalysis.walkCount.gt(challenge.getGoal());
+    }
 
 }
