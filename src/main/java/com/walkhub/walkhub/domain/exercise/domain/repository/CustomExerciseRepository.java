122ae5a861754bf59bd14631a776a16b7a20bf70
김범진 <sdpthf@naver.com>
2022-02-12 21:21:26 +0900

    ⚡ :: 성공한 날짜 조회하는 메소드 인자값 변경

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CustomExerciseRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CustomExerciseRepository.java
@@ -1,10 +1,10 @@
 package com.walkhub.walkhub.domain.exercise.domain.repository;
 
-import com.walkhub.walkhub.domain.challenge.domain.ChallengeStatus;
+import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface CustomExerciseRepository {
-    List<LocalDate> querySuccessDateList(ChallengeStatus challengeStatus);
+    List<LocalDate> querySuccessDateList(Long userId, Challenge challenge);
 }
