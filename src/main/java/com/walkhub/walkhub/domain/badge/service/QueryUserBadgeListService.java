7ab4752f0e2353cdaf7aa3717ad83c81b2e2ea02
lyutvs <lyutvs@gmail.com>
2022-02-12 23:24:40 +0900

    ♻️:: 명세대로 수정

--- src/main/java/com/walkhub/walkhub/domain/badge/service/QueryUserBadgeListService.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/service/QueryUserBadgeListService.java
@@ -1,8 +1,9 @@
 package com.walkhub.walkhub.domain.badge.service;
 
-import com.walkhub.walkhub.domain.badge.domain.repository.BadgeRepository;
+import com.walkhub.walkhub.domain.badge.domain.repository.BadgeCollectionRepository;
 import com.walkhub.walkhub.domain.badge.presentation.dto.response.QueryUserBadgeListResponse;
 import com.walkhub.walkhub.domain.badge.presentation.dto.response.QueryUserBadgeListResponse.DefaultBadgeResponse;
+import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,17 +15,18 @@ import java.util.stream.Collectors;
 @Service
 public class QueryUserBadgeListService {
 
-    private final BadgeRepository badgeRepository;
+    private final BadgeCollectionRepository badgeCollectionRepository;
     private final UserFacade userFacade;
 
-    public QueryUserBadgeListResponse execute(Long badgeId) {
+    public QueryUserBadgeListResponse execute(Long userId) {
 
-        List<DefaultBadgeResponse> badgeList = badgeRepository.findAll(badgeId)
+        User user = userFacade.getCurrentUser();
+        List<DefaultBadgeResponse> badgeList = badgeCollectionRepository.findByUserId(userId)
                 .stream()
-                .map(badge -> DefaultBadgeResponse.builder()
-                        .name(badge.getName())
-                        .imageUrl(badge.getImageUrl())
-                        .condition(badge.getCondition())
+                .map(badgeCollection -> DefaultBadgeResponse.builder()
+                        .name(badgeCollection.getBadge().getName())
+                        .imageUrl(badgeCollection.getBadge().getImageUrl())
+                        .condition(badgeCollection.getBadge().getCondition())
                         .build())
                 .collect(Collectors.toList());
 
