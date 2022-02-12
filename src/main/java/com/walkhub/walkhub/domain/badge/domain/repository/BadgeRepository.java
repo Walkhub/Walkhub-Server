ffde98dfae372ac184994c63b8e4a18f51ae995c
lyutvs <lyutvs@gmail.com>
2022-02-12 23:26:51 +0900

    ♻️:: findByuserId

--- src/main/java/com/walkhub/walkhub/domain/badge/domain/repository/BadgeRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/domain/repository/BadgeRepository.java
@@ -3,8 +3,5 @@ package com.walkhub.walkhub.domain.badge.domain.repository;
 import com.walkhub.walkhub.domain.badge.domain.Badge;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface BadgeRepository extends CrudRepository<Badge, Long> {
-    List<Badge> findAll(Long badgeId);
 }
