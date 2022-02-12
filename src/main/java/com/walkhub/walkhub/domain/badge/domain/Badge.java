ca0d028b37ac5bda544d75fa774c4a46fdcd06c7
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-13 02:58:20 +0900

    ⚡️ :: BadgeCollection이랑 관계 매핑

--- src/main/java/com/walkhub/walkhub/domain/badge/domain/Badge.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/domain/Badge.java
@@ -13,6 +13,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -28,6 +30,9 @@ public class Badge extends BaseTimeEntity {
 
     @ColumnDefault(DefaultImage.BADGE_IMAGE)
     private String imageUrl;
+
+    @OneToMany(mappedBy = "badge")
+    private List<BadgeCollection> badgeCollections;
 
     @Builder
     public Badge(String name, String imageUrl) {
