580567c95791318b2d60f4cb9adb6ec01336ef14
이준서 <ljchan71@gmail.com>
2022-02-12 21:53:20 +0900

    ⚡ :: 코드스멜

--- src/main/java/com/walkhub/walkhub/domain/teacher/service/ConfirmTeacherCodeService.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/service/ConfirmTeacherCodeService.java
@@ -3,7 +3,6 @@ package com.walkhub.walkhub.domain.teacher.service;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.request.TeacherCodeRequest;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
-import com.walkhub.walkhub.global.enums.Authority;
 import com.walkhub.walkhub.global.exception.InvalidVerificationCodeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
