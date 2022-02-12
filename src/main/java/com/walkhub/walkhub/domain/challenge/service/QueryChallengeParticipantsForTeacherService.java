5943c6357ccfa7bad8aa68cb48293eef082b3f32
김범진 <sdpthf@naver.com>
2022-02-12 21:21:40 +0900

    ⚡ :: 전체적인 로직 변경

--- src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeParticipantsForTeacherService.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeParticipantsForTeacherService.java
@@ -1,19 +1,15 @@
 package com.walkhub.walkhub.domain.challenge.service;
 
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
-import com.walkhub.walkhub.domain.challenge.domain.ChallengeStatus;
 import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeStatusRepository;
 import com.walkhub.walkhub.domain.challenge.domain.type.SuccessScope;
 import com.walkhub.walkhub.domain.challenge.facade.ChallengeFacade;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeParticipantsForTeacherResponse;
 import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
-import com.walkhub.walkhub.domain.user.domain.Section;
-import com.walkhub.walkhub.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,30 +25,28 @@ public class QueryChallengeParticipantsForTeacherService {
     public QueryChallengeParticipantsForTeacherResponse execute(Long challengeId, SuccessScope successScope) {
         Challenge challenge = challengeFacade.getChallengeById(challengeId);
 
-        List<QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants> result =
-                challengeStatusRepository.queryChallengeParticipantsList(challenge, successScope);
-
-        return new QueryChallengeParticipantsForTeacherResponse(result);
+        return new QueryChallengeParticipantsForTeacherResponse(
+                queryChallengeParticipantsForTeacherResponseBuilder(challenge, successScope)
+        );
     }
 
-//    private QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants queryChallengeParticipantsForTeacherResponseBuilder(ChallengeStatus challengeStatus, SuccessScope successScope) {
-//        User user = challengeStatus.getUser();
-//        Section userSection = user.getSection();
-//        Challenge challenge = challengeStatus.getChallenge();
-//
-//        List<LocalDate> successDateList = exerciseAnalysisRepository.querySuccessDateList(challengeStatus);
-//
-//        return QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants.builder()
-//                .userId(user.getId())
-//                .grade(userSection.getGrade())
-//                .classNum(userSection.getGrade())
-//                .number(user.getNumber())
-//                .name(user.getName())
-//                .profileImageUrl(user.getProfileImageUrl())
-//                .schoolName(user.getSchool().getName())
-//                .successDate(successDateList)
-//                .isSuccess(challengeStatus.getSuccessCount() > challenge.getSuccessStandard())
-//                .build();
-//    }
+    private List<QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants> queryChallengeParticipantsForTeacherResponseBuilder(
+            Challenge challenge, SuccessScope successScope
+    ) {
+        return challengeStatusRepository.queryChallengeParticipantsList(challenge, successScope)
+                .stream()
+                .map(vo -> QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants.builder()
+                        .userId(vo.getUserId())
+                        .grade(vo.getGrade())
+                        .classNum(vo.getClassNum())
+                        .number(vo.getNumber())
+                        .name(vo.getName())
+                        .profileImageUrl(vo.getProfileImageUrl())
+                        .schoolName(vo.getSchoolName())
+                        .successDate(exerciseAnalysisRepository.querySuccessDateList(vo.getUserId(), challenge))
+                        .isSuccess(vo.getIsSuccess())
+                        .build())
+                .collect(Collectors.toList());
+    }
 
 }
