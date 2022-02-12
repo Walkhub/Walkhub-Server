3199d5620936013f53f7d27e472c0c62be082a20
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-12 23:03:32 +0900

    :recycle: :: try catch로 NPE error 처리

--- src/main/java/com/walkhub/walkhub/domain/exercise/cache/ExerciseAnalysisCacheRepositoryImpl.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/cache/ExerciseAnalysisCacheRepositoryImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -31,29 +32,38 @@ public class ExerciseAnalysisCacheRepositoryImpl implements ExerciseAnalysisCach
         Integer walkCount = doubleWalkCount.intValue();
         Long ranking = zSetOperations.rank(EXERCISE_ANALYSIS_KEY, userId);
 
-        return ExerciseAnalysisDto.builder()
-                .walkCount(walkCount)
-                .ranking(ranking == null ? 0 : ranking.intValue())
-                .userId(userId)
-                .build();
+        try {
+            return ExerciseAnalysisDto.builder()
+                    .walkCount(walkCount)
+                    .ranking(Objects.requireNonNull(ranking).intValue())
+                    .userId(userId)
+                    .build();
+        } catch (NullPointerException e) {
+            throw RedisTransactionException.EXCEPTION;
+        }
     }
 
     @Override
     public List<ExerciseAnalysisDto> getUserIdsByRankTop100() {
-        Set<ZSetOperations.TypedTuple<Object>> rankUserIds = zSetOperations.reverseRangeWithScores(EXERCISE_ANALYSIS_KEY, 0, 99);
-        assert rankUserIds != null;
-
-        List<ExerciseAnalysisDto> exerciseAnalysisDtos = new ArrayList<>(rankUserIds.size());
+        Set<ZSetOperations.TypedTuple<Object>> rankUserIds = Optional.ofNullable(
+                zSetOperations.reverseRangeWithScores(EXERCISE_ANALYSIS_KEY, 0, 99))
+                .orElseThrow(() -> RedisTransactionException.EXCEPTION);
         int rank = 1;
 
-        for (ZSetOperations.TypedTuple<Object> tuple : rankUserIds) {
-            ExerciseAnalysisDto exerciseAnalysisDto = ExerciseAnalysisDto.builder()
-                    .walkCount(tuple.getScore() == null ? 0 : Integer.parseInt(tuple.getScore().toString()))
-                    .userId(tuple.getValue() == null ? 0 : (long) tuple.getValue())
-                    .ranking(rank)
-                    .build();
-            exerciseAnalysisDtos.add(exerciseAnalysisDto);
-            rank++;
+        List<ExerciseAnalysisDto> exerciseAnalysisDtos = new ArrayList<>(rankUserIds.size());
+
+        try {
+            for (ZSetOperations.TypedTuple<Object> tuple : rankUserIds) {
+                ExerciseAnalysisDto exerciseAnalysisDto = ExerciseAnalysisDto.builder()
+                        .walkCount(Objects.requireNonNull(tuple.getScore()).intValue())
+                        .userId((Long) tuple.getValue())
+                        .ranking(rank)
+                        .build();
+                exerciseAnalysisDtos.add(exerciseAnalysisDto);
+                rank++;
+            }
+        } catch (NullPointerException e) {
+            throw RedisTransactionException.EXCEPTION;
         }
 
         return exerciseAnalysisDtos;
