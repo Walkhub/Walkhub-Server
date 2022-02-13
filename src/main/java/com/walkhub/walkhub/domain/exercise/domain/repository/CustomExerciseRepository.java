657e214cf5089bbb1ada42fbc95f00a5a81c56a7
김범진 <sdpthf@naver.com>
2022-02-13 19:16:00 +0900

    ♻ :: challengeId

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CustomExerciseRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CustomExerciseRepository.java
@@ -1,10 +1,8 @@
 package com.walkhub.walkhub.domain.exercise.domain.repository;
 
-import com.walkhub.walkhub.domain.challenge.domain.Challenge;
-
 import java.time.LocalDate;
 import java.util.List;
 
 public interface CustomExerciseRepository {
-    List<LocalDate> querySuccessDateList(Long userId, Challenge challenge);
+    List<LocalDate> querySuccessDateList(Long userId, Long challengeId);
 }
