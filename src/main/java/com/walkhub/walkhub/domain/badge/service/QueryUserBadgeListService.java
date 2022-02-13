ba1a1f8dfa579d8c86a7065fe1460ae5c374836a
lyutvs <lyutvs@gmail.com>
2022-02-14 00:41:43 +0900

    ♻️:: codesmell 해결

--- src/main/java/com/walkhub/walkhub/domain/badge/service/QueryUserBadgeListService.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/service/QueryUserBadgeListService.java
@@ -3,8 +3,6 @@ package com.walkhub.walkhub.domain.badge.service;
 import com.walkhub.walkhub.domain.badge.domain.repository.BadgeCollectionRepository;
 import com.walkhub.walkhub.domain.badge.presentation.dto.response.QueryUserBadgeListResponse;
 import com.walkhub.walkhub.domain.badge.presentation.dto.response.QueryUserBadgeListResponse.DefaultBadgeResponse;
-import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +14,9 @@ import java.util.stream.Collectors;
 public class QueryUserBadgeListService {
 
     private final BadgeCollectionRepository badgeCollectionRepository;
-    private final UserFacade userFacade;
 
     public QueryUserBadgeListResponse execute(Long userId) {
 
-        User user = userFacade.getCurrentUser();
         List<DefaultBadgeResponse> badgeList = badgeCollectionRepository.findByUserId(userId)
                 .stream()
                 .map(badgeCollection -> DefaultBadgeResponse.builder()
