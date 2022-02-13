75730bea9bf784aace3b57761a0b2e42a56a2dfe
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-14 00:11:54 +0900

    ⚡️ :: user section_id null로 바꾸는 로직

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.walkhub.walkhub.domain.user.domain.repository;
 import com.walkhub.walkhub.domain.user.domain.Section;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.global.enums.Authority;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +17,9 @@ public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByPhoneNumber(String phoneNumber);
     List<User> findAllBySectionAndAuthority(Section section, Authority authority);
     List<User> findAllBySchoolIdAndNameContaining(Long id, String name);
+
+    @Transactional
+    @Modifying
+    @Query("update User u set u.section = null where u.section = :section")
+    void setUserSectionNull(@Param("section") Section section);
 }
