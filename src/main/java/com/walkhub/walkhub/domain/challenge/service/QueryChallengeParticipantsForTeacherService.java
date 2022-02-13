82defde713374abed701053046153a8516282f25
김범진 <sdpthf@naver.com>
2022-02-13 23:54:46 +0900

    ⚡ :: challengeId를 challenge로 변경

--- src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeParticipantsForTeacherService.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeParticipantsForTeacherService.java
@@ -1,7 +1,9 @@
 package com.walkhub.walkhub.domain.challenge.service;
 
+import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeStatusRepository;
 import com.walkhub.walkhub.domain.challenge.domain.type.SuccessScope;
+import com.walkhub.walkhub.domain.challenge.facade.ChallengeFacade;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeParticipantsForTeacherResponse;
 import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,20 +16,23 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class QueryChallengeParticipantsForTeacherService {
+    private final ChallengeFacade challengeFacade;
     private final ChallengeStatusRepository challengeStatusRepository;
     private final ExerciseAnalysisRepository exerciseAnalysisRepository;
 
     @Transactional(readOnly = true)
     public QueryChallengeParticipantsForTeacherResponse execute(Long challengeId, SuccessScope successScope) {
+        Challenge challenge = challengeFacade.getChallengeById(challengeId);
+
         return new QueryChallengeParticipantsForTeacherResponse(
-                queryChallengeParticipantsForTeacherResponseBuilder(challengeId, successScope)
+                queryChallengeParticipantsForTeacherResponseBuilder(challenge, successScope)
         );
     }
 
     private List<QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants> queryChallengeParticipantsForTeacherResponseBuilder(
-            Long challengeId, SuccessScope successScope
+            Challenge challenge, SuccessScope successScope
     ) {
-        return challengeStatusRepository.queryChallengeParticipantsList(challengeId, successScope)
+        return challengeStatusRepository.queryChallengeParticipantsList(challenge, successScope)
                 .stream()
                 .map(vo -> QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants.builder()
                         .userId(vo.getUserId())
@@ -37,7 +42,7 @@ public class QueryChallengeParticipantsForTeacherService {
                         .name(vo.getName())
                         .profileImageUrl(vo.getProfileImageUrl())
                         .schoolName(vo.getSchoolName())
-                        .successDate(exerciseAnalysisRepository.querySuccessDateList(vo.getUserId(), challengeId))
+                        .successDate(exerciseAnalysisRepository.querySuccessDateList(vo.getUserId(), challenge))
                         .isSuccess(vo.getIsSuccess())
                         .build())
                 .collect(Collectors.toList());
--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CustomExerciseRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CustomExerciseRepository.java
@@ -1,8 +1,10 @@
 package com.walkhub.walkhub.domain.exercise.domain.repository;
 
+import com.walkhub.walkhub.domain.challenge.domain.Challenge;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public interface CustomExerciseRepository {
-    List<LocalDate> querySuccessDateList(Long userId, Long challengeId);
+    List<LocalDate> querySuccessDateList(Long userId, Challenge challenge);
 }
