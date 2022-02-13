0b34a0232af71b9da5ae3cca85d474450dcfec93
김범진 <sdpthf@naver.com>
2022-02-14 04:50:23 +0900

    ♻ :: code smell

--- src/main/java/com/walkhub/walkhub/domain/user/domain/User.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/User.java
@@ -2,8 +2,8 @@ package com.walkhub.walkhub.domain.user.domain;
 
 import com.walkhub.walkhub.domain.badge.domain.Badge;
 import com.walkhub.walkhub.domain.challenge.domain.ChallengeStatus;
-import com.walkhub.walkhub.domain.exercise.domain.ExerciseAnalysis;
 import com.walkhub.walkhub.domain.exercise.domain.Exercise;
+import com.walkhub.walkhub.domain.exercise.domain.ExerciseAnalysis;
 import com.walkhub.walkhub.domain.school.domain.School;
 import com.walkhub.walkhub.domain.user.domain.type.HealthInfo;
 import com.walkhub.walkhub.domain.user.domain.type.Sex;
@@ -11,10 +11,6 @@ import com.walkhub.walkhub.domain.user.presentation.dto.request.UpdateUserInfoRe
 import com.walkhub.walkhub.global.entity.BaseTimeEntity;
 import com.walkhub.walkhub.global.enums.Authority;
 import com.walkhub.walkhub.infrastructure.image.DefaultImage;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
