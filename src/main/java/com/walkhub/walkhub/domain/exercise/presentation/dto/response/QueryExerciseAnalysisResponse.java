58e7201f8151545a6a7b77996738ba4ed174458e
lyutvs <lyutvs@gmail.com>
2022-02-14 15:44:34 +0900

    ♻️:: Response 변경

--- src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/response/QueryExerciseAnalysisResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/response/QueryExerciseAnalysisResponse.java
@@ -1,33 +1,16 @@
 package com.walkhub.walkhub.domain.exercise.presentation.dto.response;
 
-
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
+@Builder
 public class QueryExerciseAnalysisResponse {
-
-    private final List<ExerciseAnalysisResponse> exerciseAnalysisList;
-
-    @Getter
-    @Builder
-    public static class ExerciseAnalysisResponse {
-        private final List<WalkCountListResponse> walkCountList;
-        private final Integer dailyWalkCountGoal;
-        private final Integer walkCount;
-        private final Integer calorie;
-        private final Integer distance;
-        private final LocalDateTime walkTime;
-    }
-
-    @Getter
-    @Builder
-    public static class WalkCountListResponse {
-        private final Integer walkCount;
-    }
+    private final List<Integer> walkCountList;
+    private final Integer dailyWalkCountGoal;
+    private final Integer walkCount;
+    private final Double calorie;
+    private final Integer distance;
 }
