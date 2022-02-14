30b7d44bd957e3fed2341c864bbc19120ffda5f5
parksangwoo <b01056721617@gmail.com>
2022-02-14 10:59:46 +0900

    ♻️ :: 반환타입 변경

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeStatusRepositoryCustom.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeStatusRepositoryCustom.java
@@ -1,10 +1,12 @@
 package com.walkhub.walkhub.domain.challenge.domain.repository;
 
+import com.walkhub.walkhub.domain.challenge.domain.repository.vo.RelatedChallengeParticipantsVO;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeParticipantsForStudentResponse;
+import com.walkhub.walkhub.domain.school.domain.School;
 
 import java.util.List;
 
 public interface ChallengeStatusRepositoryCustom {
     Integer getParticipantsListByChallengeId(Long challengeId);
-    List<QueryChallengeParticipantsForStudentResponse.RelatedChallengeParticipants> getRelatedChallengeParticipantsList(Long challengeId);
+    List<RelatedChallengeParticipantsVO> getRelatedChallengeParticipantsList(Long challengeId, School school, Integer grade, Integer classNum);
 }
