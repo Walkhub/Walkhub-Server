1057f9ae2d83ff19278f9e39b67c0e6286459a9f
parksangwoo <b01056721617@gmail.com>
2022-02-14 10:59:27 +0900

    ♻️ :: ChallengeStatusRepositoryCustom 추가

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeStatusRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeStatusRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface ChallengeStatusRepository extends CrudRepository<ChallengeStatus, ChallengeStatusId> {
+public interface ChallengeStatusRepository extends CrudRepository<ChallengeStatus, ChallengeStatusId>, ChallengeStatusRepositoryCustom {
     Optional<ChallengeStatus> findByChallengeAndUser(Challenge challenge, User user);
     List<ChallengeStatus> findAllByUser(User user);
 }
