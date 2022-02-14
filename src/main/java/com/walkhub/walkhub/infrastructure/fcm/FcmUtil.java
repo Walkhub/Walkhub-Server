bdd607bdc09216e0e04252e20716b8efe30102ae
lyutvs <lyutvs@gmail.com>
2022-02-15 01:48:26 +0900

    ⚡️:: Type 추가

--- src/main/java/com/walkhub/walkhub/infrastructure/fcm/FcmUtil.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/fcm/FcmUtil.java
@@ -1,10 +1,11 @@
 package com.walkhub.walkhub.infrastructure.fcm;
 
+import com.walkhub.walkhub.domain.notification.domain.type.Type;
 import com.walkhub.walkhub.infrastructure.fcm.dto.SendDto;
 
 public interface FcmUtil {
 
     void initialize();
 
-    void sendNotification(SendDto sendDto);
+    void sendNotification(SendDto sendDto, Type type);
 }
