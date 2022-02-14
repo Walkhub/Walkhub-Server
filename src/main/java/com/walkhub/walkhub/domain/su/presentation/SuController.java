10b1cee7fd9c11f150548cab96f8817cc56071fa
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-14 09:44:50 +0900

    ⚡️ :: Create Root Account api 추가

--- src/main/java/com/walkhub/walkhub/domain/su/presentation/SuController.java
+++ src/main/java/com/walkhub/walkhub/domain/su/presentation/SuController.java
@@ -1,10 +1,14 @@
 package com.walkhub.walkhub.domain.su.presentation;
 
+import com.walkhub.walkhub.domain.su.presentation.dto.response.CreateRootAccountResponse;
 import com.walkhub.walkhub.domain.su.presentation.dto.response.SchoolListResponse;
+import com.walkhub.walkhub.domain.su.service.CreateRootAccountService;
 import com.walkhub.walkhub.domain.su.service.ShowSchoolService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class SuController {
 
     private final ShowSchoolService showSchoolService;
+    private final CreateRootAccountService createRootAccountService;
 
     @GetMapping
     public SchoolListResponse showSchool(Pageable page) {
         return showSchoolService.execute(page);
+    }
+
+    @PostMapping("/accounts/{school-id}")
+    public CreateRootAccountResponse rootAccount(@PathVariable("school-id") Long schoolId) {
+        return createRootAccountService.execute(schoolId);
     }
 }
