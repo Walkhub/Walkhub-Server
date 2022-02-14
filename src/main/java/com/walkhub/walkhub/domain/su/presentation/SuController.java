e4a930f4802cb0679a4d8e5def20454c0b79c94f
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-14 14:24:28 +0900

    :recycle: :: ResponseStatus 200 -> 201로 수정

--- src/main/java/com/walkhub/walkhub/domain/su/presentation/SuController.java
+++ src/main/java/com/walkhub/walkhub/domain/su/presentation/SuController.java
@@ -6,10 +6,12 @@ import com.walkhub.walkhub.domain.su.service.CreateRootAccountService;
 import com.walkhub.walkhub.domain.su.service.ShowSchoolService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -26,6 +28,7 @@ public class SuController {
         return showSchoolService.execute(page);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/accounts/{school-id}")
     public CreateRootAccountResponse rootAccount(@PathVariable("school-id") Long schoolId) {
         return createRootAccountService.execute(schoolId);
