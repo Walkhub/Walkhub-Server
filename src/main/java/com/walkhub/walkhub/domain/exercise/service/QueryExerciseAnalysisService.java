dd75d56975d3bb71a57eb22168098cd8b56e7e80
lyutvs <lyutvs@gmail.com>
2022-02-14 16:13:07 +0900

    ⚡️:: QueryExerciseAnalysisService

--- src/main/java/com/walkhub/walkhub/domain/exercise/service/QueryExerciseAnalysisService.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/service/QueryExerciseAnalysisService.java
@@ -1,15 +1,16 @@
 package com.walkhub.walkhub.domain.exercise.service;
 
+import com.walkhub.walkhub.domain.exercise.domain.ExerciseAnalysis;
 import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
 import com.walkhub.walkhub.domain.exercise.presentation.dto.response.QueryExerciseAnalysisResponse;
-import com.walkhub.walkhub.domain.exercise.presentation.dto.response.QueryExerciseAnalysisResponse.ExerciseAnalysisResponse;
-import com.walkhub.walkhub.domain.user.domain.CalorieLevel;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,18 +23,23 @@ public class QueryExerciseAnalysisService {
 
     @Transactional(readOnly = true)
     public QueryExerciseAnalysisResponse execute() {
-        User user = userFacade.getCurrentUser();
-        List<ExerciseAnalysisResponse> queryExerciseAnalysisResponseList =
-                exerciseAnalysisRepository.findByUser(user)
-                        .stream()
-                        .map(exerciseAnalysis -> ExerciseAnalysisResponse.builder()
-                                .dailyWalkCountGoal(user.getDailyWalkCountGoal())
-                                .walkCount(exerciseAnalysis.getWalkCount())
-                                .calorie(exerciseAnalysis.getCalorie())
-                                .distance(exerciseAnalysis.getDistance())
-                                .build())
-                        .collect(Collectors.toList());
 
-        return new QueryExerciseAnalysisResponse(queryExerciseAnalysisResponseList);
+        User user = userFacade.getCurrentUser();
+        LocalDate startAt = LocalDate.now().minusDays(28);
+
+        ExerciseAnalysis exerciseAnalysis = exerciseAnalysisRepository.findByUserAndDate(user, LocalDate.now())
+                .orElse(ExerciseAnalysis.builder().build());
+
+        List<Integer> walkCountList = exerciseAnalysisRepository.findAllByUserAndDateBetween(user, startAt, LocalDate.now())
+                .stream().map(ExerciseAnalysis::getWalkCount)
+                .collect(Collectors.toList());
+
+        return QueryExerciseAnalysisResponse.builder()
+                .walkCount(exerciseAnalysis.getWalkCount())
+                .walkCountList(walkCountList)
+                .calorie(exerciseAnalysis.getCalorie())
+                .dailyWalkCountGoal(user.getDailyWalkCountGoal())
+                .distance(exerciseAnalysis.getDistance())
+                .build();
     }
 }
