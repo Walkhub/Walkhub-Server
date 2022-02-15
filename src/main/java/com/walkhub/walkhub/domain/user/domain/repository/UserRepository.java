42e16b026bf8897f08a455c7f433da1ee08412a8
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-15 10:32:52 +0900

    :coffin: :: findAllBySectionAndAuthority 삭제

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.walkhub.walkhub.domain.user.domain.repository;
 
-import com.walkhub.walkhub.domain.user.domain.Section;
 import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.global.enums.Authority;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -11,6 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByAccountId(String accountId);
     Optional<User> findByPhoneNumber(String phoneNumber);
-    List<User> findAllBySectionAndAuthority(Section section, Authority authority);
     List<User> findAllBySchoolIdAndNameContaining(Long id, String name);
 }
