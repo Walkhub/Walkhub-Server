b5e08267f211c0c41066d35040ee33997974d6e7
김범진 <sdpthf@naver.com>
2022-02-15 10:45:17 +0900

    ⚡ :: 원래대로 변경

--- src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeParticipantsForTeacherService.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeParticipantsForTeacherService.java
@@ -2,18 +2,14 @@ package com.walkhub.walkhub.domain.challenge.service;
 
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeStatusRepository;
-import com.walkhub.walkhub.domain.challenge.domain.repository.vo.ChallengeParticipantsVO;
 import com.walkhub.walkhub.domain.challenge.domain.type.SuccessScope;
 import com.walkhub.walkhub.domain.challenge.facade.ChallengeFacade;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeParticipantsForTeacherResponse;
-import com.walkhub.walkhub.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -34,39 +30,21 @@ public class QueryChallengeParticipantsForTeacherService {
     private List<QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants> queryChallengeParticipantsForTeacherResponseBuilder(
             Challenge challenge, SuccessScope successScope
     ) {
-        Map<Long, List<ChallengeParticipantsVO>> participants =
-                challengeStatusRepository.queryChallengeParticipantsList(challenge, successScope).stream()
-                        .collect(Collectors.groupingBy(ChallengeParticipantsVO::getUserId));
-
-        return participants.values().stream()
-                .map(vo -> buildChallengeParticipantsResponse(vo, challenge))
+        return challengeStatusRepository.queryChallengeParticipantsList(challenge, successScope)
+                .stream()
+                .map(vo -> QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants.builder()
+                        .userId(vo.getUserId())
+                        .grade(vo.getGrade())
+                        .classNum(vo.getClassNum())
+                        .number(vo.getNumber())
+                        .name(vo.getName())
+                        .profileImageUrl(vo.getProfileImageUrl())
+                        .schoolName(vo.getSchoolName())
+                        .successDate(vo.getExerciseAnalysesDates())
+                        .isSuccess(vo.getIsSuccess())
+                        .build()
+                )
                 .collect(Collectors.toList());
-    }
-
-    private QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants buildChallengeParticipantsResponse(
-            List<ChallengeParticipantsVO> challengeParticipantsVOs,
-            Challenge challenge
-    ) {
-        List<LocalDate> successDates = challengeParticipantsVOs.stream()
-                .map(ChallengeParticipantsVO::getExerciseAnalysesDate)
-                .sorted().collect(Collectors.toList());
-
-        ChallengeParticipantsVO participantsVO = challengeParticipantsVOs.stream().findFirst()
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
-
-        Integer successCount = successDates.size();
-
-        return QueryChallengeParticipantsForTeacherResponse.ChallengeParticipants.builder()
-                .userId(participantsVO.getUserId())
-                .grade(participantsVO.getGrade())
-                .classNum(participantsVO.getClassNum())
-                .number(participantsVO.getNumber())
-                .name(participantsVO.getName())
-                .profileImageUrl(participantsVO.getProfileImageUrl())
-                .schoolName(participantsVO.getSchoolName())
-                .successDate(successDates)
-                .isSuccess(challenge.getSuccessStandard() <= successCount)
-                .build();
     }
 
 }
