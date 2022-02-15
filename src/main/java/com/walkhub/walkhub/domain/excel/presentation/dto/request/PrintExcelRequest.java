19a676cdfbd93208d88af8eff28ca00eb2a60188
이준서 <ljchan71@gmail.com>
2022-02-15 22:58:59 +0900

    ♻️ :: userType으로 변경

--- src/main/java/com/walkhub/walkhub/domain/excel/presentation/dto/request/PrintExcelRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/excel/presentation/dto/request/PrintExcelRequest.java
@@ -1,6 +1,6 @@
 package com.walkhub.walkhub.domain.excel.presentation.dto.request;
 
-import com.walkhub.walkhub.global.enums.Authority;
+import com.walkhub.walkhub.domain.excel.domain.type.UserType;
 import java.time.LocalDate;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -14,7 +14,7 @@ public class PrintExcelRequest {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate endAt;
 
-	private Authority authority;
+	private UserType userType;
 
 	private Integer grade;
 
--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseAnalysisRepositoryCustomImpl.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseAnalysisRepositoryCustomImpl.java
@@ -7,6 +7,7 @@ import static com.walkhub.walkhub.domain.user.domain.QSection.section;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.walkhub.walkhub.domain.excel.domain.type.UserType;
 import com.walkhub.walkhub.domain.excel.presentation.dto.request.PrintExcelRequest;
 import com.walkhub.walkhub.domain.exercise.domain.repository.vo.PrintExcelVo;
 import com.walkhub.walkhub.domain.exercise.domain.repository.vo.QPrintExcelVo;
@@ -17,20 +18,17 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class ExerciseAnalysisRepositoryCustomImpl implements ExerciseAnalysisRepositoryCustom {
+
 	private final JPAQueryFactory queryFactory;
 
 	@Override
 	public List<PrintExcelVo> getPrintExcelVoList(PrintExcelRequest excelRequest, Long schoolId) {
 
-		 LocalDate startAt = excelRequest.getStartAt();
-
-		 LocalDate endAt = excelRequest.getEndAt();
-
-		 Authority authority = excelRequest.getAuthority();
-
-		 Integer grade = excelRequest.getGrade();
-
-		 Integer classNum = excelRequest.getClassNum();
+		LocalDate startAt = excelRequest.getStartAt();
+		LocalDate endAt = excelRequest.getEndAt();
+		UserType userType = excelRequest.getUserType();
+		Integer grade = excelRequest.getGrade();
+		Integer classNum = excelRequest.getClassNum();
 
 		return queryFactory
 			.select(new QPrintExcelVo(
@@ -52,28 +50,44 @@ public class ExerciseAnalysisRepositoryCustomImpl implements ExerciseAnalysisRep
 			.where(
 				school.id.eq(schoolId),
 				exerciseAnalysis.date.between(startAt, endAt),
-				nullFilter(authority,grade,classNum)
+				userTypeFilter(userType, grade, classNum)
 			)
 			.groupBy(user)
 			.fetch();
 	}
 
-	private BooleanBuilder nullFilter(Authority authority, Integer grade, Integer classNum) {
-
+	private BooleanBuilder userTypeFilter(UserType userType, Integer grade, Integer classNum) {
 		BooleanBuilder builder = new BooleanBuilder();
 
-		if (authority != null) {
-			builder.and(user.authority.eq(authority));
-
-			if (authority.equals(Authority.USER)) {
-				if (grade != null) {
-					builder.and(section.grade.eq(grade));
-				}
-				if (classNum != null) {
-					builder.and(section.classNum.eq(classNum));
-				}
+		switch (userType) {
+			case STUDENT: {
+				builder.and(user.authority.eq(Authority.USER));
+				builder.and(nullFilter(builder, grade, classNum));
+				break;
+			}
+			case TEACHER: {
+				builder.and(user.authority.eq(Authority.TEACHER));
+				break;
+			}
+			case ALL: {
+				builder.and(user.authority.eq(Authority.USER).or(
+					user.authority.eq(Authority.TEACHER)
+				));
 			}
 		}
+
+		return builder;
+	}
+
+	private BooleanBuilder nullFilter(BooleanBuilder builder, Integer grade, Integer classNum) {
+		if (grade != null) {
+			builder.and(section.grade.eq(grade));
+		}
+
+		if (classNum != null) {
+			builder.and(section.classNum.eq(classNum));
+		}
+
 		return builder;
 	}
 }
