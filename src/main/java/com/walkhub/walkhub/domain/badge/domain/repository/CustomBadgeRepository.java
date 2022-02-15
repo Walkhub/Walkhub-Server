135e50ec1ce27ed005719fd34dba828e0c7ba118
lyutvs <lyutvs@gmail.com>
2022-02-15 14:53:19 +0900

    ⚡️:: MyBadgeVO 로 변경

--- src/main/java/com/walkhub/walkhub/domain/badge/domain/repository/CustomBadgeRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/domain/repository/CustomBadgeRepository.java
@@ -1,10 +1,11 @@
 package com.walkhub.walkhub.domain.badge.domain.repository;
 
 import com.walkhub.walkhub.domain.badge.domain.repository.vo.DefaultBadgeVO;
+import com.walkhub.walkhub.domain.badge.domain.repository.vo.MyBadgeVo;
 
 import java.util.List;
 
 public interface CustomBadgeRepository {
     List<DefaultBadgeVO> findAllByBadgeCollectionsNotIn(Long userId);
-    List<DefaultBadgeVO> findAllByBadgeCollections(Long userId);
+    List<MyBadgeVo> findAllByBadgeCollections(Long userId);
 }
