9be173f1d839982d1cea0a1b79ab5d2c7e6295fb
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-15 10:31:38 +0900

    ⚡️ :: 노션에 따라 수정

--- src/main/java/com/walkhub/walkhub/domain/teacher/service/QueryStudentCodeService.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/service/QueryStudentCodeService.java
@@ -3,11 +3,12 @@ package com.walkhub.walkhub.domain.teacher.service;
 import com.walkhub.walkhub.domain.exercise.domain.ExerciseAnalysis;
 import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.response.DetailsClassResponse;
+import com.walkhub.walkhub.domain.teacher.presentation.dto.response.DetailsClassResponse.TeacherResponse;
+import com.walkhub.walkhub.domain.teacher.presentation.dto.response.DetailsClassResponse.UserListResponse;
 import com.walkhub.walkhub.domain.user.domain.Section;
 import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.domain.user.domain.repository.UserRepository;
+import com.walkhub.walkhub.domain.user.exception.UserNotFoundException;
 import com.walkhub.walkhub.domain.user.facade.SectionFacade;
-import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.global.enums.Authority;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,30 +22,43 @@ import java.util.stream.Collectors;
 @Service
 public class QueryStudentCodeService {
 
-    private final UserFacade userFacade;
     private final SectionFacade sectionFacade;
-    private final UserRepository userRepository;
     private final ExerciseAnalysisRepository exerciseAnalysisRepository;
 
     @Transactional(readOnly = true)
-    public DetailsClassResponse execute() {
-        User teacher = userFacade.getCurrentUser();
-        Section section = sectionFacade.getSectionById(teacher.getSection().getId());
+    public DetailsClassResponse execute(Long sectionId) {
+        Section section = sectionFacade.getSectionById(sectionId);
 
-        List<DetailsClassResponse.UserListResponse> result =
-                userRepository.findAllBySectionAndAuthority(section, Authority.STUDENT)
+        List<UserListResponse> result =
+                section.getUsers()
                         .stream()
+                        .filter(user -> user.getAuthority() == Authority.STUDENT)
                         .map(this::buildUserListResponse)
                         .collect(Collectors.toList());
 
-        return new DetailsClassResponse(section.getClassCode(), result);
+        User teacher = section.getUsers().stream()
+                .filter(user -> user.getAuthority() == Authority.TEACHER)
+                .findFirst()
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        TeacherResponse teacherResponse = TeacherResponse.builder()
+                .userId(teacher.getId())
+                .name(teacher.getName())
+                .profileImageUrl(teacher.getProfileImageUrl())
+                .build();
+
+        return DetailsClassResponse.builder()
+                .teacher(teacherResponse)
+                .classCode(section.getClassCode())
+                .userList(result)
+                .build();
     }
 
-    private DetailsClassResponse.UserListResponse buildUserListResponse(User user) {
+    private UserListResponse buildUserListResponse(User user) {
         Integer walkCount = exerciseAnalysisRepository.findByUserAndDate(user, LocalDate.now())
                 .map(ExerciseAnalysis::getWalkCount)
                 .orElse(0);
-        return DetailsClassResponse.UserListResponse.builder()
+        return UserListResponse.builder()
                 .userId(user.getId())
                 .name(user.getName())
                 .profileImageUrl(user.getProfileImageUrl())
