2fce9ef97db96913071a0fd2163cfb858897a0d9
lyutvs <lyutvs@gmail.com>
2022-02-15 14:54:33 +0900

    ♻️:: MyBadgeVo 를 List 로 받아오기

--- src/main/java/com/walkhub/walkhub/domain/badge/presentation/dto/response/QueryMyBadgeListResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/presentation/dto/response/QueryMyBadgeListResponse.java
@@ -1,8 +1,7 @@
 package com.walkhub.walkhub.domain.badge.presentation.dto.response;
 
-import com.walkhub.walkhub.domain.badge.domain.repository.vo.DefaultBadgeVO;
+import com.walkhub.walkhub.domain.badge.domain.repository.vo.MyBadgeVo;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
@@ -10,15 +9,5 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class QueryMyBadgeListResponse {
-
-    private final List<DefaultBadgeVO> badgeLists;
-
-    @Getter
-    @Builder
-    public static class BadgeListResponse {
-        private final Long id;
-        private final String name;
-        private final String imageUrl;
-        private final boolean isMine;
-    }
+    private final List<MyBadgeVo> badgeLists;
 }
