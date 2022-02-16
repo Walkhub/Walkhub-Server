ea0e6cf696961eb74ef1a1f13823c7eaba9801c8
jhhong0509 <jhhong0509@naver.com>
2022-02-17 00:53:13 +0900

    :bug: :: (#363) Param 어노테이션 import 추가

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/ChallengeRepository.java
@@ -2,21 +2,21 @@ package com.walkhub.walkhub.domain.challenge.domain.repository;
 
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.school.domain.School;
+import com.walkhub.walkhub.domain.user.domain.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import com.walkhub.walkhub.domain.user.domain.User;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
 public interface ChallengeRepository extends CrudRepository<Challenge, Long>, ChallengeRepositoryCustom {
 
-	@Query("select c from Challenge c where c.user.school = :school")
-	List<Challenge> findAllBySchool(@Param("school") School school);
+    @Query("select c from Challenge c where c.user.school = :school")
+    List<Challenge> findAllBySchool(@Param("school") School school);
 
-	List<Challenge> findAllByUser(User user);
+    List<Challenge> findAllByUser(User user);
 
-	void deleteAllByUserAndEndAtAfter(User user, LocalDate now);
+    void deleteAllByUserAndEndAtAfter(User user, LocalDate now);
 
 }
