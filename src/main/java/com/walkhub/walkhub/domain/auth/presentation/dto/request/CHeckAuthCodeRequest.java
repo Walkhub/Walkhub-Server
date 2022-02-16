e288e2c53c6229e12cb95a41fd92f54ce931a15a
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-17 03:08:15 +0900

    :bookmark_tabs: :: CHeckAuthCodeRequest

---
+++ src/main/java/com/walkhub/walkhub/domain/auth/presentation/dto/request/CHeckAuthCodeRequest.java
@@ -0,0 +1,18 @@
+package com.walkhub.walkhub.domain.auth.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
+public class CHeckAuthCodeRequest {
+
+    @NotBlank(message = "phone_number는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    private final String phoneNumber;
+
+    @NotBlank(message = "auth_code는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    private final String authCode;
+    
+}
