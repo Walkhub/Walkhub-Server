b9a0158c85d9781608b8cbe7c55db4d36414bcc7
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-16 15:33:55 +0900

    ⚡️ :: querydsl에 맞게 수정

--- src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryParticipatedChallengeListService.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryParticipatedChallengeListService.java
@@ -1,34 +1,34 @@
 package com.walkhub.walkhub.domain.challenge.service;
 
-import com.walkhub.walkhub.domain.challenge.domain.ChallengeStatus;
 import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeStatusRepository;
 import com.walkhub.walkhub.domain.challenge.facade.ChallengeFacade;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeListResponse;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeListResponse.ChallengeResponse;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class QueryParticipatedChallengeListService {
 
-	private final ChallengeStatusRepository challengeStatusRepository;
-	private final UserFacade userFacade;
-	private final ChallengeFacade challengeFacade;
+    private final ChallengeStatusRepository challengeStatusRepository;
+    private final UserFacade userFacade;
+    private final ChallengeFacade challengeFacade;
 
-	@Transactional(readOnly = true)
-	public QueryChallengeListResponse execute() {
+    @Transactional(readOnly = true)
+    public QueryChallengeListResponse execute() {
 
-		List<ChallengeResponse> challengeResponseList = challengeStatusRepository.findAllByUser(userFacade.getCurrentUser())
-			.stream()
-			.map(ChallengeStatus::getChallenge)
-			.map(challengeFacade::challengeResponseBuilder)
-			.collect(Collectors.toList());
+        List<ChallengeResponse> challengeResponseList = challengeStatusRepository.getUser(userFacade.getCurrentUser())
+                .stream()
+                .map(challengeFacade::challengeResponseBuilder)
+                .collect(Collectors.toList());
 
-		return new QueryChallengeListResponse(challengeResponseList);
-	}
+
+        return new QueryChallengeListResponse(challengeResponseList);
+    }
 }
