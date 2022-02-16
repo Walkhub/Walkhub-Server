ee3a8f5814d2395f4aa139145302c1a3897044e1
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-17 01:00:15 +0900

    :recycle: :: refreshToken 저장 로직 삭제

--- src/main/java/com/walkhub/walkhub/domain/teacher/service/ConfirmTeacherCodeService.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/service/ConfirmTeacherCodeService.java
@@ -1,13 +1,10 @@
 package com.walkhub.walkhub.domain.teacher.service;
 
-import com.walkhub.walkhub.domain.auth.domain.RefreshToken;
-import com.walkhub.walkhub.domain.auth.domain.repository.RefreshTokenRepository;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.request.TeacherCodeRequest;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.response.TokenResponse;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.global.exception.InvalidVerificationCodeException;
-import com.walkhub.walkhub.global.security.jwt.JwtProperties;
 import com.walkhub.walkhub.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,8 +16,6 @@ public class ConfirmTeacherCodeService {
 
     private final UserFacade userFacade;
     private final JwtTokenProvider jwtTokenProvider;
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final JwtProperties jwtProperties;
 
     @Transactional
     public TokenResponse execute(TeacherCodeRequest request) {
@@ -33,14 +28,6 @@ public class ConfirmTeacherCodeService {
 
         String accessToken = jwtTokenProvider.generateAccessToken(user.getAccountId());
         String refreshToken = jwtTokenProvider.generateRefreshToken(user.getAccountId());
-
-        refreshTokenRepository.save(
-                RefreshToken.builder()
-                        .accountId(user.getAccountId())
-                        .token(refreshToken)
-                        .timeToLive(jwtProperties.getRefreshExp())
-                        .build()
-        );
 
         return new TokenResponse(accessToken, refreshToken);
     }
