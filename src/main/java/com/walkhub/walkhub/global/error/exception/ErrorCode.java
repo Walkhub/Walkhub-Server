9531ee2df74db422c877a2b87a2091103f7e8375
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-17 03:09:34 +0900

    ⚡️ :: PHONE_NUMBER_NOT_FOUND 추가

--- src/main/java/com/walkhub/walkhub/global/error/exception/ErrorCode.java
+++ src/main/java/com/walkhub/walkhub/global/error/exception/ErrorCode.java
@@ -31,6 +31,7 @@ public enum ErrorCode {
     SCHOOL_NOT_FOUND(404, "USER-404-4", "School Not Found"),
     SECTION_NOT_FOUND(404, "SECTION-404-1", "Section Not Found"),
     REFRESH_TOKEN_NOT_FOUND(404, "AUTH-404-1", "Refresh Token Not Found"),
+    PHONE_NUMBER_NOT_FOUND(404, "USER-404-5", "PhoneNumber Not Found"),
 
     CHALLENGE_NOT_FOUND(404, "CHALLENGE-404-1", "Challenge Not Found"),
 
