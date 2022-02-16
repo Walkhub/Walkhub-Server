5b47061017ba47db8db07f5f5cf212bc7cbd9d5f
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-17 01:22:07 +0900

    :recycle: :: builder로 수정

--- src/main/java/com/walkhub/walkhub/domain/teacher/presentation/dto/response/TokenResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/presentation/dto/response/TokenResponse.java
@@ -1,10 +1,10 @@
 package com.walkhub.walkhub.domain.teacher.presentation.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@Builder
 public class TokenResponse {
 
     private final String accessToken;
