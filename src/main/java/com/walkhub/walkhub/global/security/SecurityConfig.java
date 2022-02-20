da4560f21c1412691dce235b8a883be7aa6ff487
jhhong0509 <jhhong0509@naver.com>
2022-02-21 00:07:28 +0900

    ⚡️ :: (#418) SecurityConfig에 progress 추가

--- src/main/java/com/walkhub/walkhub/global/security/SecurityConfig.java
+++ src/main/java/com/walkhub/walkhub/global/security/SecurityConfig.java
@@ -95,6 +95,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/challenges").hasAnyAuthority("TEACHER", "ROOT", "SU")
                 .antMatchers(HttpMethod.PATCH, "/challenges/{challenge-id}").hasAnyAuthority("TEACHER", "ROOT", "SU")
                 .antMatchers(HttpMethod.DELETE, "/challenges/{challenge-id}").hasAnyAuthority("TEACHER", "ROOT", "SU")
+                .antMatchers(HttpMethod.GET, "/challenges/{challenge-id}/progress").hasAuthority("TEACHER")
                 .antMatchers(HttpMethod.GET, "/challenges/{challenge-id}").authenticated()
                 .antMatchers(HttpMethod.POST, "/challenges/{challenge-id}").authenticated()
                 .antMatchers(HttpMethod.GET, "/challenges/{challenge-id}/participants/students").authenticated()
