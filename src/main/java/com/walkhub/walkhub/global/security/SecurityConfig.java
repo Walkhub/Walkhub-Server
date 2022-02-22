61dc7eb107dee0191b2069269c9195b59fda9d41
lyutvs <lyutvs@gmail.com>
2022-02-22 16:28:19 +0900

    ♻️:: authenticated 로 변경

--- src/main/java/com/walkhub/walkhub/global/security/SecurityConfig.java
+++ src/main/java/com/walkhub/walkhub/global/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PATCH, "/users/goal").authenticated()
                 .antMatchers(HttpMethod.PATCH, "/users/schools").authenticated()
                 .antMatchers(HttpMethod.GET, "/users/levels/lists").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/users").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/users").authenticated()
 
 
                 // badges
