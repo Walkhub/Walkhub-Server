cb49a0bb62f1783cfb0b1adfd4088a3e1bfc4058
KIM JUNG BIN <kjbin0420@naver.com>
2022-02-22 14:17:58 +0900

    ⚡️ :: (#422) AsyncConfigurer를 override하여 asyncExecutor 설정

--- src/main/java/com/walkhub/walkhub/global/async/AsyncConfig.java
+++ src/main/java/com/walkhub/walkhub/global/async/AsyncConfig.java
@@ -1,8 +1,27 @@
 package com.walkhub.walkhub.global.async;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync
 @Configuration
-public class AsyncConfig { }
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("async-thread-");
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(100);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+        executor.initialize();
+
+        return executor;
+    }
+}
