447be46f15c28036460e706721c9ed54b61cfbbd
jhhong0509 <jhhong0509@naver.com>
2022-02-23 00:26:31 +0900

    :recycle: :: (#430) unused import 삭제

--- src/main/java/com/walkhub/walkhub/domain/auth/service/UserSignInService.java
+++ src/main/java/com/walkhub/walkhub/domain/auth/service/UserSignInService.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @RequiredArgsConstructor
