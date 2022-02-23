55c414baa314d4b57828d4c642c83ce1f3658d86
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-23 10:47:58 +0900

    :bookmark_tabs: :: QueryExerciseDetailsService

---
+++ src/main/java/com/walkhub/walkhub/domain/exercise/service/QueryExerciseDetailsService.java
@@ -0,0 +1,35 @@
+package com.walkhub.walkhub.domain.exercise.service;
+
+import com.walkhub.walkhub.domain.exercise.domain.Exercise;
+import com.walkhub.walkhub.domain.exercise.domain.repository.LocationRepository;
+import com.walkhub.walkhub.domain.exercise.facade.ExerciseFacade;
+import com.walkhub.walkhub.domain.exercise.presentation.dto.response.QueryExerciseDetailsResponse;
+import com.walkhub.walkhub.domain.exercise.presentation.dto.response.QueryExerciseDetailsResponse.ExerciseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class QueryExerciseDetailsService {
+
+    private final ExerciseFacade exerciseFacade;
+    private final LocationRepository locationRepository;
+
+    public QueryExerciseDetailsResponse execute(Long exerciseId) {
+        Exercise exercise = exerciseFacade.getById(exerciseId);
+
+        List<ExerciseResponse> locations = locationRepository.findAllByExerciseOrderBySequence(exercise)
+                .stream()
+                .map(location -> ExerciseResponse.builder()
+                        .order(location.getSequence())
+                        .latitude(location.getLatitude())
+                        .longitude(location.getLongitude())
+                        .build())
+                .collect(Collectors.toList());
+
+        return new QueryExerciseDetailsResponse(exercise.getImageUrl(), locations);
+    }
+}
