f8c504f0e0e4f64ea13d2b4f5b61aa4b5d2ae07e
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-23 10:48:10 +0900

    ⚡️ :: findAllByExerciseOrderBySequence 추가

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/LocationRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/LocationRepository.java
@@ -5,6 +5,9 @@ import com.walkhub.walkhub.domain.exercise.domain.Location;
 import com.walkhub.walkhub.domain.exercise.domain.LocationId;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LocationRepository extends CrudRepository<Location, LocationId> {
 	Location findTop1ByExerciseOrderBySequenceDesc(Exercise exercise);
+	List<Location> findAllByExerciseOrderBySequence(Exercise exercise);
 }
