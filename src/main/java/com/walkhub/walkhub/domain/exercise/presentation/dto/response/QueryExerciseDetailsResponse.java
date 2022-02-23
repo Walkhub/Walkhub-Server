b021132835a8cec6bfd093fda5ae36f9cff1f874
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-23 10:47:43 +0900

    :bookmark_tabs: :: QueryExerciseDetailsResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/response/QueryExerciseDetailsResponse.java
@@ -0,0 +1,24 @@
+package com.walkhub.walkhub.domain.exercise.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class QueryExerciseDetailsResponse {
+
+    private final String imageUrl;
+    private final List<ExerciseResponse> exerciseList;
+
+    @Getter
+    @Builder
+    public static class ExerciseResponse {
+        private final Integer order;
+        private final BigDecimal latitude;
+        private final BigDecimal longitude;
+    }
+}
