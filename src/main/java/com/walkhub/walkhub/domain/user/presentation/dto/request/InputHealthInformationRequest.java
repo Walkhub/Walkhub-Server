0e7024523e9790bf7b627dea9daff8c4cfa4526a
parksangwoo <b01056721617@gmail.com>
2022-02-25 17:38:22 +0900

    ⚡️ :: Sex 추가, Notnull 삭제

--- src/main/java/com/walkhub/walkhub/domain/user/presentation/dto/request/InputHealthInformationRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/user/presentation/dto/request/InputHealthInformationRequest.java
@@ -1,10 +1,10 @@
 package com.walkhub.walkhub.domain.user.presentation.dto.request;
 
+import com.walkhub.walkhub.domain.user.domain.type.Sex;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 
@@ -12,11 +12,11 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class InputHealthInformationRequest {
 
-    @NotNull(message = "height는 null일 수 없습니다.")
     @Digits(integer = 3, fraction = 1)
     private BigDecimal height;
 
-    @NotNull(message = "weight는 null일 수 없습니다.")
     @Positive(message = "weight는 양수여야 합니다.")
     private Integer weight;
+
+    private Sex sex;
 }
