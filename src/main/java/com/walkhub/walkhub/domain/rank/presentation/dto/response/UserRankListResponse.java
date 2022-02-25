93c6273ac15b11720fa6df8875232ead44aaa9af
parksangwoo <b01056721617@gmail.com>
2022-02-25 23:38:38 +0900

    ⚡️ :: @JsonInclude 삭제

--- src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/UserRankListResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/UserRankListResponse.java
@@ -1,6 +1,5 @@
 package com.walkhub.walkhub.domain.rank.presentation.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,7 +7,6 @@ import java.util.List;
 
 @Getter
 @Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserRankListResponse {
 
     private final UserRankResponse myRank;
@@ -19,8 +17,6 @@ public class UserRankListResponse {
     public static class UserRankResponse {
         private final Long userId;
         private final String name;
-        private final Integer grade;
-        private final Integer classNum;
         private final Integer ranking;
         private final String profileImageUrl;
         private final Integer walkCount;
