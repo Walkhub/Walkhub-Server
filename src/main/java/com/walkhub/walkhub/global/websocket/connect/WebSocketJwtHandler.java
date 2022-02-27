762376c276d72183414b8a9ed3ee6d3619493fdd
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-27 13:38:37 +0900

    :recycle: :: import 삭제

--- src/main/java/com/walkhub/walkhub/global/websocket/connect/WebSocketJwtHandler.java
+++ src/main/java/com/walkhub/walkhub/global/websocket/connect/WebSocketJwtHandler.java
@@ -7,8 +7,6 @@ import com.walkhub.walkhub.global.security.jwt.JwtTokenProvider;
 import com.walkhub.walkhub.global.websocket.property.ClientProperty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
