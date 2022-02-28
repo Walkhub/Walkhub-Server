517ed9d95fdfbeebce02f3895481d508a20c1961
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-28 16:41:02 +0900

    ⚡️ :: findAllByUserSectionAndUserIsMeasuring 추가

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/ExerciseRepository.java
@@ -1,6 +1,7 @@
 package com.walkhub.walkhub.domain.exercise.domain.repository;
 
 import com.walkhub.walkhub.domain.exercise.domain.Exercise;
+import com.walkhub.walkhub.domain.user.domain.Section;
 import com.walkhub.walkhub.domain.user.domain.User;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
     Optional<Exercise> findByIsExercisingTrueAndUser(User user);
     List<Exercise> findAllByUser(User user);
+    List<Exercise> findAllByUserSectionAndUserIsMeasuring(Section section);
 }
