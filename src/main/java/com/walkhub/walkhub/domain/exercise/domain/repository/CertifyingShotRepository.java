771f45298ea8aac0ab8a6055ccf482ce07b4759a
김범진 <sdpthf@naver.com>
2022-02-28 21:42:02 +0900

    ⚰ :: (#485) Certifying Shot

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/repository/CertifyingShotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.walkhub.walkhub.domain.exercise.domain.repository;
-
-import com.walkhub.walkhub.domain.exercise.domain.CertifyingShot;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CertifyingShotRepository extends CrudRepository<CertifyingShot, Long> {
-}
