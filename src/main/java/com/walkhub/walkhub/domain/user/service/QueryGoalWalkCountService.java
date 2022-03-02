4756046f2d34730b9ea74175becf6cb4e0a211ad
이준서 <ljchan71@gmail.com>
2022-03-03 01:31:19 +0900

    ⚡ :: (#511) read only

--- src/main/java/com/walkhub/walkhub/domain/user/service/QueryGoalWalkCountService.java
+++ src/main/java/com/walkhub/walkhub/domain/user/service/QueryGoalWalkCountService.java
@@ -5,6 +5,7 @@ import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.domain.user.presentation.dto.response.QueryGoalWalkCountResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,6 +13,7 @@ public class QueryGoalWalkCountService {
 
 	private final UserFacade userFacade;
 
+	@Transactional(readOnly = true)
 	public QueryGoalWalkCountResponse execute() {
 		User user = userFacade.getCurrentUser();
 
