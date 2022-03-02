469ba7a286ecead6e71a29560913245046eb22ef
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-02 16:40:28 +0900

    ⚡️ :: findByCode

--- src/main/java/com/walkhub/walkhub/domain/badge/domain/repository/BadgeRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/domain/repository/BadgeRepository.java
@@ -1,7 +1,12 @@
 package com.walkhub.walkhub.domain.badge.domain.repository;
 
 import com.walkhub.walkhub.domain.badge.domain.Badge;
+import com.walkhub.walkhub.domain.badge.enums.BadgeType;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
 public interface BadgeRepository extends CrudRepository<Badge, Long>, CustomBadgeRepository {
+    Optional<Badge> findByCode(BadgeType code);
 }
