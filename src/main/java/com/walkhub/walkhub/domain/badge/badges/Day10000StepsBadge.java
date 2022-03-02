d0233691685cdd875b53d9cdc15c38509b540e04
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-02 19:39:23 +0900

    :recycle: :: isGoalSuccess로직 수정

--- src/main/java/com/walkhub/walkhub/domain/badge/badges/Day10000StepsBadge.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/badges/Day10000StepsBadge.java
@@ -5,29 +5,39 @@ import com.walkhub.walkhub.domain.badge.domain.repository.BadgeRepository;
 import com.walkhub.walkhub.domain.badge.domain.repository.vo.DefaultBadgeVO;
 import com.walkhub.walkhub.domain.badge.enums.BadgeType;
 import com.walkhub.walkhub.domain.badge.exception.BadgeNotFoundException;
+import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-public class Day0000StepsBadge implements BaseBadge {
+public class Day10000StepsBadge implements BaseBadge {
 
     private final BadgeRepository badgeRepository;
+    private final UserFacade userFacade;
+    private final ExerciseAnalysisRepository exerciseAnalysisRepository;
 
     @Override
     public Boolean hasBadge(List<DefaultBadgeVO> userOwnBadge) {
         return userOwnBadge.stream()
-                .anyMatch(defaultBadgeVO -> defaultBadgeVO.getCode() == BadgeType.DAY_10000_steps);
+                .anyMatch(defaultBadgeVO -> defaultBadgeVO.getCode() == BadgeType.DAY_10000_STEPS);
     }
 
     @Override
     public Badge getBadgeEntity() {
-        return badgeRepository.findByCode(BadgeType.NEWBIE)
+        return badgeRepository.findByCode(BadgeType.DAY_10000_STEPS)
                 .orElseThrow(() -> BadgeNotFoundException.EXCEPTION);
     }
 
     @Override
     public Boolean isGoalSuccess() {
-        return null;
+        User user = userFacade.getCurrentUser();
+
+        Integer walkCount =
+                exerciseAnalysisRepository.countAllByUserAndWalkCountGreaterThanEqual(user, 10000);
+
+        return walkCount >= 1;
     }
 }
