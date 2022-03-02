924bdc6213e00036ad7867e77553e7acc8d33a7e
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-02 19:38:19 +0900

    ⚡️ :: BetaTesterBadge, Day10000StepsBadge, Day30000StepsBadge, Day50000StepsBadge 추가

--- src/main/java/com/walkhub/walkhub/domain/badge/badges/BadgeFactory.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/badges/BadgeFactory.java
@@ -2,6 +2,8 @@ package com.walkhub.walkhub.domain.badge.badges;
 
 import com.walkhub.walkhub.domain.badge.domain.repository.BadgeRepository;
 import com.walkhub.walkhub.domain.badge.enums.BadgeType;
+import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +12,21 @@ import org.springframework.stereotype.Component;
 public class BadgeFactory {
 
     private final BadgeRepository badgeRepository;
+    private final UserFacade userFacade;
+    private final ExerciseAnalysisRepository exerciseAnalysisRepository;
 
     public BaseBadge getBadge(BadgeType badgeType) {
         switch (badgeType) {
             case NEWBIE:
                 return new StartBadge(badgeRepository);
+            case BETA_TESTER:
+                return new BetaTesterBadge(badgeRepository);
+            case DAY_10000_STEPS:
+                return new Day10000StepsBadge(badgeRepository, userFacade, exerciseAnalysisRepository);
+            case DAY_30000_STEPS:
+                return new Day30000StepsBadge(badgeRepository, userFacade, exerciseAnalysisRepository);
+            case DAY_50000_STEPS:
+                return new Day50000StepsBadge(badgeRepository, userFacade, exerciseAnalysisRepository);
             default:
                 return null;
         }
