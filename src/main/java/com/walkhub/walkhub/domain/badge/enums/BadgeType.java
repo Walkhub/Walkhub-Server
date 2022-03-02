3638211c6c429205db67efa5ff87c85cfb618404
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-02 16:39:03 +0900

    :bookmark_tabs: :: BadgeType

---
+++ src/main/java/com/walkhub/walkhub/domain/badge/enums/BadgeType.java
@@ -0,0 +1,22 @@
+package com.walkhub.walkhub.domain.badge.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum BadgeType {
+
+    NEWBIE("newbie"),
+    BETA_TESTER("beta_tester"),
+    DAY_10000_steps("day_10000_steps"),
+    DAY_30000_steps("day_30000_steps"),
+    DAY_50000_steps("day_50000_steps"),
+    FROZEN_HUMAN("frozen_human"),
+    GO_JIN_HO("go_jin_ho"),
+    SLIVER_SHOES("sliver_shoes"),
+    GOLD_SHOES("gold_shoes"),
+    MARATHON("marathon");
+
+    private final String code;
+}
