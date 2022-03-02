47ebf78f86e4b677d21511e264936abd28b7d3dd
김범진 <sdpthf@naver.com>
2022-03-03 00:52:31 +0900

    ⚡ :: (#506) 운동 종료 시 user isMeasuring = false

--- src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/request/CreateExerciseRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/request/CreateExerciseRequest.java
@@ -1,7 +1,9 @@
 package com.walkhub.walkhub.domain.exercise.presentation.dto.request;
 
 import com.walkhub.walkhub.domain.exercise.domain.type.GoalType;
+
 import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,9 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateExerciseRequest {
 
-	private Integer goal;
+    @NotNull(message = "goal은 null, 공백을 허용하지 않습니다.")
+    private Integer goal;
 
-	@NotNull(message = "goal_type은 null, 공백을 허용x")
-	private GoalType goalType;
+    @NotNull(message = "goal_type은 null, 공백을 허용하지 않습니다.")
+    private GoalType goalType;
 
 }
