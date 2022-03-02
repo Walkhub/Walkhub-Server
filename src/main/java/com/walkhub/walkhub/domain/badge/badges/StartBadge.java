cc84a42988a121f3ba70d31f7285fa7f1d0ac413
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-02 16:38:07 +0900

    :bookmark_tabs: :: StartBadge

---
+++ src/main/java/com/walkhub/walkhub/domain/badge/badges/StartBadge.java
@@ -0,0 +1,34 @@
+package com.walkhub.walkhub.domain.badge.badges;
+
+import com.walkhub.walkhub.domain.badge.domain.Badge;
+import com.walkhub.walkhub.domain.badge.domain.repository.BadgeRepository;
+import com.walkhub.walkhub.domain.badge.domain.repository.vo.DefaultBadgeVO;
+import com.walkhub.walkhub.domain.badge.enums.BadgeType;
+import com.walkhub.walkhub.domain.badge.exception.BadgeNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class StartBadge implements BaseBadge {
+
+    private final BadgeRepository badgeRepository;
+
+    @Override
+    public Boolean hasBadge(List<DefaultBadgeVO> userOwnBadge) {
+        return userOwnBadge.stream()
+                .anyMatch(defaultBadgeVO -> defaultBadgeVO.getBadgeType() == BadgeType.NEWBIE);
+    }
+
+    @Override
+    public Badge getBadgeEntity() {
+        return badgeRepository.findByCode(BadgeType.NEWBIE)
+                .orElseThrow(() -> BadgeNotFoundException.EXCEPTION);
+    }
+
+    @Override
+    public Boolean isGoalSuccess() {
+        return true;
+    }
+
+}
