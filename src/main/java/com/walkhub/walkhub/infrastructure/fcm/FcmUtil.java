a0ae4e00f2e087d4cf24d5b7741ab60455f8ebca
lyutvs <lyutvs@gmail.com>
2022-03-03 00:36:15 +0900

    ⚡️:: 알림 켜기, 끄기 추가

--- src/main/java/com/walkhub/walkhub/infrastructure/fcm/FcmUtil.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/fcm/FcmUtil.java
@@ -1,10 +1,20 @@
 package com.walkhub.walkhub.infrastructure.fcm;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.infrastructure.fcm.dto.request.NotificationRequest;
+
+import java.util.List;
 
 public interface FcmUtil {
 
     void initialize();
 
     void sendNotification(NotificationRequest sendDto);
+
+
+    void subscribeTopic(List<User> users, NotificationRequest request) throws FirebaseMessagingException;
+
+
+    void unSubscribeTopic(List<User> users, NotificationRequest request) throws FirebaseMessagingException;
 }
