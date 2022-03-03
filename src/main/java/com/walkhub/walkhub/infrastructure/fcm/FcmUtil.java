fb7483d6dd89b155b530bf1778da042d3da4c22c
lyutvs <lyutvs@gmail.com>
2022-03-03 12:05:39 +0900

    ⚡️:: FcmUtil

--- src/main/java/com/walkhub/walkhub/infrastructure/fcm/FcmUtil.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/fcm/FcmUtil.java
@@ -1,6 +1,8 @@
 package com.walkhub.walkhub.infrastructure.fcm;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.walkhub.walkhub.domain.challenge.domain.Challenge;
+import com.walkhub.walkhub.domain.notice.domain.Notice;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.infrastructure.fcm.dto.request.NotificationRequest;
 
@@ -9,12 +11,16 @@ import java.util.List;
 public interface FcmUtil {
 
     void initialize();
-
     void sendNotification(NotificationRequest sendDto);
-
-
     void subscribeTopic(List<User> users, NotificationRequest request) throws FirebaseMessagingException;
-
-
     void unSubscribeTopic(List<User> users, NotificationRequest request) throws FirebaseMessagingException;
+
+    void sendNoticeNotification(Notice notice, ContentType contentType);
+
+    void sendCheeringNotification();
+
+    void sendChallengeExpirationNotification(Challenge challenge, ContentType contentType);
+    void sendChallengeSuccessNotification(Challenge challenge, ContentType contentType);
+    void sendChallengeCanParticipate(Challenge challenge, ContentType contentType);
+
 }
