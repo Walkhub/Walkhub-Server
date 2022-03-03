66d022e3cc89223e318670bfcde3d6df493313db
lyutvs <lyutvs@gmail.com>
2022-03-03 12:05:27 +0900

    📑 :: NotificationInformation

---
+++ src/main/java/com/walkhub/walkhub/infrastructure/fcm/dto/request/NotificationInformation.java
@@ -0,0 +1,33 @@
+package com.walkhub.walkhub.infrastructure.fcm.dto.request;
+
+import com.walkhub.walkhub.domain.challenge.domain.Challenge;
+import com.walkhub.walkhub.domain.notice.domain.Notice;
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.global.enums.UserScope;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NotificationInformation {
+
+    private User user;
+
+    private String title;
+
+    private UserScope scope;
+
+    private String data;
+
+    public static NotificationInformation challengeNotificationInformation(Challenge challenge) {
+        return new NotificationInformation(challenge.getUser(), challenge.getName(),
+                UserScope.valueOf(challenge.getUserScope().toString()), challenge.getId().toString());
+    }
+
+    public static NotificationInformation noticeNotificationInformation(Notice notice) {
+        return new NotificationInformation(notice.getUser(), notice.getTitle(),
+                UserScope.valueOf(notice.getScope().toString()), notice.getId().toString());
+    }
+
+
+}
