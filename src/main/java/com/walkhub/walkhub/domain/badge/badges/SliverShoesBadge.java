c48f56e1ed57437a8ffb7874fe87b879ae13b3b3
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-03-03 13:47:05 +0900

    :bookmark_tabs: :: SliverShoesBadge

---
+++ src/main/java/com/walkhub/walkhub/domain/badge/badges/SliverShoesBadge.java
@@ -0,0 +1,42 @@
+package com.walkhub.walkhub.domain.badge.badges;
+
+import com.walkhub.walkhub.domain.badge.domain.Badge;
+import com.walkhub.walkhub.domain.badge.domain.repository.BadgeRepository;
+import com.walkhub.walkhub.domain.badge.domain.repository.vo.DefaultBadgeVO;
+import com.walkhub.walkhub.domain.badge.enums.BadgeType;
+import com.walkhub.walkhub.domain.badge.exception.BadgeNotFoundException;
+import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
+import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class SliverShoesBadge implements BaseBadge {
+
+    private final BadgeRepository badgeRepository;
+    private final UserFacade userFacade;
+    private final ExerciseAnalysisRepository exerciseAnalysisRepository;
+
+    @Override
+    public boolean hasBadge(List<DefaultBadgeVO> userNotOwnBadge) {
+        return userNotOwnBadge.stream()
+                .noneMatch(defaultBadgeVO -> defaultBadgeVO.getCode() == BadgeType.SLIVER_SHOES);
+    }
+
+    @Override
+    public Badge getBadgeEntity() {
+        return badgeRepository.findByCode(BadgeType.SLIVER_SHOES)
+                .orElseThrow(() -> BadgeNotFoundException.EXCEPTION);
+    }
+
+    @Override
+    public boolean isGoalSuccess() {
+        User user = userFacade.getCurrentUser();
+
+        Integer walkCount = exerciseAnalysisRepository.sumWalkCountByUserId(user);
+
+        return walkCount >= 100000;
+    }
+}
