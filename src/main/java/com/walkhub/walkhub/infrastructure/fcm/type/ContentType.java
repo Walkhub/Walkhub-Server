0a56510df063784749a6ee5fc9bc2cc69f50f27e
lyutvs <lyutvs@gmail.com>
2022-03-06 17:37:47 +0900

    ♻️:: 파일 위치 변경

--- src/main/java/com/walkhub/walkhub/infrastructure/fcm/type/ContentType.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/fcm/type/ContentType.java
@@ -1,4 +1,4 @@
-package com.walkhub.walkhub.infrastructure.fcm;
+package com.walkhub.walkhub.infrastructure.fcm.type;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum ContentType {
 
     EXPIRED_CHALLENGE("챌린지가 기간 만료되었습니다."),
-    SUCCESS_CHALLENGE("챌린지 성공 하였습니다."),
-    CAN_PARTICIPATE_CHALLENGE("챌린지를 참여할수 있습니다."),
+    SUCCESS_CHALLENGE("챌린지 성공하였습니다."),
+    CAN_PARTICIPATE_CHALLENGE("챌린지를 참여할 수 있습니다."),
 
     CREATE_NOTICE("공지사항이 등록되었습니다.");
 
