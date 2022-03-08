4908d6d2890c610999fcddb08564dd1a023450ea
Lee SeoJune <sung07288346@gmail.com>
2022-03-08 23:41:03 +0900

    ⚡️ :: Sort

--- src/main/java/com/walkhub/walkhub/domain/calorielevel/domain/repository/CalorieLevelRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/calorielevel/domain/repository/CalorieLevelRepository.java
@@ -2,12 +2,14 @@ package com.walkhub.walkhub.domain.calorielevel.domain.repository;
 
 import com.walkhub.walkhub.domain.calorielevel.domain.CalorieLevel;
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CalorieLevelRepository extends CrudRepository<CalorieLevel, Long> {
-    List<CalorieLevel> findAllBy();
+public interface CalorieLevelRepository extends JpaRepository<CalorieLevel, Long> {
+    List<CalorieLevel> findAllBy(Sort sort);
 
     Optional<CalorieLevel> findByLevel(Integer level);
 }
