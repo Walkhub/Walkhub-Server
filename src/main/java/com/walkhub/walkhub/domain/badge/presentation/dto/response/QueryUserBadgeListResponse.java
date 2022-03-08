6b8b1eb893cc77d14de380f7bea382aa6bd056a7
Lee SeoJune <sung07288346@gmail.com>
2022-03-08 13:45:37 +0900

    ⚡️ :: code smell

--- src/main/java/com/walkhub/walkhub/domain/badge/presentation/dto/response/QueryUserBadgeListResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/presentation/dto/response/QueryUserBadgeListResponse.java
@@ -1,6 +1,5 @@
 package com.walkhub.walkhub.domain.badge.presentation.dto.response;
 
-import com.walkhub.walkhub.domain.badge.enums.BadgeType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
