23b13767f155d0bbc6b001ba62957140272550d0
Lee SeoJune <sung07288346@gmail.com>
2022-03-08 10:16:16 +0900

    ⚡️ :: dto 분리

--- src/main/java/com/walkhub/walkhub/domain/badge/presentation/dto/response/QueryMyBadgeListResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/presentation/dto/response/QueryMyBadgeListResponse.java
@@ -1,7 +1,9 @@
 package com.walkhub.walkhub.domain.badge.presentation.dto.response;
 
 import com.walkhub.walkhub.domain.badge.domain.repository.vo.MyBadgeVo;
+import com.walkhub.walkhub.domain.badge.enums.BadgeType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
@@ -9,5 +11,16 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class QueryMyBadgeListResponse {
-    private final List<MyBadgeVo> badgeLists;
+    private final List<MyBadge> badgeLists;
+
+    @Getter
+    @Builder
+    public static class MyBadge {
+        private final Long badgeId;
+        private final String name;
+        private final String imageUrl;
+        private final BadgeType code;
+        private final Boolean isMine;
+        private final String condition;
+    }
 }
