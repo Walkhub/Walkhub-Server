d7045828eb5356acfbb9f5f2f18a4faf3b0f01ca
Lee SeoJune <sung07288346@gmail.com>
2022-03-08 14:48:53 +0900

    ⚡️ :: findByClassCode

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/SectionRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/SectionRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface SectionRepository extends JpaRepository<Section, Long> {
     Optional<Section> findByClassCodeAndSchool(String classCode, School school);
     Optional<Section> findBySchoolAndGradeAndClassNum(School school, Integer grade, Integer classNum);
+    Optional<Section> findByClassCode(String classCode);
 }
