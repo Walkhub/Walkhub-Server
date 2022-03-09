ad796c803e0f920c91de297ff297484b4a42268b
Leeseojune <sung07288346@gmail.com>
2022-03-09 15:00:52 +0900

    ⚡️ :: method names
    Co-authored-by: 홍정현 <57659252+jhhong0509@users.noreply.github.com>

--- src/main/java/com/walkhub/walkhub/domain/calorielevel/domain/repository/CalorieLevelRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/calorielevel/domain/repository/CalorieLevelRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CalorieLevelRepository extends JpaRepository<CalorieLevel, Long> {
-    List<CalorieLevel> findAllBy(Sort sort);
+    List<CalorieLevel> findAllByOrderByLevelAsc();
 
     Optional<CalorieLevel> findByLevel(Integer level);
 }
