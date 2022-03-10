c6e84347593e3ad2e7a51b434ff6c0a248fce3f2
Lee SeoJune <sung07288346@gmail.com>
2022-03-10 16:15:51 +0900

    ⚡️ :: endpoint /lists/students

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
@@ -42,7 +42,7 @@ public class ChallengeController {
         createChallengeService.execute(request);
     }
 
-    @GetMapping("/lists")
+    @GetMapping("/lists/students")
     public QueryChallengeListResponse queryChallengeList() {
         return queryChallengeListService.execute();
     }
