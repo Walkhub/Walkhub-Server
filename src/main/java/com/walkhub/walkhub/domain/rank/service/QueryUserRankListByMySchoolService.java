08b127c36fdb1413b65c400aa1d1eacfefaff8be
parksangwoo <b01056721617@gmail.com>
2022-03-18 09:54:09 +0900

    ♻️ :: codesmell 해결

--- src/main/java/com/walkhub/walkhub/domain/rank/service/QueryUserRankListByMySchoolService.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/service/QueryUserRankListByMySchoolService.java
@@ -20,7 +20,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
