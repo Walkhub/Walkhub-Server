93a90875b2c90888761d843cde9968cf0fad1010
Leeseojune <sung07288346@gmail.com>
2022-03-24 15:39:38 +0900

    ⚡️ :: RequestParam

--- src/main/java/com/walkhub/walkhub/domain/exercise/presentation/ExerciseController.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/ExerciseController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -77,7 +78,7 @@ public class ExerciseController {
     }
 
     @GetMapping("/lists")
-    public ExerciseListResponse queryExerciseList(int page) {
+    public ExerciseListResponse queryExerciseList(@RequestParam int page) {
         return queryExerciseListService.execute(page);
     }
 
