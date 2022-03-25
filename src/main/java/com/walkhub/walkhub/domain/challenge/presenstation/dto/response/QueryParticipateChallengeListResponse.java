8e7c46e8395bbcb087b70a2f000c455700dd4ae7
김범진 <sdpthf@naver.com>
2022-03-25 22:51:21 +0900

    📑 :: (#570) QueryParticipateChallengeListResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/response/QueryParticipateChallengeListResponse.java
@@ -0,0 +1,39 @@
+package com.walkhub.walkhub.domain.challenge.presenstation.dto.response;
+
+import com.walkhub.walkhub.domain.challenge.domain.type.GoalScope;
+import com.walkhub.walkhub.domain.exercise.domain.type.GoalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class QueryParticipateChallengeListResponse {
+
+    private final List<ChallengeResponse> challengeList;
+
+    @Getter
+    @Builder
+    public static class ChallengeResponse {
+        private final Long id;
+        private final String name;
+        private final LocalDate startAt;
+        private final LocalDate endAt;
+        private final Integer goal;
+        private final GoalScope goalScope;
+        private final GoalType goalType;
+        private final Integer totalWalkCount;
+        private final Writer writer;
+    }
+
+    @Getter
+    @Builder
+    public static class Writer {
+        private final Long userId;
+        private final String name;
+        private final String profileImageUrl;
+    }
+}
