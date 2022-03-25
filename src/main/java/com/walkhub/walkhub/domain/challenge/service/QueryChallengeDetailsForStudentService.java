529f9c4800ea6ce78f03c247eef91977d949ec1c
김범진 <sdpthf@naver.com>
2022-03-25 23:20:01 +0900

    ⚡ :: (#570) 챌린지 상세보기 학생용 로직 추가

--- src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeDetailsForStudentService.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/service/QueryChallengeDetailsForStudentService.java
@@ -5,10 +5,14 @@ import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeRepositor
 import com.walkhub.walkhub.domain.challenge.domain.repository.vo.ChallengeDetailsForStudentVO;
 import com.walkhub.walkhub.domain.challenge.facade.ChallengeFacade;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeDetailsForStudentResponse;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeDetailsForStudentResponse.Participant;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeDetailsForStudentResponse.Writer;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.global.annotation.ServiceWithTransactionalReadOnly;
 import lombok.RequiredArgsConstructor;
+
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @ServiceWithTransactionalReadOnly
@@ -22,6 +26,39 @@ public class QueryChallengeDetailsForStudentService {
         Challenge challenge = challengeFacade.getChallengeById(challengeId);
         User user = userFacade.getCurrentUser();
 
-        ChallengeDetailsForStudentVO challengeDetailsForStudentVO = challengeRepository.queryChallengeDetails(challenge, user);
+        ChallengeDetailsForStudentVO vo = challengeRepository.queryChallengeDetails(challenge, user);
+        return buildResponse(vo);
+    }
+
+    private QueryChallengeDetailsForStudentResponse buildResponse(ChallengeDetailsForStudentVO vo) {
+        return QueryChallengeDetailsForStudentResponse.builder()
+                .name(vo.getName())
+                .content(vo.getContent())
+                .userScope(vo.getUserScope())
+                .goal(vo.getGoal())
+                .goalScope(vo.getGoalScope())
+                .goalType(vo.getGoalType())
+                .award(vo.getAward())
+                .imageUrl(vo.getImageUrl())
+                .startAt(vo.getStartAt())
+                .endAt(vo.getEndAt())
+                .successStandard(vo.getSuccessStandard())
+                .writer(Writer.builder()
+                        .userId(vo.getWriterUserId())
+                        .name(vo.getWriterName())
+                        .profileImageUrl(vo.getWriterProfileImageUrl())
+                        .build())
+                .isMine(vo.getIsMine())
+                .isParticipated(vo.getIsParticipated())
+                .participantCount(vo.getParticipantCount())
+                .participantList(vo.getParticipantList()
+                        .stream()
+                        .map(participant -> Participant.builder()
+                                .userId(participant.getUserId())
+                                .name(participant.getName())
+                                .profileImageUrl(participant.getProfileImageUrl())
+                                .build())
+                        .collect(Collectors.toList()))
+                .build();
     }
 }
