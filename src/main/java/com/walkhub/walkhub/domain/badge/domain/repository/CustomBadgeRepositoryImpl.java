82c8d12bc1cecc881a473b4d2aee94cf26e3dd7d
leeseojune <sung07288346@gmail.com>
2022-03-26 00:56:15 +0900

    ⚰ :: Repository annotation

--- src/main/java/com/walkhub/walkhub/domain/badge/domain/repository/CustomBadgeRepositoryImpl.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/domain/repository/CustomBadgeRepositoryImpl.java
@@ -6,7 +6,6 @@ import com.walkhub.walkhub.domain.badge.domain.repository.vo.MyBadgeVo;
 import com.walkhub.walkhub.domain.badge.domain.repository.vo.QDefaultBadgeVO;
 import com.walkhub.walkhub.domain.badge.domain.repository.vo.QMyBadgeVo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -15,7 +14,6 @@ import static com.walkhub.walkhub.domain.badge.domain.QBadgeCollection.badgeColl
 import static com.walkhub.walkhub.domain.user.domain.QUser.user;
 
 @RequiredArgsConstructor
-@Repository
 public class CustomBadgeRepositoryImpl implements CustomBadgeRepository {
 
     private final JPAQueryFactory query;
--- src/main/java/com/walkhub/walkhub/domain/exercise/cache/ExerciseAnalysisCacheRepositoryImpl.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/cache/ExerciseAnalysisCacheRepositoryImpl.java
@@ -3,7 +3,6 @@ package com.walkhub.walkhub.domain.exercise.cache;
 import com.walkhub.walkhub.domain.exercise.exception.RedisTransactionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ZSetOperations;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -14,7 +13,6 @@ import java.util.Objects;
 import java.util.Set;
 
 @RequiredArgsConstructor
-@Repository
 public class ExerciseAnalysisCacheRepositoryImpl implements ExerciseAnalysisCacheRepository {
 
     public static final String EXERCISE_ANALYSIS_KEY = "exercise_analysis_";
