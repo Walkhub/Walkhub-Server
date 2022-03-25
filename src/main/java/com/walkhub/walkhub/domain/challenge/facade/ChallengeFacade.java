30ebc63e7ba97d11a042d3930c4c4b1c51241595
김범진 <sdpthf@naver.com>
2022-03-25 22:53:53 +0900

    ♻ :: (#570) 메소드 분리

--- src/main/java/com/walkhub/walkhub/domain/challenge/facade/ChallengeFacade.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/facade/ChallengeFacade.java
@@ -3,10 +3,7 @@ package com.walkhub.walkhub.domain.challenge.facade;
 
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeRepository;
-import com.walkhub.walkhub.domain.challenge.domain.repository.vo.ShowChallengeVO;
 import com.walkhub.walkhub.domain.challenge.exception.ChallengeNotFoundException;
-import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeListResponse.ChallengeResponse;
-import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeListResponse.Writer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -21,21 +18,4 @@ public class ChallengeFacade {
                 .orElseThrow(() -> ChallengeNotFoundException.EXCEPTION);
     }
 
-    public ChallengeResponse challengeResponseBuilder(ShowChallengeVO vo) {
-        return ChallengeResponse.builder()
-                .challengeId(vo.getChallengeId())
-                .name(vo.getName())
-                .startAt(vo.getStartAt())
-                .endAt(vo.getEndAt())
-                .goalScope(vo.getGoalScope())
-                .goalType(vo.getGoalType())
-                .writer(Writer.builder()
-                        .userId(vo.getChallengeId())
-                        .name(vo.getWriterName())
-                        .profileImageUrl(vo.getProfileImageUrl())
-                        .build())
-                .goal(vo.getGoal())
-                .award(vo.getAward())
-                .build();
-    }
 }
