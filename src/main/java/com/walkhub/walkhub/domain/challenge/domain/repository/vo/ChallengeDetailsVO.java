0a8e7da4b1e582db9145f620175da199341a1291
김범진 <sdpthf@naver.com>
2022-03-25 22:52:26 +0900

    📑 :: (#570) ChallengeDetailsVO

---
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/vo/ChallengeDetailsVO.java
@@ -0,0 +1,60 @@
+package com.walkhub.walkhub.domain.challenge.domain.repository.vo;
+
+import com.querydsl.core.annotations.QueryProjection;
+import com.walkhub.walkhub.domain.challenge.domain.type.GoalScope;
+import com.walkhub.walkhub.domain.exercise.domain.type.GoalType;
+import com.walkhub.walkhub.global.enums.UserScope;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+public class ChallengeDetailsVO {
+
+    private final String name;
+    private final String content;
+    private final UserScope userScope;
+    private final Integer goal;
+    private final GoalScope goalScope;
+    private final GoalType goalType;
+    private final String award;
+    private final String imageUrl;
+    private final LocalDate startAt;
+    private final LocalDate endAt;
+    private final Integer successStandard;
+    private final Long writerUserId;
+    private final String writerName;
+    private final String writerProfileImageUrl;
+    private final Boolean isMine;
+    private final Boolean isParticipated;
+    private final Long participantCount;
+    private final List<RelatedChallengeParticipantsVO> participantList;
+
+    @QueryProjection
+    public ChallengeDetailsVO(String name, String content, UserScope userScope,
+                              Integer goal, GoalScope goalScope, GoalType goalType,
+                              String award, String imageUrl, LocalDate startAt, LocalDate endAt, Integer successStandard,
+                              Long writerUserId, String writerName, String writerProfileImageUrl,
+                              Boolean isMine, Boolean isParticipated,
+                              Long participantCount, List<RelatedChallengeParticipantsVO> participantList) {
+        this.name = name;
+        this.content = content;
+        this.userScope = userScope;
+        this.goal = goal;
+        this.goalScope = goalScope;
+        this.goalType = goalType;
+        this.award = award;
+        this.imageUrl = imageUrl;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.successStandard = successStandard;
+        this.writerUserId = writerUserId;
+        this.writerName = writerName;
+        this.writerProfileImageUrl = writerProfileImageUrl;
+        this.isMine = isMine;
+        this.isParticipated = isParticipated;
+        this.participantCount = participantCount;
+        this.participantList = participantList;
+    }
+}
