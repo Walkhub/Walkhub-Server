60a04e909a9aafb0cae52464644784d2050d41f1
lyutvs <lyutvs@gmail.com>
2022-03-29 10:16:30 +0900

    ⚡️ :: Type 추가

--- src/main/java/com/walkhub/walkhub/domain/notification/domain/type/Type.java
+++ src/main/java/com/walkhub/walkhub/domain/notification/domain/type/Type.java
@@ -3,6 +3,8 @@ package com.walkhub.walkhub.domain.notification.domain.type;
 public enum Type {
     NOTICE,
     CHALLENGE,
+    CHALLENGE_EXPIRATION,
+    CHALLENGE_SUCCESS,
     EXERCISE,
     CHEERING
 }
