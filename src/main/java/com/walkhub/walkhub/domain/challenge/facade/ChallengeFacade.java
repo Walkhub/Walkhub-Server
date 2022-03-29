c86519f8e7e68b4bc16be2cc6e2a0dcff51902a4
김범진 <sdpthf@naver.com>
2022-03-29 12:57:26 +0900

    ⚡ :: (#570) personBuilder

--- src/main/java/com/walkhub/walkhub/domain/challenge/facade/ChallengeFacade.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/facade/ChallengeFacade.java
@@ -4,6 +4,7 @@ package com.walkhub.walkhub.domain.challenge.facade;
 import com.walkhub.walkhub.domain.challenge.domain.Challenge;
 import com.walkhub.walkhub.domain.challenge.domain.repository.ChallengeRepository;
 import com.walkhub.walkhub.domain.challenge.exception.ChallengeNotFoundException;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.PersonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -18,4 +19,11 @@ public class ChallengeFacade {
                 .orElseThrow(() -> ChallengeNotFoundException.EXCEPTION);
     }
 
+    public PersonResponse personBuilder(Long userId, String name, String profileUrl) {
+        return PersonResponse.builder()
+                .userId(userId)
+                .name(name)
+                .profileImageUrl(profileUrl)
+                .build();
+    }
 }
