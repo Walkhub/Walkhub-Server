f3fb0feba461a1e8dca8a22497eb10f7209cde8f
lyutvs <lyutvs@gmail.com>
2022-03-29 10:16:56 +0900

    ⚡️:: NotificationController

--- src/main/java/com/walkhub/walkhub/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/walkhub/walkhub/domain/notification/presentation/NotificationController.java
@@ -1,5 +1,6 @@
 package com.walkhub.walkhub.domain.notification.presentation;
 
+import com.walkhub.walkhub.domain.notification.presentation.dto.request.SubscribeRequest;
 import com.walkhub.walkhub.domain.notification.presentation.dto.response.QueryNotificationListResponse;
 import com.walkhub.walkhub.domain.notification.service.NotificationReadService;
 import com.walkhub.walkhub.domain.notification.service.QueryNotificationListService;
@@ -9,13 +10,9 @@ import com.walkhub.walkhub.infrastructure.fcm.dto.request.NotificationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -39,13 +36,13 @@ public class NotificationController {
     }
 
     @PatchMapping
-    public void unSubscribeTopic(List<User> users, NotificationRequest request) {
-        fcmUtil.unSubscribeTopic(users, request);
+    public void unSubscribeTopic(@RequestBody @Valid SubscribeRequest userRequest, NotificationRequest request) {
+        fcmUtil.unSubscribeTopic(userRequest.getUsers(), request);
     }
 
     @PatchMapping("/on")
-    public void subscribeTopic(List<User> users, NotificationRequest request) {
-        fcmUtil.subscribeTopic(users, request);
+    public void subscribeTopic(@RequestBody @Valid SubscribeRequest userRequest, NotificationRequest request) {
+        fcmUtil.subscribeTopic(userRequest.getUsers(), request);
     }
 
 }
