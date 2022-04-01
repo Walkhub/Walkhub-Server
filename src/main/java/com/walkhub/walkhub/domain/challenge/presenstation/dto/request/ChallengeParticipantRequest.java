c7f6940a8f8ff6dd09b72d933d443ec5a45b8abe
정대현 <eogus_2513@dsm.hs.kr>
2022-04-01 09:45:06 +0900

    :recycle: :: 기본 생성자로 변경

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/request/ChallengeParticipantRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/request/ChallengeParticipantRequest.java
@@ -2,18 +2,18 @@ package com.walkhub.walkhub.domain.challenge.presenstation.dto.request;
 
 import com.walkhub.walkhub.domain.challenge.domain.type.ChallengeParticipantsOrder;
 import com.walkhub.walkhub.domain.challenge.domain.type.ChallengeParticipantsScope;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeParticipantRequest {
 
-    private final String keyword;
-    private final ChallengeParticipantsOrder sort;
-    private final ChallengeParticipantsScope userScope;
-    private final Integer grade;
-    private final Integer classNum;
-    private final Long size;
+    private String keyword;
+    private ChallengeParticipantsOrder sort;
+    private ChallengeParticipantsScope userScope;
+    private Integer grade;
+    private Integer classNum;
+    private Long size;
 
 }
