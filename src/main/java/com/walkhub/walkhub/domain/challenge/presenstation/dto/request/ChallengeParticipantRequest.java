107921269a92b380514aff29ec212652f7917ae2
정대현 <eogus_2513@dsm.hs.kr>
2022-04-01 14:21:26 +0900

    :recycle: :: @AllArgsConstructor, final 추가

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/request/ChallengeParticipantRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/request/ChallengeParticipantRequest.java
@@ -2,26 +2,27 @@ package com.walkhub.walkhub.domain.challenge.presenstation.dto.request;
 
 import com.walkhub.walkhub.domain.challenge.domain.type.ChallengeParticipantsOrder;
 import com.walkhub.walkhub.domain.challenge.domain.type.ChallengeParticipantsScope;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class ChallengeParticipantRequest {
 
-    private String keyword;
+    private final String keyword;
 
     @NotNull(message = "sort는 Null일 수 없습니다.")
-    private ChallengeParticipantsOrder sort;
+    private final ChallengeParticipantsOrder sort;
 
     @NotNull(message = "userScope는 Null일 수 없습니다.")
-    private ChallengeParticipantsScope userScope;
+    private final ChallengeParticipantsScope userScope;
 
-    private Integer grade;
+    private final Integer grade;
 
-    private Integer classNum;
+    private final Integer classNum;
 
     @NotNull(message = "size는 Null일 수 없습니다.")
     private Long size;
