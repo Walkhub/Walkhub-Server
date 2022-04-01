6bf18a88b0a6da921bf9e0d6a2ca446deace72a5
정대현 <eogus_2513@dsm.hs.kr>
2022-04-01 09:46:48 +0900

    :recycle: :: message 추가

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/request/ChallengeParticipantRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/request/ChallengeParticipantRequest.java
@@ -5,15 +5,25 @@ import com.walkhub.walkhub.domain.challenge.domain.type.ChallengeParticipantsSco
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class ChallengeParticipantRequest {
 
     private String keyword;
+
+    @NotNull(message = "sort는 Null일 수 없습니다.")
     private ChallengeParticipantsOrder sort;
+
+    @NotNull(message = "userScope는 Null일 수 없습니다.")
     private ChallengeParticipantsScope userScope;
+
     private Integer grade;
+
     private Integer classNum;
+
+    @NotNull(message = "size는 Null일 수 없습니다.")
     private Long size;
 
 }
