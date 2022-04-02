f1f7f9c965cb28f2b9a87421430958ca246a54ee
이준서 <ljchan71@gmail.com>
2022-04-03 00:16:44 +0900

    ⚡ :: SchoolListResponse

--- src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/SchoolListResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/presentation/dto/response/SchoolListResponse.java
@@ -1,5 +1,6 @@
 package com.walkhub.walkhub.domain.rank.presentation.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,5 +21,17 @@ public class SchoolListResponse {
         private final Integer ranking;
         private final String logoImageUrl;
         private final Integer walkCount;
+        private final Long userCount;
+
+        @QueryProjection
+        public SchoolResponse(Long schoolId, String schoolName, Integer ranking,
+            String logoImageUrl, Integer walkCount, Long userCount) {
+            this.schoolId = schoolId;
+            this.schoolName = schoolName;
+            this.ranking = ranking;
+            this.logoImageUrl = logoImageUrl;
+            this.walkCount = walkCount;
+            this.userCount = userCount;
+        }
     }
 }
