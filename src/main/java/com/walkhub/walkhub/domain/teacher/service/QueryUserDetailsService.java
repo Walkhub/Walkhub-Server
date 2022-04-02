4385da11ee8a461e027226da3d354d0be23e02a2
김범진 <sdpthf@naver.com>
2022-04-02 14:15:58 +0900

    ⚡ :: 유저 상세보기 서비스 로직 변경

--- src/main/java/com/walkhub/walkhub/domain/teacher/service/QueryUserDetailsService.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/service/QueryUserDetailsService.java
@@ -1,55 +1,41 @@
 package com.walkhub.walkhub.domain.teacher.service;
 
-import com.walkhub.walkhub.domain.exercise.domain.ExerciseAnalysis;
-import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
-import com.walkhub.walkhub.domain.exercise.domain.repository.LocationRepository;
 import com.walkhub.walkhub.domain.teacher.presentation.dto.response.QueryUserDetailsResponse;
-import com.walkhub.walkhub.domain.teacher.presentation.dto.response.QueryUserDetailsResponse.ExerciseResponse;
+import com.walkhub.walkhub.domain.user.domain.Section;
 import com.walkhub.walkhub.domain.user.domain.User;
+import com.walkhub.walkhub.domain.user.domain.repository.UserRepository;
+import com.walkhub.walkhub.domain.user.domain.repository.vo.UserDetailsVO;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.global.annotation.ServiceWithTransactionalReadOnly;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @ServiceWithTransactionalReadOnly
 public class QueryUserDetailsService {
 
     private final UserFacade userFacade;
-    private final LocationRepository locationRepository;
-    private final ExerciseAnalysisRepository exerciseAnalysisRepository;
+    private final UserRepository userRepository;
 
     public QueryUserDetailsResponse execute(Long userId, LocalDate startAt, LocalDate endAt) {
         User user = userFacade.getUserById(userId);
+        Section section = user.hasSection() ? user.getSection() : Section.builder().build();
 
-        List<ExerciseResponse> exerciseList = user.getExerciseList()
-                .stream()
-                .map(locationRepository::findTop1ByExerciseOrderBySequenceDesc)
-                .map(location -> ExerciseResponse.builder()
-                        .imageUrl(location.getExercise().getImageUrl())
-                        .startAt(location.getExercise().getCreatedAt())
-                        .latitude(location.getLatitude())
-                        .longitude(location.getLongitude())
-                        .build())
-                .collect(Collectors.toList());
-
-        List<Integer> walkCountList = exerciseAnalysisRepository.findAllByUserAndDateBetween(user, startAt, endAt)
-                .stream()
-                .map(ExerciseAnalysis::getWalkCount)
-                .collect(Collectors.toList());
+        UserDetailsVO vo = userRepository.queryUserDetails(userId, startAt, endAt);
 
         return QueryUserDetailsResponse.builder()
                 .userId(user.getId())
                 .name(user.getName())
                 .profileImageUrl(user.getProfileImageUrl())
-                .grade(user.getSection().getGrade())
-                .classNum(user.getSection().getClassNum())
+                .grade(section.getGrade())
+                .classNum(section.getClassNum())
                 .number(user.getNumber())
-                .walkCountList(walkCountList)
-                .exerciseList(exerciseList)
+                .walkCountList(vo.getWalkCountList())
+                .averageWalkCount(vo.getAverageWalkCount())
+                .totalWalkCount(vo.getTotalWalkCount())
+                .averageDistance(vo.getAverageDistance())
+                .totalDistance(vo.getTotalDistance())
                 .build();
     }
 }
