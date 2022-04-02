e6679c14d2e1c301b357760e062335dc3ccc6d00
김범진 <sdpthf@naver.com>
2022-04-02 23:51:02 +0900

    ♻ :: 유저 정보도 함께 조회

--- src/main/java/com/walkhub/walkhub/domain/user/domain/repository/vo/UserDetailsVO.java
+++ src/main/java/com/walkhub/walkhub/domain/user/domain/repository/vo/UserDetailsVO.java
@@ -3,20 +3,30 @@ package com.walkhub.walkhub.domain.user.domain.repository.vo;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 public class UserDetailsVO {
-    private final List<Integer> walkCountList;
+    private final Long userId;
+    private final String name;
+    private final String profileImageUrl;
+    private final Integer grade;
+    private final Integer classNum;
+    private final Integer number;
     private final Integer averageWalkCount;
     private final Integer totalWalkCount;
     private final Integer averageDistance;
     private final Integer totalDistance;
 
     @QueryProjection
-    public UserDetailsVO(List<Integer> walkCountList, Integer averageWalkCount, Integer totalWalkCount,
+    public UserDetailsVO(Long userId, String name, String profileImageUrl,
+                         Integer grade, Integer classNum, Integer number,
+                         Integer averageWalkCount, Integer totalWalkCount,
                          Integer averageDistance, Integer totalDistance) {
-        this.walkCountList = walkCountList;
+        this.userId = userId;
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+        this.grade = grade;
+        this.classNum = classNum;
+        this.number = number;
         this.averageWalkCount = averageWalkCount;
         this.totalWalkCount = totalWalkCount;
         this.averageDistance = averageDistance;
