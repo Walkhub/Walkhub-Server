e472d786f18a45894984b9cd23108d744a0010c7
이준서 <ljchan71@gmail.com>
2022-04-03 00:46:36 +0900

    ⚡️ :: Vo수정

--- src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/vo/ShowParticipatedChallengeVO.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/domain/repository/vo/ShowParticipatedChallengeVO.java
@@ -12,30 +12,31 @@ public class ShowParticipatedChallengeVO {
 
     private final Long challengeId;
     private final String name;
+    private final String imageUrl;
     private final LocalDate startAt;
     private final LocalDate endAt;
     private final Integer goal;
     private final GoalScope goalScope;
     private final GoalType goalType;
     private final String award;
-    private final Integer totalWalkCount;
+    private final Integer totalValue;
     private final Long writerId;
     private final String writerName;
     private final String writerProfileImageUrl;
 
     @QueryProjection
-    public ShowParticipatedChallengeVO(Long challengeId, String name, LocalDate startAt, LocalDate endAt,
-                                       Integer goal, GoalScope goalScope, GoalType goalType, String award, Integer totalWalkCount,
-                                       Long writerId, String writerName, String writerProfileImageUrl) {
+    public ShowParticipatedChallengeVO(Long challengeId, String name, String imageUrl, LocalDate startAt, LocalDate endAt, Integer goal,
+        GoalScope goalScope, GoalType goalType, String award, Integer totalValue, Long writerId, String writerName, String writerProfileImageUrl) {
         this.challengeId = challengeId;
         this.name = name;
+        this.imageUrl = imageUrl;
         this.startAt = startAt;
         this.endAt = endAt;
         this.goal = goal;
         this.goalScope = goalScope;
         this.goalType = goalType;
         this.award = award;
-        this.totalWalkCount = totalWalkCount;
+        this.totalValue = totalValue;
         this.writerId = writerId;
         this.writerName = writerName;
         this.writerProfileImageUrl = writerProfileImageUrl;
