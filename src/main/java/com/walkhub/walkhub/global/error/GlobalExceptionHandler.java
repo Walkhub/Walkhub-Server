961a205911b09c402d9cc74c37fad314e5568bff
정대현 <eogus_2513@dsm.hs.kr>
2022-04-04 21:53:58 +0900

    ⚡️ :: @Vliad로 나오는 error 처리

--- src/main/java/com/walkhub/walkhub/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/walkhub/walkhub/global/error/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,13 +30,27 @@ public class GlobalExceptionHandler {
         );
     }
 
-    @ExceptionHandler(BindException.class)
+    @ExceptionHandler({BindException.class})
     public ResponseEntity<?> bindException(BindException e) {
         Map<String, String> errorMap = new HashMap<>();
 
         for (FieldError error : e.getFieldErrors()) {
             errorMap.put(error.getField(), error.getDefaultMessage());
         }
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<?> constraintViolationException(ConstraintViolationException e) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        int i = 0;
+
+        for (ConstraintViolation<?> error : e.getConstraintViolations()) {
+            errorMap.put("error" + i, error.getMessageTemplate());
+            i++;
+        }
+
         return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
     }
 }
