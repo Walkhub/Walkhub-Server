1ca9710576f89b44e5014058abf6d6699a79724c
정대현 <eogus_2513@dsm.hs.kr>
2022-04-04 10:58:38 +0900

    :recycle: :: @NoArgsConstructor -> @AllArgsConstructor로 변경

--- src/main/java/com/walkhub/walkhub/domain/auth/presentation/dto/request/CheckAccountIdRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/auth/presentation/dto/request/CheckAccountIdRequest.java
@@ -1,12 +1,12 @@
 package com.walkhub.walkhub.domain.auth.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class CheckAccountIdRequest {
 
     @NotBlank(message = "account_id는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
--- src/main/java/com/walkhub/walkhub/domain/user/presentation/dto/request/CheckClassCodeRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/user/presentation/dto/request/CheckClassCodeRequest.java
@@ -1,12 +1,12 @@
 package com.walkhub.walkhub.domain.user.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class CheckClassCodeRequest {
 
     @NotBlank(message = "code는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
