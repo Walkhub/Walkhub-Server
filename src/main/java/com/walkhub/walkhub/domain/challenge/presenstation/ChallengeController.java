8c32e9e4d1e0311e5ae5c43739763f162da87af0
sehyoun <lyutvs@gmail.com>
2022-04-08 08:22:21 +0900

    ♻️ :: 임포트 수정

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @RequestMapping("/challenges")
