f2256a5f7e2d752fed724661018970c939557c48
정대현 <eogus_2513@dsm.hs.kr>
2022-04-07 22:48:37 +0900

    :recycle: :: 디자인에 따라 수정

--- src/main/java/com/walkhub/walkhub/domain/school/presentation/dto/response/SchoolDetailsInfoResponse.java
+++ src/main/java/com/walkhub/walkhub/domain/school/presentation/dto/response/SchoolDetailsInfoResponse.java
@@ -3,14 +3,22 @@ package com.walkhub.walkhub.domain.school.presentation.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 @Builder
 public class SchoolDetailsInfoResponse {
 
-    private final Long userCount;
-    private final Integer weekTotalWalkCount;
-    private final Integer monthTotalWalkCount;
-    private final Integer weekRanking;
-    private final Integer monthRanking;
+    private final DateRankResponse week;
+    private final DateRankResponse month;
+
+    @Getter
+    @Builder
+    public static class DateRankResponse {
+        private final Long userCount;
+        private final LocalDate date;
+        private final Integer totalWalkCount;
+        private final Integer ranking;
+    }
 
 }
