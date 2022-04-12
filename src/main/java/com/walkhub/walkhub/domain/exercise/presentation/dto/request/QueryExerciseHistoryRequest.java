7b6a65ccdec392aeb34313648464193052a76293
정대현 <eogus_2513@dsm.hs.kr>
2022-04-12 23:17:54 +0900

    :recycle: :: ErrorMessage 추가

--- src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/request/QueryExerciseHistoryRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/presentation/dto/request/QueryExerciseHistoryRequest.java
@@ -2,15 +2,24 @@ package com.walkhub.walkhub.domain.exercise.presentation.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
 public class QueryExerciseHistoryRequest {
 
-    private final LocalDate startAt;
-    private final LocalDate endAt;
-    private final Integer page;
+    @NotNull(message = "start_at은 Null일 수 없습니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startAt;
+
+    @NotNull(message = "end_at은 Null일 수 없습니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endAt;
+
+    @NotNull(message = "page는 Null일 수 없습니다.")
+    private Integer page;
 
 }
