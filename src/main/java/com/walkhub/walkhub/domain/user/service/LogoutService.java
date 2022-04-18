4f53b36584b45122572e162a6f1a71687c00f31c
KimBeomJin <sdpthf@naver.com>
2022-04-18 11:26:54 +0900

    ⚡️ :: logout 시 refresh token 삭제

--- src/main/java/com/walkhub/walkhub/domain/user/service/LogoutService.java
+++ src/main/java/com/walkhub/walkhub/domain/user/service/LogoutService.java
@@ -1,5 +1,8 @@
 package com.walkhub.walkhub.domain.user.service;
 
+import com.walkhub.walkhub.domain.auth.domain.RefreshToken;
+import com.walkhub.walkhub.domain.auth.domain.repository.RefreshTokenRepository;
+import com.walkhub.walkhub.domain.auth.exception.RefreshTokenNotFoundException;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.global.annotation.ServiceWithTransactionalReadOnly;
@@ -12,11 +15,17 @@ import javax.transaction.Transactional;
 public class LogoutService {
 
     private final UserFacade userFacade;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Transactional
     public void execute() {
         User user = userFacade.getCurrentUser();
         user.setDeviceToken(null);
+
+        RefreshToken refreshToken = refreshTokenRepository.findByAccountId(user.getAccountId())
+                .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
+
+        refreshTokenRepository.delete(refreshToken);
     }
 
 }
