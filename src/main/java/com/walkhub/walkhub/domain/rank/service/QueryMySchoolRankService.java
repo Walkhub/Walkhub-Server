07c09a44550d50d75ca70ee1ed67da772c74268b
lyutvs <lyutvs@gmail.com>
2022-04-20 02:00:45 +0900

    ⚡️ :: 객체 정리

--- src/main/java/com/walkhub/walkhub/domain/rank/service/QueryMySchoolRankService.java
+++ src/main/java/com/walkhub/walkhub/domain/rank/service/QueryMySchoolRankService.java
@@ -1,6 +1,8 @@
 package com.walkhub.walkhub.domain.rank.service;
 
 import com.walkhub.walkhub.domain.rank.presentation.dto.response.SchoolRankResponse.MySchoolResponse;
+import com.walkhub.walkhub.domain.school.domain.School;
+import com.walkhub.walkhub.domain.user.domain.Section;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.global.annotation.ServiceWithTransactionalReadOnly;
@@ -15,13 +17,15 @@ public class QueryMySchoolRankService {
 
     public MySchoolResponse execute() {
         User user = userFacade.getCurrentUser();
+        School school = user.getSchool();
+        Section section = user.hasSection() ? user.getSection() : Section.builder().build();
 
         return MySchoolResponse.builder()
-                .schoolId(user.getSchool().getId())
-                .name(user.getSchool().getName())
-                .logoImageUrl(user.getSchool().getLogoImageUrl())
-                .grade(user.getSection().getGrade())
-                .classNum(user.getSection().getClassNum())
+                .schoolId(school.getId())
+                .name(school.getName())
+                .logoImageUrl(school.getLogoImageUrl())
+                .grade(section.getGrade())
+                .classNum(section.getClassNum())
                 .build();
     }
 }
