bf96fb85b7e40fc989771055872c787e4e180f8c
정대현 <eogus_2513@dsm.hs.kr>
2022-04-19 13:30:30 +0900

    :bookmark_tabs: :: CreateChallengeResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/dto/response/CreateChallengeResponse.java
@@ -0,0 +1,12 @@
+package com.walkhub.walkhub.domain.challenge.presenstation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateChallengeResponse {
+
+    private final Long ChallengeId;
+
+}
