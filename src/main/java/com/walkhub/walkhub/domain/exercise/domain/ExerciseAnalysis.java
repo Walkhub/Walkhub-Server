7142ecd70bacd348b4eeedd8a1ab6bfb626021fb
KimBeomJin <sdpthf@naver.com>
2022-04-25 16:34:19 +0900

    ⚡️ :: exercise analysis entity

--- src/main/java/com/walkhub/walkhub/domain/exercise/domain/ExerciseAnalysis.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/domain/ExerciseAnalysis.java
@@ -7,14 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
@@ -39,13 +32,12 @@ public class ExerciseAnalysis {
     @NotNull
     private LocalDate date;
 
-    @Column(nullable = false)
+    @NotNull
     private Double exerciseTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
 
     @Builder
     public ExerciseAnalysis(Double calorie, Integer walkCount, Double exerciseTime,
