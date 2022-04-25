0aac6cbb9b706f3c057afa8f5d64b49e63b18f94
KimBeomJin <sdpthf@naver.com>
2022-04-25 16:31:35 +0900

    ⚡️ :: badge collection foreign key not null

--- src/main/java/com/walkhub/walkhub/domain/badge/domain/BadgeCollection.java
+++ src/main/java/com/walkhub/walkhub/domain/badge/domain/BadgeCollection.java
@@ -6,12 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,12 +16,12 @@ public class BadgeCollection {
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "badge_id")
+    @JoinColumn(name = "badge_id", nullable = false)
     private Badge badge;
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Builder
