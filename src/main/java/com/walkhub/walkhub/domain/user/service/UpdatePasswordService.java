ff4049f07bdf242540181eaaf8178e10cf61cf4e
정대현 <eogus_2513@dsm.hs.kr>
2022-04-26 19:34:16 +0900

    :recycle: :: code smells 해결

--- src/main/java/com/walkhub/walkhub/domain/user/service/UpdatePasswordService.java
+++ src/main/java/com/walkhub/walkhub/domain/user/service/UpdatePasswordService.java
@@ -1,10 +1,6 @@
 package com.walkhub.walkhub.domain.user.service;
 
 import com.walkhub.walkhub.domain.user.domain.User;
-import com.walkhub.walkhub.domain.user.domain.UserAuthCode;
-import com.walkhub.walkhub.domain.user.domain.repository.UserAuthCodeRepository;
-import com.walkhub.walkhub.domain.user.exception.UnauthorizedUserAuthCodeException;
-import com.walkhub.walkhub.domain.user.exception.UserAuthCodeNotFoundException;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
 import com.walkhub.walkhub.domain.user.presentation.dto.request.UpdatePasswordRequest;
 import com.walkhub.walkhub.global.annotation.ServiceWithTransactionalReadOnly;
