3d3c2fdae6b10f0acfca30b70e87b26f6ebf2a7e
정대현 <eogus_2513@dsm.hs.kr>
2022-04-26 20:23:45 +0900

    :recycle: :: 정렬

--- src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
+++ src/main/java/com/walkhub/walkhub/domain/challenge/presenstation/ChallengeController.java
@@ -3,11 +3,35 @@ package com.walkhub.walkhub.domain.challenge.presenstation;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.request.ChallengeParticipantRequest;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.request.CreateChallengeRequest;
 import com.walkhub.walkhub.domain.challenge.presenstation.dto.request.UpdateChallengeRequest;
-import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.*;
-import com.walkhub.walkhub.domain.challenge.service.*;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.ChallengeResponse;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeDetailsForStudentResponse;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeDetailsForTeacherResponse;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeListForStudentResponse;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeListForTeacherResponse;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryChallengeParticipantListResponse;
+import com.walkhub.walkhub.domain.challenge.presenstation.dto.response.QueryParticipateChallengeListResponse;
+import com.walkhub.walkhub.domain.challenge.service.CreateChallengeService;
+import com.walkhub.walkhub.domain.challenge.service.ParticipateChallengeService;
+import com.walkhub.walkhub.domain.challenge.service.QueryChallengeDetailsForStudentService;
+import com.walkhub.walkhub.domain.challenge.service.QueryChallengeDetailsForTeacherService;
+import com.walkhub.walkhub.domain.challenge.service.QueryChallengeListForStudentService;
+import com.walkhub.walkhub.domain.challenge.service.QueryChallengeListForTeacherService;
+import com.walkhub.walkhub.domain.challenge.service.QueryChallengeParticipantListService;
+import com.walkhub.walkhub.domain.challenge.service.QueryParticipatedChallengeListService;
+import com.walkhub.walkhub.domain.challenge.service.RemoveChallengeService;
+import com.walkhub.walkhub.domain.challenge.service.UpdateChallengeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -36,7 +60,7 @@ public class ChallengeController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/{challenge-id}")
     public ChallengeResponse updateChallenge(@PathVariable("challenge-id") Long id,
-                                @RequestBody @Valid UpdateChallengeRequest request) {
+                                             @RequestBody @Valid UpdateChallengeRequest request) {
         return updateChallengeService.execute(id, request);
     }
 
@@ -79,7 +103,7 @@ public class ChallengeController {
 
     @GetMapping("/{challenge-id}/progress")
     public QueryChallengeParticipantListResponse queryChallengeParticipantList(@PathVariable("challenge-id") Long id,
-                                                                          @Valid ChallengeParticipantRequest request) {
+                                                                               @Valid ChallengeParticipantRequest request) {
         return queryChallengeParticipantListService.execute(id, request);
     }
 
