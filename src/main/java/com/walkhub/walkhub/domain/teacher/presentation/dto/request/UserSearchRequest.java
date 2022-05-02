6bd690cb24abea414898fd7301b3198f46e6b957
정대현 <eogus_2513@dsm.hs.kr>
2022-05-02 17:22:32 +0900

    :recycle: :: validation option 추가

--- src/main/java/com/walkhub/walkhub/domain/teacher/presentation/dto/request/UserSearchRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/teacher/presentation/dto/request/UserSearchRequest.java
@@ -5,12 +5,22 @@ import com.walkhub.walkhub.domain.teacher.type.SortStandard;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 public class UserSearchRequest {
+
+    @NotNull(message = "scope는 Null일 수 없습니다.")
     private final AuthorityScope scope;
+
+    @NotNull(message = "sort는 Null일 수 없습니다.")
     private final SortStandard sort;
+
     private final Integer grade;
+
     private final Integer classNum;
+
     private final String name;
+
 }
