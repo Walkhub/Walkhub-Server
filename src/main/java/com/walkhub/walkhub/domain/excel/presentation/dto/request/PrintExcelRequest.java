54e7020a3bd93604dbec8c67a7d67c7aff5d9caf
정대현 <eogus_2513@dsm.hs.kr>
2022-05-02 11:39:44 +0900

    :bug: :: @NoArgsConstructor 제거 & @DateTimeFormat 설정 & @NotNull 설정

--- src/main/java/com/walkhub/walkhub/domain/excel/presentation/dto/request/PrintExcelRequest.java
+++ src/main/java/com/walkhub/walkhub/domain/excel/presentation/dto/request/PrintExcelRequest.java
@@ -3,18 +3,25 @@ package com.walkhub.walkhub.domain.excel.presentation.dto.request;
 import com.walkhub.walkhub.domain.excel.domain.type.UserType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class PrintExcelRequest {
 
-    private LocalDate startAt;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private final LocalDate startAt;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate endAt;
-    private UserType userType;
-    private Integer grade;
-    private Integer classNum;
+
+    @NotNull(message = "user_type은 Null일 수 없습니다.")
+    private final UserType userType;
+
+    private final Integer grade;
+
+    private final Integer classNum;
 }
