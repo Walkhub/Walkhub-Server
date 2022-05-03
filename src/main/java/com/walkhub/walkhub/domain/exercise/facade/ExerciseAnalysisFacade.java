09b29cd0efbbdf8db2e5ca5dab99ce3063e03372
KimBeomJin <sdpthf@naver.com>
2022-05-03 15:49:23 +0900

    ⚡️ :: 운동 분석 결과 출력 로직 facade로 분리

---
+++ src/main/java/com/walkhub/walkhub/domain/exercise/facade/ExerciseAnalysisFacade.java
@@ -0,0 +1,41 @@
+package com.walkhub.walkhub.domain.exercise.facade;
+
+import com.walkhub.walkhub.domain.exercise.domain.ExerciseAnalysis;
+import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
+import com.walkhub.walkhub.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Component
+public class ExerciseAnalysisFacade {
+
+    private final ExerciseAnalysisRepository exerciseAnalysisRepository;
+
+    public List<Integer> getWalkCountList(User user, LocalDate startAt, LocalDate endAt) {
+        Map<LocalDate, List<ExerciseAnalysis>> exerciseAnalysisDateList =
+                exerciseAnalysisRepository.findAllByUserAndDateBetweenOrderByDate(user, startAt, endAt)
+                        .stream()
+                        .collect(Collectors.groupingBy(ExerciseAnalysis::getDate));
+
+        List<Integer> walkCountList = new LinkedList<>();
+
+        for (LocalDate i = startAt; !i.isAfter(LocalDate.now()); i = i.plusDays(1)) {
+            List<ExerciseAnalysis> exerciseAnalyseOfToday = exerciseAnalysisDateList.get(i);
+            if (exerciseAnalyseOfToday == null) {
+                walkCountList.add(0);
+            } else {
+                walkCountList.add(exerciseAnalyseOfToday.get(0).getWalkCount());
+            }
+        }
+
+        return walkCountList;
+    }
+
+}
--- src/main/java/com/walkhub/walkhub/domain/exercise/service/QueryExerciseAnalysisService.java
+++ src/main/java/com/walkhub/walkhub/domain/exercise/service/QueryExerciseAnalysisService.java
@@ -2,6 +2,7 @@ package com.walkhub.walkhub.domain.exercise.service;
 
 import com.walkhub.walkhub.domain.exercise.domain.ExerciseAnalysis;
 import com.walkhub.walkhub.domain.exercise.domain.repository.ExerciseAnalysisRepository;
+import com.walkhub.walkhub.domain.exercise.facade.ExerciseAnalysisFacade;
 import com.walkhub.walkhub.domain.exercise.presentation.dto.response.QueryExerciseAnalysisResponse;
 import com.walkhub.walkhub.domain.user.domain.User;
 import com.walkhub.walkhub.domain.user.facade.UserFacade;
@@ -9,10 +10,7 @@ import com.walkhub.walkhub.global.annotation.ServiceWithTransactionalReadOnly;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @ServiceWithTransactionalReadOnly
@@ -20,6 +18,7 @@ public class QueryExerciseAnalysisService {
 
     private final ExerciseAnalysisRepository exerciseAnalysisRepository;
     private final UserFacade userFacade;
+    private final ExerciseAnalysisFacade exerciseAnalysisFacade;
 
     public QueryExerciseAnalysisResponse execute() {
         User user = userFacade.getCurrentUser();
@@ -29,21 +28,7 @@ public class QueryExerciseAnalysisService {
         ExerciseAnalysis todayExerciseAnalysis = exerciseAnalysisRepository.findByUserAndDate(user, now)
                 .orElse(ExerciseAnalysis.builder().walkCount(0).calorie(0.0).distance(0).exerciseTime(0.0).build());
 
-        Map<LocalDate, List<ExerciseAnalysis>> exerciseAnalysisDateList =
-                exerciseAnalysisRepository.findAllByUserAndDateBetweenOrderByDate(user, startAt, now)
-                        .stream()
-                        .collect(Collectors.groupingBy(ExerciseAnalysis::getDate));
-
-        List<Integer> walkCountList = new LinkedList<>();
-
-        for (LocalDate i = startAt; !i.isAfter(LocalDate.now()); i = i.plusDays(1)) {
-            List<ExerciseAnalysis> exerciseAnalyseOfToday = exerciseAnalysisDateList.get(i);
-            if (exerciseAnalyseOfToday == null) {
-                walkCountList.add(0);
-            } else {
-                walkCountList.add(exerciseAnalyseOfToday.get(0).getWalkCount());
-            }
-        }
+        List<Integer> walkCountList = exerciseAnalysisFacade.getWalkCountList(user, startAt, now);
 
         return QueryExerciseAnalysisResponse.builder()
                 .walkCountList(walkCountList)
