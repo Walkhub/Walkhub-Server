c4172711e4b7536157bb1f32de5def8f40ffe7b0
정대현 <eogus_2513@dsm.hs.kr>
2022-05-07 19:45:18 +0900

    :bookmark_tabs: :: (#735) SchoolRankRepositoryCustom

---
+++ src/main/java/com/walkhub/walkhub/domain/rank/domain/repository/SchoolRankRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.walkhub.walkhub.domain.rank.domain.repository;
+
+import com.walkhub.walkhub.domain.rank.domain.repository.vo.SchoolListVo;
+import com.walkhub.walkhub.domain.rank.domain.type.SchoolDateType;
+import com.walkhub.walkhub.domain.rank.domain.type.Sort;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface SchoolRankRepositoryCustom {
+
+    List<SchoolListVo> getSchoolSearch(Sort sort, SchoolDateType dateType, LocalDate createdAt, String name);
+}
