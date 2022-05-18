57e285af6e655524f0503fd89e46c0328b2a0420
lyutvs <lyutvs@gmail.com>
2022-05-18 16:01:04 +0900

    📑 :: NotificatoinWhetherResponse

---
+++ src/main/java/com/walkhub/walkhub/domain/notification/presentation/dto/response/NotificationWhetherResponse.java
@@ -0,0 +1,23 @@
+package com.walkhub.walkhub.domain.notification.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class NotificationWhetherResponse {
+
+    private final List<WhetherResponse> whetherResponseList;
+
+    @Getter
+    @Builder
+    public static class WhetherResponse {
+        private final Long id;
+        private final String title;
+        private final Boolean isSubscribe;
+    }
+
+}
