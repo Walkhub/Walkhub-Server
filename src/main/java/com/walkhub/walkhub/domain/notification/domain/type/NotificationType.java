d98bfdc75faaeb7a72fbdc9c7cb65cd17e346de7
lyutvs <lyutvs@gmail.com>
2022-05-19 16:22:21 +0900

    📑 :: NotificationType

---
+++ src/main/java/com/walkhub/walkhub/domain/notification/domain/type/NotificationType.java
@@ -0,0 +1,10 @@
+package com.walkhub.walkhub.domain.notification.domain.type;
+
+public enum NotificationType {
+    NOTICE,
+    CHALLENGE,
+    CHALLENGE_EXPIRATION,
+    CHALLENGE_SUCCESS,
+    EXERCISE,
+    CHEERING
+}
