01019f7455b028575b3472d289b6ca1ceb44b04b
정대현 <eogus_2513@dsm.hs.kr>
2022-06-05 18:58:16 +0900

    ⚡️ :: USER_PROFILE_IMAGE 추가

--- src/main/java/com/walkhub/walkhub/infrastructure/image/DefaultImage.java
+++ src/main/java/com/walkhub/walkhub/infrastructure/image/DefaultImage.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultImage {
-    public static final String USER_PROFILE_IMAGE = "'a'";
+    public static final String USER_PROFILE_IMAGE = "'https://walkhub.s3.ap-northeast-2.amazonaws.com/walkhub/63f3c045-5fbf-432e-bcd5-a4e605844dbbUser.svg'";
     public static final String CHALLENGE_IMAGE = "'b'";
     public static final String SCHOOL_LOGO_IMAGE = "'c'";
     public static final String BADGE_IMAGE = "'d'";
