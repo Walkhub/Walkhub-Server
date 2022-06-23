afb92514a9c55a02ed93110e2bbecee0b9e56b92
lyutvs <lyutvs@gmail.com>
2022-06-23 21:34:05 +0900

    ⚡️ :: 필요없는 연관관계 삭제

--- src/main/java/com/walkhub/walkhub/domain/notification/domain/Topic.java
+++ src/main/java/com/walkhub/walkhub/domain/notification/domain/Topic.java
@@ -1,8 +1,6 @@
 package com.walkhub.walkhub.domain.notification.domain;
 
-
 import com.walkhub.walkhub.domain.notification.domain.type.NotificationType;
-import com.walkhub.walkhub.domain.user.domain.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,14 +30,9 @@ public class Topic {
     @ColumnDefault("0")
     private Boolean isSubscribe;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
     @Builder
-    public Topic(NotificationType type, Boolean isSubscribe, User user) {
+    public Topic(NotificationType type, Boolean isSubscribe) {
         this.type = type;
         this.isSubscribe = isSubscribe;
-        this.user = user;
     }
 }
