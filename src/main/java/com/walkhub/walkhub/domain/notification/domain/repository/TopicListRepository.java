dbbbfc5b9a66079d94afce150428830ae495bba9
lyutvs <lyutvs@gmail.com>
2022-06-23 21:33:52 +0900

    ⚡️ :: fetch join

--- src/main/java/com/walkhub/walkhub/domain/notification/domain/repository/TopicListRepository.java
+++ src/main/java/com/walkhub/walkhub/domain/notification/domain/repository/TopicListRepository.java
@@ -3,12 +3,14 @@ package com.walkhub.walkhub.domain.notification.domain.repository;
 import com.walkhub.walkhub.domain.notification.domain.TopicList;
 import com.walkhub.walkhub.domain.notification.domain.TopicListId;
 import com.walkhub.walkhub.domain.user.domain.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface TopicListRepository extends CrudRepository<TopicList, TopicListId> {
 
+    @Query("select tl from TopicList tl join fetch tl.id.user")
     List<TopicList> findAllByIdUser(User user);
 
 }
