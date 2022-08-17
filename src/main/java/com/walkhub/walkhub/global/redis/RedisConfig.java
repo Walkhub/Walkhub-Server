7cc9c88c56043d5f3b38b63aa2e7ac6139d651b5
정대현 <eogus_2513@dsm.hs.kr>
2022-08-17 21:44:47 +0900

    :recycle: :: redis config 수정

--- src/main/java/com/walkhub/walkhub/global/redis/RedisConfig.java
+++ src/main/java/com/walkhub/walkhub/global/redis/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
@@ -21,7 +22,11 @@ public class RedisConfig {
     @Bean
     @ConditionalOnMissingBean(RedisConnectionFactory.class)
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
--- src/main/java/com/walkhub/walkhub/global/redis/properties/RedisProperties.java
+++ src/main/java/com/walkhub/walkhub/global/redis/properties/RedisProperties.java
@@ -12,4 +12,5 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 public class RedisProperties {
     private final String host;
     private final int port;
+    private final String password;
 }
